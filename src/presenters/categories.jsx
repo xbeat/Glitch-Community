bccfd009632e1f25e66c64d04f2f22805bcd8849
Glitch (brillig-landing) <none>
2018-03-09 23:11:33 +0000

    🎊🎃 Checkpoint src/presenters/categories.jsx:8/306 src/presenters/pages/questions.js:8/22 src/presenters/pages/index.js:8/95 src/templates/pages/index.jade:8/16 webpack.config.js:8/12 src/presenters/pages/search.js:8/12 src/models/category.js:8/14 src/templates/includes/category.jade:8/1

--- src/presenters/categories.jsx
+++ src/presenters/categories.jsx
@@ -4,13 +4,13 @@ const Categories = ({categories}) => (
   <section class="categories" role="navigation">
     <h2>More Ideas</h2>
     <ul>
-      { categories.map((category) => (
-        <a class="category-box-link" href="hello-worlds">
+      { categories.map(({avatarUrl, color, name, url}) => (
+        <a class="category-box-link" href={url}>
           <li>
-            <div class="category-box centered" style="background-color: #FCF3AF">
-              <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fcomputer.svg" alt="Hello Worlds"/>
+            <div class="category-box centered" style={`background-color: ${color()}`}>
+              <img src={avatarUrl} alt={name}/>
             </div>
-            <div class="category-box-label centered" style="background-color: #FCF3AF">Hello Worlds</div>
+            <div class="category-box-label centered" style={`background-color: ${color()}`}>{name}</div>
           </li>
         </a>
       ))}
@@ -19,22 +19,3 @@ const Categories = ({categories}) => (
 );
 
 export default Categories;
-
-
-/*
-section.categories(role="navigation")
-      h2 More Ideas
-      ul
-        - @categories().forEach (category) ->
-
-          - avatarUrl = category.avatarUrl
-          - color = category.color()
-          - name = category.name
-          - url = category.url
-
-          a.category-box-link(href=url)
-            li
-              .category-box.centered(style="background-color: #{color}")
-                img(src=avatarUrl alt=name)
-              .category-box-label.centered(style="background-color: #{color}")= name
-*/
--- src/presenters/pages/index.js
+++ src/presenters/pages/index.js
@@ -3,11 +3,11 @@ const LayoutPresenter = require("../layout");
 const CtaButtonsPresenter = require("../cta-buttons");
 const HeaderPresenter = require("../header");
 const FeaturedCollectionPresenter = require("../featured-collection");
-const CategoryPresenter = require ("../category");
 const RecentProjectsPresenter = require("../recent-projects");
 const QuestionsPresenter = require("../questions");
 const Reactlet = require("../../templates/reactlet");
 
+import Categories from "../categories.jsx";
 import WhatIsGlitch from "../what-is-glitch.jsx";
 import ByFogCreek from "../includes/by-fogcreek.jsx";
 import {render, unmountComponentAtNode} from 'react-dom';
@@ -65,8 +65,15 @@ module.exports = function(application) {
       return categories.map((category) =>CategoryPresenter(application, category));
     },
    
-    categories() {
-      return application.categories();
+    Categories() {
+      const id = "categories";
+      setTimeout(() => { 
+        return render(
+          React.createElement(Categories, {categories: application.categories()}),
+          document.getElementById(id)
+        );
+      });
+      return Reactlet({id: id});
     },
     
     QuestionsPresenter() {
