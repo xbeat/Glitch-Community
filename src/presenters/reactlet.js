0836e611f7e8599d4888f497967284a5ad64d5a5
Glitch (brillig-landing) <none>
2018-03-12 18:11:22 +0000

    🛀🍦 Checkpoint src/presenters/reactlet.js:8/33 src/templates/reactlet.jade:8/61

---
+++ src/presenters/reactlet.js
@@ -0,0 +1,36 @@
+// This shim helps attach React components to Jadelet files
+// Also see presenters/reactlet.js
+
+const ReactletTemplate = require("../templates/includes/reactlet");
+
+module.exports = function(application, category) {
+  const { projects } = category;
+
+  const projectElements = projects.map(project => ProjectItemPresenter(application, project, category));
+
+  const self = {
+
+    category,
+    projects,
+
+    style() {
+      return {backgroundColor: category.backgroundColor()};
+    },
+
+    url() {
+      return category.url();
+    },
+
+    name() {
+      return category.name();
+    },
+    
+    description() {
+      return category.description();
+    }
+  };
+    
+  self.projects = projectElements;
+
+  return CategoryTemplate(self);
+};
