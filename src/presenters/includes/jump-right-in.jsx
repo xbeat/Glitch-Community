0bd38878121951b0c02239f3a3658ff263d14f8a
Glitch (brillig-landing) <none>
2018-03-14 21:52:40 +0000

    💀🙅 Checkpoint ./src/presenters/includes/jump-right-in.jsx:8/774

--- src/presenters/includes/jump-right-in.jsx
+++ src/presenters/includes/jump-right-in.jsx
@@ -9,13 +9,13 @@ const StarterProject = ({title, domain, description, avatarUrl}) => {
           <img width="32" height="32" src={GlitchTeamAvatarUrl} alt="Glitch Team"/>
         </div>
       </div>
-      <a href={`https://glitch.com/edit/#!/remix/${domain}`}>
+      <a href={`/~${domain}`}>
         <div className="project">
           <div className="project-container">
             <img className="avatar" src={avatarUrl} alt={`Avatar image for ${title}`}/>
             <button>
               <span className="private-project-badge"></span>
-              <div className="project-name">{title} <div className="emoji microphone"></div></div>
+              <div className="project-name">{title}</div>
             </button>
             <div className="description">{description}</div>
             <div className="overflow-mask"></div>
@@ -27,16 +27,29 @@ const StarterProject = ({title, domain, description, avatarUrl}) => {
 };
   
 const JumpRightIn = () => {
+  
+  /*
+    hello-node and hello-webpage are remixes of the "real" sites.
+    I'll make sure the links are pointed to the right spot before deployment.
+    
+    Their edit links are here:
+     hello-webpage: https://glitch.com/edit/#!/join/ee85f517-943e-4eec-8291-af16d8b5dcf4
+     hello-node: https://glitch.com/edit/#!/join/14042991-af37-4350-a1e3-84149029506a
+     
+    It's safe to play with them and make enhancements that we can move back to the real thi
+  
+  
+  */
   const starterProjects = [
     {
-      title: "Remix a Node.js App",
+      title: "hello-node",
       domain: "hello-node",
       description: "A simple Node app built on Express, instantly up and running.",
       avatarUrl: "https://cdn.glitch.com/180b5e22-4649-4c71-9a21-2482eb557c8c%2FNode.js_logo.svg?1521062036657"
     },
     {
-      title: "Remix a Web Page",
-      domain: "hello-website",
+      title: "hello-webpage",
+      domain: "hello-webpage",
       description: "Your very own basic web page, ready for you to customize.",
       avatarUrl: "https://cdn.glitch.com/180b5e22-4649-4c71-9a21-2482eb557c8c%2FHTML5_logo_and_wordmark.svg?1521062032021"
     },
