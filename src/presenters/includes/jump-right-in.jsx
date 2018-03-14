a84d9ce3313661547c6182c2e36af8d7c278d40f
Glitch (brillig-landing) <none>
2018-03-14 19:49:05 +0000

    💦💴 Checkpoint ./src/presenters/includes/jump-right-in.jsx:8/716 ./.glitch-assets:8/388 ./public/styles/emoji.styl:8/139

--- src/presenters/includes/jump-right-in.jsx
+++ src/presenters/includes/jump-right-in.jsx
@@ -28,7 +28,7 @@ article.projects.jump-right-in
 */ 
 
 import React from 'react';
-const StarterProject = ({name}) => {
+const StarterProject = ({title, domain, description, avatarUrl}) => {
   const GlitchTeamAvatarUrl = "https://cdn.gomix.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch-team-avatar.svg?1489266029267"
 
   return (
@@ -40,15 +40,15 @@ const StarterProject = ({name}) => {
           </div>
         </div>
       </span>
-      <a href={`/~{name}`">
+      <a href={`https://glitch.com/edit/#!/remix/{project}`}>
         <div className="project" data-track="project" data-track-label="backbone-todomvc">
           <div className="project-container">
-            <img className="avatar" src="https://cdn.glitch.com/project-avatar/c16a0c85-5369-42c2-9f2f-9144d02c5c93.png" alt="backbone-todomvc avatar"/>
+            <img className="avatar" src="{avatarUrl}" alt="Avatar image for {title}"/>
             <button className="">
               <span className="private-project-badge"></span>
-              <div className="project-name">{name}</div>
+              <div className="project-name">{title}<div class="emoji microphone"></div></div>
             </button>
-            <div className="description">TodoMVC example using Backbone.js</div>
+            <div className="description">{description}</div>
             <div className="overflow-mask"></div>
           </div>
         </div>
@@ -57,14 +57,20 @@ const StarterProject = ({name}) => {
   );
 };
   
-const JumpRightIn = () => (
-  <article className="jump-right-in projects">
-    <h2>Jump Right In</h2>
-    <ul className="projects-container">
-      <StarterProject key="website" name="website"/>
-      <StarterProject key="node" name="node"/>
-    </ul>
-  </article>
-);
+const JumpRightIn = () => {
+  starterProjects = [
+    {title: "Create a node.js app, domain, description, avatarUrl},
+  ];
+     
+  return (
+    <article className="jump-right-in projects">
+      <h2>Jump Right In</h2>
+      <ul className="projects-container">
+        <StarterProject key="website" name="website"/>
+        <StarterProject key="node" name="node"/>
+      </ul>
+    </article>
+  );
+}
 
 export default JumpRightIn;
