2e23421ffa472d89a08613cbde001e89a37ab84c
Glitch (brillig-landing) <none>
2018-03-19 22:06:14 +0000

    🍞👣 Checkpoint ./src/presenters/cta-buttons.js:8/2647 ./src/templates/includes/cta-buttons.jade:8/655 ./public/styles/profile.styl:8/93

--- src/presenters/cta-buttons.js
+++ src/presenters/cta-buttons.js
@@ -1,14 +1,11 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Check that you're happy with the conversion, then remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const Observable = require('o_0');
 
 const CtaButtonsTemplate = require("../templates/includes/cta-buttons");
 const CtaPopPresenter = require("./pop-overs/cta-pop");
+const ProjectModel = require("../models/project");
+const Reactlet = require("./reactlet");
+
+import JumpRightIn from "./includes/jump-right-in.jsx";
 
 module.exports = function(application) {
 
@@ -22,6 +19,27 @@ module.exports = function(application) {
 
     hiddenUnlessIsSignedIn() {
       if (!application.currentUser().isSignedIn()) { return 'hidden'; }
+    },
+    
+    JumpRightIn() {
+      const projectIds = [
+        'a0fcd798-9ddf-42e5-8205-17158d4bf5bb', // 'hello-express'
+        '929980a8-32fc-4ae7-a66f-dddb3ae4912c', // 'hello-webpage'
+      ];
+      const projects = ProjectModel.getProjectsByIds(application.api(), projectIds);
+      const fetchedProjects = projects.filter(project => project.fetched());
+      const starterProjects = fetchedProjects.map((project) => {
+        const {domain, description, avatar} = project;
+        return {
+          title: domain(),
+          domain: domain(),
+          description: description(),
+          avatar: avatar(),
+          showOverlay() { project.showOverlay(application); }
+        } 
+      });
+      
+      return Reactlet(JumpRightIn, {starterProjects});
     }
   };
 
