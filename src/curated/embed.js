2523544b6d75852515b23adc321487b45dc6a7d4
Glitch (brillig-landing) <none>
2018-03-20 15:35:23 +0000

    🕵⏲ Checkpoint ./src/curated/embed.js:8/679

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 module.exports =`
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 591px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/drawing-game?path=README.md&previewSize=75&sidebarCollapsed=true" alt="drawing-game on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/faithful-violet?path=index.html&previewSize=50&sidebarCollapsed=true" alt="faithful-violet on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
