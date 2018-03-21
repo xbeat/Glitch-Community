257eef8a1db380c74335d05af9fbdf60e3213592
Glitch (brillig-landing) <none>
2018-03-21 15:24:38 +0000

    👽🏍 Checkpoint ./public/styles/includes/includes.styl:8/42 ./src/curated/featured.js:8/66

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -6,7 +6,7 @@ Example:
   img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FWebcomicNameFeature.png?1517409070264",
   
   // Optional fields:
-  link: 'https://webcomicname-mashup.glitch.me'
+  link: 'https://webcomicname-mashup.glitch.me' // if absent, tile won't be a link.
   imgTitle:  ''  // 'title' attribute on the <img>, which displays text on hover.
 },
 */
@@ -26,7 +26,6 @@ module.exports =
     , {
       title: 'Make a Mash-up Comic',
       img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FWebcomicNameFeature.png?1517409070264",
-      //link: 'https://webcomicname-mashup.glitch.me'
-      imgTitle: "testerino"
+      link: 'https://webcomicname-mashup.glitch.me'
     },
   ];
