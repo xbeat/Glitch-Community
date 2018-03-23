90f96529dfc49a42014d0527e44f7ca3fcb6009c
Glitch (pricey-swim) <none>
2018-03-23 20:11:47 +0000

    🍭💋 Checkpoint ./webpack.config.js:8/102 ./.gitignore:8/13

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = () => {
           loader: "eslint-loader",
           options: {
             fix: true,
-            cache: true, //caching tends to make the config stick, so turn it off when reconfiguring
+            cache: `${SRC}/.eslintcache`, //caching tends to make the config stick, so blank it when reconfiguring
             emitError: true,
             emitWarning: true,
             failOnError: false,
