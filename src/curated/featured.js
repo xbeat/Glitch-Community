2b43df0b50d026cfcb893cf612bbc8cea60d2161
Glitch (community) <none>
2018-04-02 11:36:40 +0000

    🌏🎻 Checkpoint ./src/curated/featured.js:11/1548

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -14,28 +14,18 @@ Example:
 module.exports =
   [
     {
-      title: 'Get Ready!',
-      img: 'https://cdn.glitch.com/85de0811-a09d-482e-8aba-1039316a71ac%2Fv2_1wtcwd-monday.jpg?1521719756785',
-      link: 'https://medium.com/glitch/changing-the-way-we-build-the-web-together-14f8a792a600'
+      title: 'Teach Alexa New Skills',
+      img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Falexa.png?1491426616444',
+      link: 'https://glitch.com/~alexa-skill'
     }
     , {
-      title: 'Glitch Embeds',
-      img: "https://cdn.glitch.com/85de0811-a09d-482e-8aba-1039316a71ac%2Fv2_1wtcwd-tuesday.jpg?1521719757143",
-      link: 'https://medium.com/glitch/making-learning-to-code-more-accessible-d802effd52bf'
+      title: 'Create a Slack App',
+      img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffeatured-slack-slash-bot.svg?1494429489069",
+      link: 'https://glitch.com/~slack-slash-command'
     }
     , {
-      title: 'Faster Dev & Debugger',
-      img: "https://cdn.glitch.com/85de0811-a09d-482e-8aba-1039316a71ac%2Fv2_1w2cwd-wednesday.jpg?1521719756980",
-      link: 'https://medium.com/glitch/tackling-the-biggest-pain-points-in-web-development-57d64afe19dc'
-    }
-    , {
-      title: 'Glitch Rewind',
-      img: "https://cdn.glitch.com/85de0811-a09d-482e-8aba-1039316a71ac%2Fv2_1wtcwd-thursday.jpg?1521719757598",
-      link: 'https://medium.com/glitch/reinventing-version-control-with-glitch-rewind-914c350da442'
-    }
-    , {
-      title: 'Glitch Opens Up!',
-      img: "https://cdn.glitch.com/85de0811-a09d-482e-8aba-1039316a71ac%2Fv2_1wtcwd-friday.jpg?1521719757785",
-      link: 'https://medium.com/glitch/glitch-opens-up-welcome-85c62d0d6e84'
-    }    
+      title: 'Make WebVR, Reality',
+      img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FmozillaAframe.png?1494449169684",
+      link: 'https://glitch.com/aframe'
+    }   
   ];
