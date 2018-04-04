fe163ffc7700a691c5978ebd5fa0295d17a859ab
Glitch (community) <none>
2018-04-04 11:18:14 +0000

    🎬🚵 Checkpoint ./src/curated/embed.js:11/612

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 module.exports =`
 <!-- Copy and Paste Me -->
 <div class="glitch-embed-wrap" style="height: 388px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/faithful-violet?path=README.md&previewSize=100&sidebarCollapsed=true" alt="faithful-violet on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+  <iframe src="https://glitch.com/embed/#!/embed/floating-dots?path=README.md&previewSize=100&sidebarCollapsed=true" alt="floating-dots on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
