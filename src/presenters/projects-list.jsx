acbe23bac7d437f0b84ff88e6de55948704cd526
Glitch (future-riddle) <none>
2018-04-11 15:56:54 +0000

    💆👯 Checkpoint ./src/templates/projects-list.jade:8/392 ./src/presenters/projects-list.jsx:8/4353

---
+++ src/presenters/projects-list.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProjectItem from "./project-item.jsx"
+
+export const ProjectsList = ({closeAllPopOvers, title, projects, projectOptions}) => {
+  
+  const self = {
+
+    sectionTitle: title,
+
+    projects() {
+      return projects.map(project => Reactlet(ProjectItem, {closeAllPopOvers: application.closeAllPopOvers, project: project.asProps(), projectOptions}));
+    },
+
+    visibleIfNoPins() {
+      if ((title === 'Pinned Projects') && (projects.length === 0)) {
+        return 'visible';
+      }
+    },
+
+    hiddenUnlessTitleIsPinned() {
+      if (title !== 'Pinned Projects') { return 'hidden'; }
+    },
+  };
+
+  const psst = "https://cdn.glitch.com/55f8497b-3334-43ca-851e-6c9780082244%2Fpsst.svg?1500486136908";
+
+  return (
+
+    article.projects
+      h2
+        span= @sectionTitle
+          span.emoji.pushpin.emoji-in-title(class=@hiddenUnlessTitleIsPinned)
+      .placeholder.hidden(class=@visibleIfNoPins)
+        img.psst(src=psst)
+        p Pin your projects to show them off
+          span.emoji.pushpin
+      ul.projects-container
+        = @projects
+    );
+};
+
+ProjectsList.propTypes = {
+  projects: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+
+
+/*
+
+
+*/
