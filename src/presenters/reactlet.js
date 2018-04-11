41451a85de4a4ba09bb0d373aec57fa7a3bf3cf6
Glitch (future-riddle) <none>
2018-04-11 21:53:56 +0000

    🏭🖼 Checkpoint ./src/presenters/reactlet.js:8/916

--- src/presenters/reactlet.js
+++ src/presenters/reactlet.js
@@ -5,23 +5,41 @@ const ReactletTemplate = require("../templates/reactlet");
 import {render} from 'react-dom';
 import React from 'react';
 let anchorId = 1;
+let stack = [];
+let batchPending = false;
 
 module.exports = function(Component, props) {
   const id = `reactlet-${Component.name}-${anchorId}`;
   anchorId++;
-
-  setTimeout(() => { 
-    const el = document.getElementById(id);
-    if(!el) {
-      // The page rerendered multiple times before
-      // we got put into the dom.
-      // Jadelet gonna jadelet, it's ok.
-      return;
-    }
-    return render(
+  
+  stack.push[{
+    id: id,
+    render: (el) => { 
+      render(
       React.createElement(Component, props),
       el
     );
+  }}];
+  
+  if(batchPending) {
+    return;
+  }
+
+  batchPending = true;
+  setTimeout(() => { 
+    while(stack.length) {
+      const {id, render} = stack.pop();
+      const el = document.getElementById(id);
+      if(!el) {
+        // The page rerendered multiple times before
+        // we got put into the dom.
+        // Jadelet gonna jadelet, it's ok.
+        console.log("reactlet: skipping render.");
+        return;
+      }
+      render(el);
+    };
+    batchPending = false;
   });
 
   return ReactletTemplate({id: id});
