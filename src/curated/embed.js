42489db060a1e38a7f8db0923babbe5d6e04c0f4
Glitch (community) <none>
2018-04-16 09:04:45 +0000

    💕🎐 Checkpoint ./src/curated/embed.js:11/598 ./.glitch-assets:11/1295 ./src/curated/featured.js:11/929

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 module.exports =`
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 388px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/space-is-the-place?path=README.md&previewSize=100&sidebarCollapsed=true" alt="space-is-the-place on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 441px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/gravity-snakes?path=README.md&previewSize=100" alt="gravity-snakes on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
