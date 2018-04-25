5e4c3e04e0ed0f77b921d0d986bc7e3aa4d7f004
Glitch (future-riddle) <none>
2018-04-25 15:31:42 +0000

    🌋⛩ Checkpoint ./webpack.config.js:8/4

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = () => {
   let mode = 'development';
   if(process.env.NODE_ENV === 'production') {
     mode = 'production';
-    /*plugins = [
+    plugins = [
       new UglifyJsPlugin({
         cache: true,
         sourceMap: true,
@@ -29,7 +29,7 @@ module.exports = () => {
      new webpack.DefinePlugin({
        'process.env.NODE_ENV': JSON.stringify('production')
      })
-    ];*/
+    ];
   }
   
   console.log(`Starting Webpack in ${mode} mode.`);
