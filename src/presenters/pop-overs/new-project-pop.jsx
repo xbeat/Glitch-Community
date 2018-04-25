c4830dc008400136d24a2fc500feb157ed6bb620
Glitch (community-new-project) <none>
2018-04-25 18:09:07 +0000

    👟🚪 Checkpoint ./src/presenters/pop-overs/new-project-pop.jsx:2/2411 ./src/presenters/includes/project-result-list-item.jsx:2/1456 ./src/presenters/includes/project-result-item.jsx:2/508

--- src/presenters/pop-overs/new-project-pop.jsx
+++ src/presenters/pop-overs/new-project-pop.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-// import ProjectResultItem from 'includes/project-result-item'; // change to -result-list-item
+import ProjectResultItem from '../includes/project-result-item.jsx'; // change to -result-list-item
 // import Project from 'Project';
+// projct-result-item.jsx takes project (w title etc.) , returns html
+// replace new project with project result item
 
 const NewProject = ({title, domain, description, avatar}) => {
 
@@ -10,76 +12,17 @@ const NewProject = ({title, domain, description, avatar}) => {
       <p>{title}</p>
     </li>
   );
-  // - ProjectItem = require "./project-item"
-  // - NewProjectItemPresenter = require "../../source/presenters/pop-overs/new-project-item"
 
-  // function githubAuthLink() {
-  //   var clientId, redirectUri, scope;
-  //   clientId = GITHUB_CLIENT_ID;
-  //   scope = "user:email";
-  //   redirectUri = encodeURIComponent(`${APP_URL}/login/github`);
-  //   return `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`;
-  // }
-
-  // function facebookAuthLink() {
-  //   var callbackURL, clientId, scopes;
-  //   clientId = FACEBOOK_CLIENT_ID;
-  //   scopes = "email";
-  //   callbackURL = encodeURIComponent(`${APP_URL}/login/facebook`);
-  //   return "https://www.facebook.com/v2.9/dialog/oauth?" + `client_id=${clientId}&scope=${scopes}&redirect_uri=${callbackURL}`;
-  // }
-
-  // const SignInPopButton = (props) => (
-  //   <a className="button-link" href={props.href}>
-  //     <div className="button button-small">Sign in with {props.company}
-  //       <span className={`emoji ${props.emoji}`}></span>
-  //     </div>
-  //   </a>
-  // );
-
-}
-
-// const basepath = "https://glitch.com/edit";
-// const newProjects = [
-//   {
-//     domain: 'hello-express',
-//     // staticName: 'node-app',
-//     description: 'Create a Node app built on Express',
-//     staticAvatar: basepath + '/images/new-project-avatars/hello-express.svg',
-//     avatarUpdatedAt: new Date()
-//   },
-//   {
-//     domain: 'hello-sqlite',
-//     // staticName: 'node-sqlite',
-//     description: 'A Node app with an SQLite database to hold data.',
-//     staticAvatar: basepath + '/images/new-project-avatars/hello-sqlite.svg',
-//     avatarUpdatedAt: new Date()
-//   },
-//   {
-//     domain: 'hello-webpage',
-//     // staticName: 'webpage',
-//     description: 'Your very own web page',
-//     staticAvatar: basepath + '/images/new-project-avatars/hello-webpage.svg',
-//     avatarUpdatedAt: new Date()
-//   }
-// ]
+};
 
 
 const NewProjectPop = ({newProjects}) => (
   <div className="pop-over new-project-pop pop-list">
     <section className="pop-over-actions pop-list-results">
       <ul className="results">
-        {/* {console.log(title, domain, description, avatar)} */}
-        
-        {/*
-        // {console.log(props.projects)}   
-        // <NewProjects project={project} />
-
-        */}
         { newProjects.map((project, key) => (
           <NewProject key={key} {...project}/>
         ))}
-
       </ul>
     </section>
   </div>
