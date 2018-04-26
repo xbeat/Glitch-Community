ecf7bdbae3336e8ac72dcd65777e4c192ed16651
Glitch (community-new-project) <none>
2018-04-26 20:21:17 +0000

    🚅🔦 Checkpoint ./src/presenters/includes/project-result-item.jsx:2/796:8/193 ./package.json:8/2

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const Users = ({users}) => {
-  return (
-    <p>i am users</p> 
-  )
-}
+// const Users = ({users}) => {
+//   return (
+//     <p>i am users</p> 
+//   )
+// }
 
-const ProjectResultItem = ({title, domain, description, avatar, url, action, users=[]}) => {
-  return (
+
+const Users = ({users}) => (
+  <p>i am users</p>
+)
+
+const ProjectResultItem = ({
+    title, 
+    domain, 
+    description, 
+    avatar, 
+    url="javascript:void(0);", 
+    action, 
+    users=[]
+  }) => (
     <a href={url} onClick={action}>
       <li className="result">
         <img className="avatar" src={avatar} alt={`Project avatar for ${title}`}/>
-        
+
         { users.length > 0 && <Users/> }
 
         <div className="result-name" title={domain}>{domain}</div>
@@ -21,14 +33,13 @@ const ProjectResultItem = ({title, domain, description, avatar, url, action, use
       </li>
     </a>
   );
-};
 
 ProjectResultItem.propTypes = {
   title: PropTypes.string.isRequired, 
   domain: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
+  url: PropTypes.string,
   action: PropTypes.func.isRequired,
   users: PropTypes.array,
 };
