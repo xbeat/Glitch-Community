ad633d00b539c9d347d923d87c4ac1020e837894
Glitch (community-new-project) <none>
2018-04-26 14:36:39 +0000

    🚏💶 Checkpoint ./src/presenters/includes/project-result-item.jsx:2/134

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+const Users = ({users}) => {
+  if (users) {
+    <p>i am users</p>
+  }
+};
+
 const ProjectResultItem = ({title, domain, description, avatar, url, action}) => {
   return (
     <a href={url} onClick={action}>
       <li className="result">
         <img className="avatar" src={avatar} alt={`Project avatar for ${title}`}/>
+        {Users}
         <div className="result-name" title={domain}>{domain}</div>
         <div className="result-description">{description}</div>
       </li>
