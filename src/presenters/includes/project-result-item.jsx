2dddeea25d492bdbeab98aeb9ea66d09a0605caf
Glitch (community-new-project) <none>
2018-04-26 15:34:37 +0000

    🌪🐒 Checkpoint ./src/presenters/includes/project-result-item.jsx:2/541:8/1791

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const Users = ({users}) => {
@@ -14,7 +15,7 @@ const ProjectResultItem = ({title, domain, description, avatar, url, action, use
         <img className="avatar" src={avatar} alt={`Project avatar for ${title}`}/>
         <Users/>
         {console.log(users)}
-        { users && Users }
+        { users.length && <Users/> }
 
         <div className="result-name" title={domain}>{domain}</div>
         <div className="result-description">{description}</div>
@@ -22,5 +23,41 @@ const ProjectResultItem = ({title, domain, description, avatar, url, action, use
     </a>
   );
 };
+
+ProjectResultItem.propTypes = {
+   //each 'key' maps to a prop that you use here.
+   //if your only use of the prop is to pass it to a child, you can skip it.
+  // do i have to specify types on all params? should i?
+  //you don't _have to_, but it's a good idea, and it'll propagate up the entire call tree.
+  // so there should never be redundancy unless multiple parts of the chain depend on the same prop,
+  // in which case the duplicate prop types are good because they'll guard you in refactors.
+  // ok coolz
+  // so you mean i dont have to redefined the proptype for a child?
+  // cool
+  // i mean that proptypes should only be defined in the spot that they are used.
+  // so if you trust that your children do it, you don't have to because they'll catch it for you.
+  // ..but if you yourself use it, then you do.
+  title: PropTypes.string.isRequired, 
+  domain: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+  users: PropTypes.array,  //now it's optional,  but if it comes in then it has to be an array.
+  // can i evaluate it 
+  
+  // now here, decide if you want to require it or not.  if you want i
+  // users maybe should be optional. in some results lists it shouldn't be a consideration
+  // so i like to think about 'whose decision should that be?",
+  // parent/caller?
+  // we can make it required here and then the parent would just pass '[]' if it wants to,
+  // or we can make it optional here and the parent can leave it off - i like this approach for this
+    // ok cool, then we do this....
+     // make it optional in proptypes and give it a default <--
+  // both are fairly ok, it kind of has to do with how many parents you have :p
+  // my thinking is that parents should only be concerned with what they're gonna render, rather than the structure of a method - as much as possible anyways
+};
+
+
 
 export default ProjectResultItem;
