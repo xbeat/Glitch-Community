d0984aa47524626a123c62170f95f3ae22d6358f
Glitch (community-new-project) <none>
2018-04-26 15:45:17 +0000

    👻🎆 Checkpoint ./src/presenters/includes/project-result-item.jsx:2/220:8/309

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -8,13 +8,12 @@ const Users = ({users}) => {
   )
 }
 
-const ProjectResultItem = ({title, domain, description, avatar, url, action, users}) => {  
+const ProjectResultItem = ({title, domain, description, avatar, url, action, users=[]}) => {    //tada, now users will *ALWAYS* be an array. 
   return (
     <a href={url} onClick={action}>
       <li className="result">
         <img className="avatar" src={avatar} alt={`Project avatar for ${title}`}/>
         <Users/>
-        {console.log(users)}
         { users.length && <Users/> }
 
         <div className="result-name" title={domain}>{domain}</div>
@@ -25,8 +24,8 @@ const ProjectResultItem = ({title, domain, description, avatar, url, action, use
 };
 
 ProjectResultItem.propTypes = {
-   //each 'key' maps to a prop that you use here.
-   //if your only use of the prop is to pass it to a child, you can skip it.
+  //each 'key' maps to a prop that you use here.
+  //if your only use of the prop is to pass it to a child, you can skip it.
   // do i have to specify types on all params? should i?
   //you don't _have to_, but it's a good idea, and it'll propagate up the entire call tree.
   // so there should never be redundancy unless multiple parts of the chain depend on the same prop,
@@ -44,7 +43,14 @@ ProjectResultItem.propTypes = {
   url: PropTypes.string.isRequired,
   action: PropTypes.func.isRequired,
   users: PropTypes.array,  //now it's optional,  but if it comes in then it has to be an array.
-  // can i evaluate it 
+  // can i evaluate it if it's not included , like users.length won't work on undefined. un
+  // unless i set a default. cool 
+  // right, so always use a default.  watch:
+  
+  // ah fancy, does =[] replace the need for proptyles?
+  // nope, because that's just a default value if it's undefined.
+  // if somebody passes you a string, proptypes will shout it down for you.
+  // ahhh icic
   
   // now here, decide if you want to require it or not.  if you want i
   // users maybe should be optional. in some results lists it shouldn't be a consideration
@@ -52,8 +58,8 @@ ProjectResultItem.propTypes = {
   // parent/caller?
   // we can make it required here and then the parent would just pass '[]' if it wants to,
   // or we can make it optional here and the parent can leave it off - i like this approach for this
-    // ok cool, then we do this....
-     // make it optional in proptypes and give it a default <--
+  // ok cool, then we do this....
+  // make it optional in proptypes and give it a default <--
   // both are fairly ok, it kind of has to do with how many parents you have :p
   // my thinking is that parents should only be concerned with what they're gonna render, rather than the structure of a method - as much as possible anyways
 };
