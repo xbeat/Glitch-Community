b2479bfcd05d6c26cee9f598a35335c81f3a6d35
Glitch (community-new-project) <none>
2018-04-27 16:38:12 +0000

    🐄🌄 Checkpoint ./utils/glitch.js:2/1350

--- utils/glitch.js
+++ utils/glitch.js
@@ -1,5 +1,8 @@
-// a wrapper for making calls to the glitch api
+// 🚧🚧🚧
+
+// a wrapper for making calls to the glitch api (currently unused cuz i can't figure out global application scoping)
 // replaces application.api with something higher level
+// interface is designed for both community and FC users
 // eventually you should turn me into a real npm package
 // 👀 https://docs.google.com/spreadsheets/d/1T9u-NscZ2RlMFqrf-lEJ7MvKw-ckIehyLOkyyNrrBHo/edit#gid=785764015
 
@@ -17,8 +20,10 @@ if (process.env.RUNNING_ON === 'staging') {
   API_URL = 'https://api.glitch.com/';
 }
 
-// const Glitch = new axios
 // needs authorizationToken (const persistentToken = self.currentUser() && self.currentUser().persistentToken();)
+// call and construct with:
+// const glitch = new Glitch(authToken)
+// then simply call methods against it, everything returns a promise, with included catch
 
 function Glitch(authToken) {
   this.authToken = authToken
@@ -26,28 +31,28 @@ function Glitch(authToken) {
     baseURL: API_URL,
     cancelToken: (source != null ? source.token : undefined),
     headers: {
-      Authorization: authToken,
+      Authorization: authToken, // optional. If undefined, it'll fail only on calls where auth is required.
     },
   });
-
-}
-
-// ^ construct w glitch = new Glitch(authToken)
-
-const glitch = {
-  // glitch.users.byIds(ids)
+  
+  // API Calls
+  
   users: {
+    // glitch.users.byIds(ids)
     byIds: (ids) => {
-      ;
+      console.log('glitch.users.byIds(ids)')
+      // returns promise
+      // cat
     }
-  },
+  }
+  
   // glitch.user.id(id)
   user: (id) => {
-    ;
+    console.log('glitch.user.id(id)')
+    // returns promise
   }
 
-  
-  
 }
+
 
 module.exports = Glitch
