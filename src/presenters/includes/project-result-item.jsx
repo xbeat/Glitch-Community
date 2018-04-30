1e9ea31c87538c8d43cb5d56b4a5ba5c3ba37dbe
Glitch (community-new-project) <none>
2018-04-30 19:56:12 +0000

    🍨⛵️ Checkpoint ./src/presenters/includes/project-result-item.jsx:2/4265

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UsersList from '../users-list.jsx';
 
+const ResultItemDetails = () => {
+  return (
+    <li className="result">
+      <img className="avatar" src={avatar} alt={`Project avatar for ${title}`}/>
+      <div className="result-name" title={domain}>{domain}</div>
+      
+      { users.length > 0 && <UsersList users={users} /> }
+      <div className="result-description">{description}</div>
+    </li>
+  );
+};
+
 const ProjectResultItem = ({
   title, 
   domain, 
@@ -12,14 +24,8 @@ const ProjectResultItem = ({
   users=[]
 }) => (
   <a href={url} onClick={action}>
-    <li className="result">
-      <img className="avatar" src={avatar} alt={`Project avatar for ${title}`}/>
+    <ResultItemDetails />
 
-      { users.length > 0 && <UsersList users={users} /> }
-
-      <div className="result-name" title={domain}>{domain}</div>
-      <div className="result-description">{description}</div>
-    </li>
   </a>
 );
 
