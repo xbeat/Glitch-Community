09feb8dbf06c10cf47a73e219db61c2887390726
Glitch (future-riddle) <none>
2018-05-01 15:48:29 +0000

    👩‍👩‍👦🚣 Checkpoint ./src/presenters/category.js:97325/19

--- src/presenters/category.js
+++ src/presenters/category.js
@@ -43,3 +43,23 @@ export default function(application, category) {
 
   return CategoryTemplate(self);
 }
+
+export (application, category) => {
+  return (
+    <article className="projects" style={backgroundColor: category.backgroundColor()}>
+      <header className="category">
+        <
+        
+article.projects(@style)
+  header.category
+    a.category-name(href=@url)
+      h2= @name
+        span.arrow →
+    span.category-image-container
+      a.category-image(href=@url)
+        img(src=@category.avatarUrl alt=@name)
+    p.category-description= @description
+
+  = @projects()
+
+}
