a8443721db8719c1b735eeb1e9d946d5948eb1c5
Glitch (community) <none>
2018-05-03 12:58:21 +0000

    🍑💕 Checkpoint ./src/curated/featured.js:725013/530

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -15,16 +15,16 @@ export default [
   {
     title: '"awayboard" by voodoocode',
     img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fawayboard.jpg?1525351432822',
-    link: 'https://awayboard.glitch.me/'
+    link: 'https://awayboard.glitch.me'
   }
   , {
-    title: '"art-connoisseur" by oa495',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fart-connoisseur.jpg?1525087810179",
-    link: 'http://art-connoisseur.glitch.me'
+    title: '"quotebot" by becckitt',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fquotebot.jpg?1525351432926",
+    link: 'https://quotebot.glitch.me'
   }
   , {
-    title: '"Biscuit Tin-der" by thisisjofrank',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fbiscuit-tinder.jpg?1525087899535",
-    link: 'https://biscuittinder.glitch.me'
+    title: '"embed-bud" by jagtalon',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fembed-bud.jpg?1525351432697",
+    link: 'https://embed-bud.glitch.me'
   }   
 ];
