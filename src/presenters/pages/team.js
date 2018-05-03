4faa96057a178961cc22bc9aba6e4626ad1bd201
Glitch (community-new-project) <none>
2018-05-03 21:38:19 +0000

    👨‍👨‍👦📷 Checkpoint ./src/presenters/pages/team.js:2/56

--- src/presenters/pages/team.js
+++ src/presenters/pages/team.js
@@ -13,7 +13,6 @@ import Project from '../../models/project';
 import TeamTemplate from '../../templates/pages/team';
 import LayoutPresenter from '../layout';
 import AddTeamUserPopPresenter from '../pop-overs/add-team-user-pop';
-// import TeamUserPresenter from '../team-user-avatar';
 import AnalyticsPresenter from '../analytics';
 
 import Reactlet from "../reactlet";
