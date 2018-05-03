4c3b64c8707ba50473450550a97890c92d0d40b6
Glitch (community-new-project) <none>
2018-05-03 18:33:52 +0000

    🐡⛺️ Checkpoint ./src/presenters/includes/user-result-item.jsx:8/22 ./src/presenters/pop-overs/new-project-pop.jsx:8/305 ./src/presenters/includes/project-result-item.jsx:8/32

--- src/presenters/pop-overs/new-project-pop.jsx
+++ src/presenters/pop-overs/new-project-pop.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ProjectResultItem from '../includes/project-result-item.jsx';
 
 const NewProjectPop = ({newProjects}) => (
   <div className="pop-over new-project-pop">
     <section className="pop-over-actions results-list">
-      <ul className="results">
+      <div className="results">
         { newProjects.map((project) => (
           <a key={project.id} href={project.url}>
             <ProjectResultItem {...project}/>
           </a>
         ))}
-      </ul>
+      </div>
     </section>
   </div>
 );
+
+NewProjectPop.propTypes = {
+  newProjects: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.number.isRequired,
+  })).isRequired,
+};
 
 export default NewProjectPop;
