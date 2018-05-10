e9eb70d630f760ee1c8b30bcd02d5d8b746d095e
Glitch (community-new-project) <none>
2018-05-10 18:32:02 +0000

    🎩🎲 Checkpoint ./src/presenters/pop-overs/popover-container.jsx:8/1085

--- src/presenters/pop-overs/popover-container.jsx
+++ src/presenters/pop-overs/popover-container.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import onClickOutside from 'react-onclickoutside';
 
-const Popove
-
 /*
 A popover is a light, hollow roll made from an egg batter similar to
 that of Yorkshire pudding, typically baked in muffin tins or dedicated
@@ -11,6 +9,10 @@ popover pans, which have straight-walled sides rather than angled.
 
 ...also it's a [Bootstrap UI pattern](https://www.w3schools.com/bootstrap/bootstrap_popover.asp)
 */
+
+const PopoverContext = React.createContext();
+
+export const PopoverContextConsumer = PopoverContext.Consumer;
 
 const Wrapper = ({children}) => (
   <span>
@@ -25,6 +27,12 @@ export default class PopoverContainer extends React.Component {
 
     this.toggle = this.toggle.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
+    
+    const clickOutsideConfig = {
+      handleClickOutside: () => this.handleClickOutside,
+      excludeScrollbar: true,
+    };
+    this.MonitoredComponent = onClickOutside(Wrapper, clickOutsideConfig);
   }
   
   handleClickOutside(event) {
@@ -45,27 +53,21 @@ export default class PopoverContainer extends React.Component {
   render() {
     // Invoke the children as a react component, passing them the toggle visibility controls.
     // The <span> is needed because onClickOutside doesn't support React.Fragment
-    const Children = <this.props.children togglePopover={this.toggle} visible={this.state.visible}/>
+    //const Children = <this.props.children togglePopover={this.toggle} visible={this.state.visible}/>
     
     // The rest of this logic sets up and configures the onClickOutside wrapper
     // https://github.com/Pomax/react-onclickoutside
 
     // We do extra work with disableOnClickOutside and handleClickOutside
     // to prevent event bindings from being created until the popover is opened.
-    const clickOutsideConfig = {
-      handleClickOutside: () => this.handleClickOutside,
-      excludeScrollbar: true,
-    };
-    const MonitoredComponent = onClickOutside(Children, clickOutsideConfig);
+    
     
     return (
-      <Wrapper>
-        {this.props.children}
-      </Wrapper>
-    );
-    
-    return (
-      <MonitoredComponent disableOnClickOutside={!this.state.visible}  eventTypes={["mousedown", "touchstart", "keyup"]}/>
+      <this.MonitoredComponent disableOnClickOutside={!this.state.visible}  eventTypes={["mousedown", "touchstart", "keyup"]}>
+        <PopoverContext.Provider value={{visible: this.state.visible, togglePopover: this.toggle}}>
+          {this.props.children}
+        </PopoverContext.Provider>
+      </this.MonitoredComponent>
     );
   }
 }
