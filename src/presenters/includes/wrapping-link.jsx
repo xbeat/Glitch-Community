00648e80310da134fa9fce45088a6d212ce4937c
Glitch (superb-guide) <none>
2018-05-10 18:29:05 +0000

    🐁📌 Checkpoint ./src/presenters/includes/wrapping-link.jsx:97325/1110

--- src/presenters/includes/wrapping-link.jsx
+++ src/presenters/includes/wrapping-link.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// react tells me that nested <a> tags is bad and I should not do it
-// so instead we will put the <a> tags in a special onclick <div>
-// which will act like a li
+// react tells me that nested <a> tags are bad and I shouldn't do that
+// so we will replace the outer tags with a special on click <div> tag
+// which will act like a link (or as close as I can get via nice code)
 
 const handler = (evt) => {
+  // Real links and interactive elements take priority
   if (evt.target.closest('a[href], button, input')) return;
+  // Make sure there wasn't a clicky div inside this clicky div
   if (evt.target.closest('[data-href]') !== evt.currentTarget) return;
+  
+  // Ok, this click is real. Do the thing
   window.location.assign(evt.currentTarget.dataset.href);
   evt.preventDefault();
 };
