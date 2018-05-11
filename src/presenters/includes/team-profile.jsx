498a8543f94abd37324ff90d758cdb79337b619b
Glitch (community-new-project) <none>
2018-05-11 20:19:32 +0000

    🍐🏥 Checkpoint ./src/presenters/includes/team-profile.jsx:8/16519

--- src/presenters/includes/team-profile.jsx
+++ src/presenters/includes/team-profile.jsx
@@ -1,48 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const TeamProfile = ({teamProfileStyle,isTeamFetched}) => {
+const UserAvatarContainer = ({teamAvatarStyle,isCurrentUserOnTeam,uploadAvatar,teamName}) => {
+  return (
+    <div className="user-avatar-container">
+      <div className="user-avatar" style={teamAvatarStyle}>
+        { isCurrentUserOnTeam && (
+          <button className="button-small button-tertiary upload-avatar-button" onClick={uploadAvatar}>
+            Upload Avatar
+          </button>
+        )}
+      </div>
+      <div className="user-information">
+        <h1 className="username">{teamName}
+        </h1>
+      </div>
+    </div>
+  /*
+.user-avatar-container(class=@hiddenUnlessTeamFetched)
+  .user-information
+    h1.username= @teamName()
+      span(data-tooltip=@verifiedTeamTooltip)
+        img.verified(src=@verifiedImage class=@hiddenUnlessVerified)
+
+    .users-information
+      = @TeamUsers
+      = @addTeamUserButton
+
+    p.thanks(class=@hiddenUnlessTeamHasThanks)= @teamThanks()
+      span= " "
+      span.emoji.sparkling_heart
+
+    p.description.content-editable(class=@hiddenUnlessCurrentUserIsOnTeam keyup=@updateDescription blur=@applyDescription placeholder="Tell us about your team" contenteditable=true role="textbox" aria-multiline=true spellcheck=false)=@initialTeamDescription                
+    p.description.read-only(class=@hiddenIfCurrentUserIsOnTeam class=@hiddenIfNoDescription)= @description
+  */
+  );
+};
+
+const TeamProfile = ({teamProfileStyle,isTeamFetched, isCurrentUserOnTeam, ...props}) => {
   return (
     <section className="profile">
       <div className="profile-container" style={teamProfileStyle}>
         <div className="profile-info">
           { !isTeamFetched && <Loader/> }
+          
+          { isTeamFetched && <UserAvatarContainer {...props}/>}
         </div>
       </div>
+      {isCurrentUserOnTeam && (
+        <button className="button-small button-tertiary upload-cover-button" onClick={uploadCover}>
+          Upload Cover  
+        </button>
+      )}
     </section>
   )
 }
-
-/*
-  section.profile
-    .profile-container(style=@teamProfileStyle)
-      .profile-info
-        .loader(class=@hiddenIfTeamFetched)
-          span= Loader(@application)
-
-        .user-avatar-container(class=@hiddenUnlessTeamFetched)
-          .user-avatar(style=@teamAvatarStyle)
-            button.button-small.button-tertiary.upload-avatar-button(class=@hiddenUnlessCurrentUserIsOnTeam click=@uploadAvatar) Upload Avatar
-
-          .user-information
-            h1.username= @teamName()
-              span(data-tooltip=@verifiedTeamTooltip)
-                img.verified(src=@verifiedImage class=@hiddenUnlessVerified)
-
-            .users-information
-              = @TeamUsers
-              = @addTeamUserButton
-
-            p.thanks(class=@hiddenUnlessTeamHasThanks)= @teamThanks()
-              span= " "
-              span.emoji.sparkling_heart
-
-            p.description.content-editable(class=@hiddenUnlessCurrentUserIsOnTeam keyup=@updateDescription blur=@applyDescription placeholder="Tell us about your team" contenteditable=true role="textbox" aria-multiline=true spellcheck=false)=@initialTeamDescription                
-            p.description.read-only(class=@hiddenIfCurrentUserIsOnTeam class=@hiddenIfNoDescription)= @description
-
-    button.button-small.button-tertiary.upload-cover-button(class=@hiddenUnlessCurrentUserIsOnTeam click=@uploadCover) Upload Cover
-            
-*/
 
 export default TeamProfile;
 
