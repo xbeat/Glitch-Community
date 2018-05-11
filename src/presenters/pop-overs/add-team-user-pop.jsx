3ee0ebdd69d37a72c46a63d7817da19b8ef7be3f
Glitch (community-new-project) <none>
2018-05-11 18:52:58 +0000

    💰👵 Checkpoint ./src/presenters/pop-overs/add-team-user-pop.jsx:8/315:97325/1101

--- src/presenters/pop-overs/add-team-user-pop.jsx
+++ src/presenters/pop-overs/add-team-user-pop.jsx
@@ -101,12 +101,12 @@ dialog.pop-over.add-team-user-pop(class=@hiddenUnlessAddTeamUserPopVisible click
         = context.UserResultItem(user)
 */
 
-const UserSearchResults = ({users}) => (
+const UserSearchResults = ({users, action}) => (
   (users.length > 0) ? (
     <ul className="results">
       {users.map(user => (
         <li key={user.id}>
-          <UserResultItem user={user} />
+          <UserResultItem user={user} action={() => action(user.id)} />
         </li>
       ))}
     </ul>
@@ -115,14 +115,21 @@ const UserSearchResults = ({users}) => (
   )
 );
 
+UserSearchResults.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired).isRequired,
+  action: PropTypes.func.isRequired,
+};
+
 class AddTeamUserPop extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
       query: '', //The actual search text
-      request: null, //The active request promise
-      results: null, //Null means still waiting vs empty -- [jude: i suggest the 'maybe' convention for nullable fields with meaning.  'maybeResults']
+      maybeRequest: null, //The active request promise
+      maybeResults: null, //Null means still waiting vs empty -- [jude: i suggest the 'maybe' convention for nullable fields with meaning.  'maybeResults'] --greg: i like it
     };
     
     this.handleChange = this.handleChange.bind(this);
@@ -142,32 +149,37 @@ class AddTeamUserPop extends React.Component {
   
   clearSearch() {
     this.setState({
-      request: null,
-      results: null,
+      maybeRequest: null,
+      maybeResults: null,
     });
   }
   
-  // I wish you could use async!
-  async startSearch() {
+  // jude: I wish you could use async!  it won't work well. :-'( 
+  // for the moment only promises are supported, but we can chat obout if we want to target async/await
+  // the main problem is that Babel has to fake it, and it fakes it rather brutally
+  // :`(
+  // right! but we have es6 promises at least.
+  
+  startSearch() {
     if (!this.state.query) {
       return this.clearSearch();
     }
     
     const request = this.props.search(this.state.query);
-    this.setState({ request });
-    const results = await request;
+    this.setState({ maybeRequest: request });
     
-    this.setState(prevState => {
-      return (request === prevState.request) ? {
-        request: null,
-        results: results.map(user => user.asProps()),
-      } : {};
+    request.then(results => {
+      this.setState(({ maybeRequest }) => {
+        return (request === maybeRequest) ? {
+          maybeRequest: null,
+          maybeResults: results.slice(0, 5).map(user => user.asProps()),
+        } : {};
+      });
     });
   }
   
   render() {
-    const isLoading = !!(this.state.request || !this.state.results);
-    const hasResults = !!this.state.results;
+    const isLoading = (!!this.state.maybeRequest || !this.state.maybeResults);
     return (
       <dialog className="pop-over add-team-user-pop">
         <section className="pop-over-info">
@@ -179,7 +191,7 @@ class AddTeamUserPop extends React.Component {
         </section>
         {!!this.state.query && <section className="pop-over-actions last-section results-list">
           {isLoading && <Loader />}
-          {hasResults && <UserSearch}
+          {!!this.state.maybeResults && <UserSearchResults users={this.state.maybeResults} action={this.props.action} />}
         </section>}
       </dialog>
     );
@@ -188,6 +200,7 @@ class AddTeamUserPop extends React.Component {
 
 AddTeamUserPop.propTypes = {
   search: PropTypes.func.isRequired,
+  action: PropTypes.func.isRequired,
 };
 
 export default AddTeamUserPop;
