9f993df58c35b2e61ebaa0900fcf8446db3a91a8
Glitch (community-new-project) <none>
2018-05-15 21:20:43 +0000

    🛳👚 Checkpoint ./src/presenters/pop-overs/sign-in-pop.jsx:8/36

--- src/presenters/pop-overs/sign-in-pop.jsx
+++ src/presenters/pop-overs/sign-in-pop.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import PopoverContainer from './popover-container.jsx';
 /* global GITHUB_CLIENT_ID, FACEBOOK_CLIENT_ID, APP_URL */
 
