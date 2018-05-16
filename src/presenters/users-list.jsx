f606cb574133692f2f2aabbd2efda86ccb1aef3f
Glitch (community) <none>
2018-05-16 18:23:59 +0000

    😰🌏 Checkpoint ./src/presenters/users-list.jsx:8/526

--- src/presenters/users-list.jsx
+++ src/presenters/users-list.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 
 import PopoverContainer from './pop-overs/popover-container.jsx';
 
+function addDefaultSrc(event) {
+  const AnonAvatarUrl = "https://cdn.glitch.com/f6949da2-781d-4fd5-81e6-1fdd56350165%2Fanon-user-on-project-avatar.svg";
+  event.target.src = AnonAvatarUrl;
+}
+
 const UserAvatar = ({
   alt, 
   userAvatarUrl,
 }) => (
-  <img className="user-list-avatar" width="32px" height="32px" src={userAvatarUrl} alt={alt}/>
+  <img onError={addDefaultSrc} className="user-list-avatar" width="32px" height="32px" src={userAvatarUrl} alt={alt}/>
 );
 
 UserAvatar.propTypes = {
