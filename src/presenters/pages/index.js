82f12b2ce592f6e6772bb3e9dd3f2e52879edc3e
Glitch (future-riddle) <none>
2018-05-17 20:11:29 +0000

    Rewound to commit 16208e383cf25e78382d077e22385d6525b1b5cc + 0 edits

--- src/presenters/pages/index.js
+++ src/presenters/pages/index.js
@@ -6,14 +6,11 @@ import QuestionsPresenter from '../questions';
 import CategoryModel from '../../models/category';
 import Reactlet from '../reactlet';
 import EmbedHtml from '../../curated/embed';
-import Observable from 'o_0';
-import {sampleSize} from 'lodash';
 
 import Categories from "../categories.jsx";
 import RandomCategories from '../random-categories.jsx';
 import WhatIsGlitch from "../what-is-glitch.jsx";
 import ByFogCreek from "../includes/by-fogcreek.jsx";
-import Observed from "../includes/observed.jsx";
 
 export default function(application) {
   console.log("Presented index");
@@ -50,30 +47,12 @@ export default function(application) {
       return application.featuredCollections.map(collection => FeaturedCollectionPresenter(application, collection));
     },
     
-    randomCategoriesObservable: Observable([]),
-    
     randomCategories() {
-
-      const propsObservable = Observable(() => {
-        let categoryModels = self.randomCategoriesObservable().filter(model => !!model.projects.length);
-        categoryModels = sampleSize(categoryModels, 3);
-               
-        const categories = categoryModels.map(categoryModel => {
-          //touch all the projects to observe them
-          categoryModel.projects.forEach(({fetched}) => fetched());
-          
-          const {...category} = categoryModel.asProps();
-          category.projects = sampleSize(category.projects, 3);
-          return category;
-        });
-
-        return {
-          closeAllPopOvers: application.closeAllPopOvers,
-          categories,
-        };
-      });
-      console.log("YO!");
-      return Reactlet(Observed, {propsObservable, component:RandomCategories});
+      const props = {
+        closeAllPopOvers: application.closeAllPopOvers,
+        getCategories: () => CategoryModel.getRandomCategories(application.api()),
+      };
+      return Reactlet(RandomCategories, props);
     },
 
     embed() {
@@ -102,10 +81,6 @@ export default function(application) {
     },
 
   };
-  
-  CategoryModel.getRandomCategories(application.api()).then(
-    (categoryModels) => self.randomCategoriesObservable(categoryModels)
-  );
 
   const content = IndexTemplate(self);
 
--- src/presenters/random-categories.jsx
+++ src/presenters/random-categories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {ProjectsUL} from "./projects-list.jsx";
+import {sampleSize} from 'lodash';
 
 const Category = ({closeAllPopOvers, category}) => {
   const ulProps = {
@@ -37,19 +38,39 @@ Category.propTypes = {
   }).isRequired,
 };
 
-const Categories = ({categories, closeAllPopOvers}) => (
-  <React.Fragment>
-    { categories.map((category) => (
-      <Category key={category.id} category={category} closeAllPopOvers={closeAllPopOvers}/>
-    ))}
-  </React.Fragment>
-);
+export default class CategoryContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+  
+  componentDidMount() {
+    this.props.getCategories().then((categoryModels) => {
+      let models = categoryModels.filter(model => !!model.projects.length);
+      models = sampleSize(models, 3);
+      
+      const categories = models.map(categoryModel => {
+        const {...category} = categoryModel.asProps();
+        category.projects = sampleSize(category.projects, 3);
+        return category;
+      });
+      this.setState({categories});
+    });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        { this.state.categories.map((category) => (
+          <Category key={category.id} category={category} closeAllPopOvers={this.props.closeAllPopOvers}/>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
 
-Categories.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-  })).isRequired,
+CategoryContainer.propTypes = {
+  getCategories: PropTypes.func.isRequired,
   closeAllPopOvers: PropTypes.func.isRequired,
 };
-
-export default Categories;
