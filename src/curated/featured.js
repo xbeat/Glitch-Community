fc1800d07fdb377d3bd2985fffdce7feda5367e2
Glitch (community) <none>
2018-05-17 13:22:00 +0000

    🌆🍭 Checkpoint ./src/curated/featured.js:725013/352

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,18 +13,18 @@ Example:
 
 export default [
   {
-    title: 'Tic Tac Toe meets Reversi',
+    title: 'Tic Tac Toe Meets Reversi',
     img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ftic-tac-tic-tac.jpg?1526562567620',
     link: 'https://tic-tac-tic-tac.glitch.me/'
   }
   , {
-    title: 'xor by sjoerdvisscher',
+    title: 'fluffyshanks by bryanrieger',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffluffy-shanks.jpg?1526562567757",
     link: 'https://fluffy-shanks.glitch.me/'
   }
   , {
-    title: 'Try out the FaceApp API',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Ffaceapp-api.jpg?1526308089454",
-    link: 'https://faceapp-api.glitch.me'
+    title: 'Create a Ribbon for Your Website',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fribbon.jpg?1526562567948",
+    link: 'https://ribbon.glitch.me/'
   }   
 ];
