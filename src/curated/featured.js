9037996208ed0ce4f3708ae708e618eb97ec8822
Glitch (community-staging) <none>
2018-05-18 19:40:24 +0000

    🙇🐗 Checkpoint ./src/curated/featured.js:97325/2 ./src/presenters/users-list.jsx:97325/192

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -18,7 +18,7 @@ export default [
     link: 'https://tic-tac-tic-tac.glitch.me/'
   }
   , {
-    title: '\"fluffy-shanks\" by bryanrieger',
+    title: '"fluffy-shanks" by bryanrieger',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffluffy-shanks.jpg?1526562567757",
     link: 'https://fluffy-shanks.glitch.me/'
   }
