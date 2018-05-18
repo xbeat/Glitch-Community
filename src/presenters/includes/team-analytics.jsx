b6faf1eda501405041e6c4770b35eb2273ff1b84
Glitch (community-new-analytics) <none>
2018-05-18 15:57:03 +0000

    👅👘 Checkpoint ./package.json:2/3764 ./src/presenters/includes/team-analytics.jsx:2/368

--- src/presenters/includes/team-analytics.jsx
+++ src/presenters/includes/team-analytics.jsx
@@ -6,6 +6,10 @@ import Loader from './loader.jsx';
 import TeamAnalyticsTimePop from '../pop-overs/team-analytics-time-pop.jsx'
 import TeamAnalyticsProjectPop from '../pop-overs/team-analytics-project-pop.jsx'
 
+import("c3").then(c3 => {
+  console.log('c3 loaded', c3);
+})
+
 // 🗑: analytics.js, analytics.jade, analytics.styl, analytics-time-pop.jsx, analytics-project-pop.jsx, clean up team.js/jade
 
 // unused yet
