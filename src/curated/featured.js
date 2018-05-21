6dde64d1dce4c042e5629ac76a4a240c12d8581d
Glitch (community) <none>
2018-05-21 11:03:33 +0000

    🎽👞 Checkpoint ./.glitch-assets:725013/1294 ./src/curated/featured.js:725013/1072

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,18 +13,18 @@ Example:
 
 export default [
   {
-    title: 'Tic Tac Toe Meets Reversi',
-    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ftic-tac-tic-tac.jpg?1526562567620',
-    link: 'https://tic-tac-tic-tac.glitch.me/'
+    title: 'Make Art with an Etch A Sketch',
+    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fetch-a-sketch.jpg?1526900123654',
+    link: 'https://etch-a-sketch.glitch.me/'
   }
   , {
-    title: '"fluffy-shanks" by bryanrieger',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffluffy-shanks.jpg?1526562567757",
-    link: 'https://fluffy-shanks.glitch.me/'
+    title: 'Play a Random Album from Spotify',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fspotirand.jpg?1526900124445",
+    link: 'https://spotirand.glitch.me/'
   }
   , {
-    title: 'Create a Ribbon for Your Website',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fribbon.jpg?1526562567948",
-    link: 'https://ribbon.glitch.me/'
+    title: 'Create Microbit Pixel Designs',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fmicrobit-pixel-creator.jpg?1526900123996",
+    link: 'https://microbit-pixel-creator.glitch.me/'
   }   
 ];
