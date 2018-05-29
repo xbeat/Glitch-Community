7c60176957664588bcf64a1cb9a0820e006292c1
Glitch (community-new-analytics) <none>
2018-05-29 19:24:56 +0000

    🌌🍯 Checkpoint ./src/presenters/pop-overs/team-analytics-activity.jsx:2/354

--- src/presenters/pop-overs/team-analytics-activity.jsx
+++ src/presenters/pop-overs/team-analytics-activity.jsx
@@ -20,12 +20,13 @@ const chartColumns = (analytics) => {
 const renderC3 = (c3) => {
   var chart = c3.generate({
     size: {
-        height: 240,
+        height: 200,
     },
     data: {
         x: 'x',
+        xFormat: '%Y-%mm-%d',
         columns: [
-            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+            ['x', 1527580801000, '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
             ['Code Views', 30, 200, 100, 400, 150, 250],
             ['App Views', 130, 340, 200, 500, 250, 350]
         ]
@@ -34,9 +35,14 @@ const renderC3 = (c3) => {
         x: {
             type: 'timeseries',
             tick: {
-                format: '%Y-%m-%d'
+                format: '%Y-%mm-%d'
             }
-        }
+        },
+        y: {
+          tick: {
+            count: 8
+          }
+        },
     },
     // legend: {
     //   item: {
