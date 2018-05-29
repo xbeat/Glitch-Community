e6ec4a5df2bc95ae28bb8c5a3207a21d6317f3ca
Glitch (future-riddle) <none>
2018-05-29 18:03:33 +0000

    👟🐂 Checkpoint ./src/client.js:8/10965

--- src/client.js
+++ src/client.js
@@ -25,20 +25,105 @@ console.log("#########");
 
 // client-side routing:
 
-//
-// Redirects
-//
-if (document.location.hash.startsWith("#!/")) {
-  document.location = EDITOR_URL + document.location.hash;
-}
+function routePage(normalizedRoute, application) {
+  let indexPage, userPage;
+  const currentUserId = application.currentUser().id();
+  if (currentUserId) {
+    application.getCurrentUserById(currentUserId);
+  }
+  const user = application.currentUser();
+  if (application.currentUser().isSignedIn()) {
+    analytics.identify(user.id(), {
+      name: user.name(),
+      login: user.login(),
+      email: user.email(),
+      created_at: user.createdAt(),
+    }
+    );
+  }
+  
+    // index page ✅
+  if ((normalizedRoute === "index.html") || (normalizedRoute === "")) {
+    application.getQuestions();
+    return {page: IndexPage(application)};
+  }
 
-// lol wut
-if (normalizedRoute === 'wp-login.php') {
-  location.assign('https://www.youtube.com/embed/DLzxrzFCyOs?autoplay=1');
+  // questions page ✅
+  if (application.isQuestionsUrl(normalizedRoute)) {
+    return {page: QuestionsPage(application), title: "Questions"};
+  }
+
+  // ~project overlay page ✅
+  if (application.isProjectUrl(normalizedRoute)) {
+    const projectDomain = application.removeFirstCharacter(normalizedRoute);
+    application.showProjectOverlayPage(projectDomain);
+    return {page: IndexPage(application)};
+  }
+
+  // user page ✅
+  if (application.isUserProfileUrl(normalizedRoute)) {
+    application.pageIsUserPage(true);
+    const userLogin = normalizedRoute.substring(1, normalizedRoute.length);
+    const page = UserPage(application, userLogin);
+    application.getUserByLogin(userLogin);
+    return {page, title:decodeURI(normalizedRoute)};
+  }
+
+    // anon user page ✅
+  if (application.isAnonUserProfileUrl(normalizedRoute)) {
+    application.pageIsUserPage(true);
+    const userId = application.anonProfileIdFromUrl(normalizedRoute);
+    const page = UserPage(application, userId);
+    application.getUserById(userId);
+    return {page, title: normalizedRoute};
+  }
+
+    // team page ✅
+  if (application.isTeamUrl(normalizedRoute)) {
+    application.pageIsTeamPage(true);
+    const team = application.getCachedTeamByUrl(normalizedRoute);
+    const page = TeamPage(application);
+    application.getTeamById(team.id);
+    return {page, title: team.name};
+  }
+
+  // search page ✅
+  if (application.isSearchUrl(normalizedRoute, queryString)) {
+    const query = queryString.q;
+    application.searchQuery(query);
+    application.searchTeams(query);
+    application.searchUsers(query);
+    application.searchProjects(query);
+    const page = SearchPage(application);
+    return {page, title: `Search for ${query}`};
+  }
+
+    // category page ✅
+  if (application.isCategoryUrl(normalizedRoute)) {
+    application.getCategory(normalizedRoute);
+    const p = CategoryPage(application);
+    document.body.appendChild(categoryPage);
+    return document.title = application.category().name();    
+  }
+
+  // error page ✅
+  const errorPage = errorPageTemplate(application);
+  document.body.appendChild(errorPage);
+  return document.title = "👻 Page not found";
 }
 
 function route(document) {
+  //
+  // Redirects
+  //
+  if (document.location.hash.startsWith("#!/")) {
+    return document.location = EDITOR_URL + document.location.hash;
+  }
 
+  // lol wut
+  if (normalizedRoute === 'wp-login.php') {
+    return location.assign('https://www.youtube.com/embed/DLzxrzFCyOs?autoplay=1');
+  }
   
   //
   // OAuth Handling
@@ -59,105 +144,11 @@ function route(document) {
   //  Page Routing
   //
   
-  let indexPage, userPage;
-  const currentUserId = application.currentUser().id();
-  if (currentUserId) {
-    application.getCurrentUserById(currentUserId);
-  }
-  const user = application.currentUser();
-  if (application.currentUser().isSignedIn()) {
-    analytics.identify(user.id(), {
-      name: user.name(),
-      login: user.login(),
-      email: user.email(),
-      created_at: user.createdAt(),
-    }
-    );
-  }
-
-  // index page ✅
-  if ((normalizedRoute === "index.html") || (normalizedRoute === "")) {
-    application.getQuestions();
-    indexPage = IndexPage(application);
-    return document.body.appendChild(indexPage);
-  }
-
-  // questions page ✅
-  if (application.isQuestionsUrl(normalizedRoute)) {
-    const questionsPage = QuestionsPage(application);
-    document.body.appendChild(questionsPage);
-    // TODO append active projects count to document.title . i.e. Questions (12)
-    return document.title = "Questions";
-  }
-
-  // ~project overlay page ✅
-  if (application.isProjectUrl(normalizedRoute)) {
-    const projectDomain = application.removeFirstCharacter(normalizedRoute);
-    application.showProjectOverlayPage(projectDomain);
-    indexPage = IndexPage(application);
-    return document.body.appendChild(indexPage);
-  }
-
-  // user page ✅
-  if (application.isUserProfileUrl(normalizedRoute)) {
-    application.pageIsUserPage(true);
-    const userLogin = normalizedRoute.substring(1, normalizedRoute.length);
-    userPage = UserPage(application, userLogin);
-    application.getUserByLogin(userLogin);
-    document.body.appendChild(userPage);
-    return document.title = decodeURI(normalizedRoute);
-  }
-
-    // anon user page ✅
-  if (application.isAnonUserProfileUrl(normalizedRoute)) {
-    application.pageIsUserPage(true);
-    const userId = application.anonProfileIdFromUrl(normalizedRoute);
-    userPage = UserPage(application, userId);
-    application.getUserById(userId);
-    document.body.appendChild(userPage);
-    return document.title = normalizedRoute;
-  }
-
-    // team page ✅
-  if (application.isTeamUrl(normalizedRoute)) {
-    application.pageIsTeamPage(true);
-    const team = application.getCachedTeamByUrl(normalizedRoute);
-    const teamPage = TeamPage(application);
-    application.getTeamById(team.id);
-    document.body.appendChild(teamPage);
-    return document.title = team.name;
-  }
-
-  // search page ✅
-  if (application.isSearchUrl(normalizedRoute, queryString)) {
-    const query = queryString.q;
-    application.searchQuery(query);
-    application.searchTeams(query);
-    application.searchUsers(query);
-    application.searchProjects(query);
-    const searchPage = SearchPage(application);
-    document.body.appendChild(searchPage);
-    return document.title = `Search for ${query}`;
-  }
-
-    // category page ✅
-  if (application.isCategoryUrl(normalizedRoute)) {
-    application.getCategory(normalizedRoute);
-    const categoryPage = CategoryPage(application);
-    document.body.appendChild(categoryPage);
-    return document.title = application.category().name();    
-  }
-
-  // error page ✅
-  const errorPage = errorPageTemplate(application);
-  document.body.appendChild(errorPage);
-  return document.title = "👻 Page not found";
+  const {page, title=document.title} = routePage(normalizedRoute, application);
+  document.title = title;
+  document.body.appendChild(page);
 }
-const {redirect, content, title} = route(location);
-
-if(redirect) {
-  
-}
+route(document);
 
 document.addEventListener("click", event => globalclick(event));
 document.addEventListener("keyup", function(event) {
