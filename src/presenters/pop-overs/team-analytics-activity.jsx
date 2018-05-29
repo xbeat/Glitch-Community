e5251cf12a4594bba2398d4305beade5e72027ce
Glitch (community-new-analytics) <none>
2018-05-29 15:20:30 +0000

    👏🌠 Checkpoint ./src/presenters/pop-overs/team-analytics-activity.jsx:2/1654 ./public/styles/styles.styl:2/21 ./views/index.ejs:2/18

--- src/presenters/pop-overs/team-analytics-activity.jsx
+++ src/presenters/pop-overs/team-analytics-activity.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+let chartIsDrawn = false
+
 const  TeamAnalyticsActivity = ({c3, analytics}) => {
   console.log('🚧',c3, analytics)
-  return (
-    
-    <p>i am chart</p>
-  );
+  if (!chartIsDrawn) {
+    console.log ('rendering , once')
+    var chart = c3.generate({
+      size: {
+          height: 240,
+      },
+      data: {
+          x: 'x',
+          columns: [
+              ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+              ['code views', 30, 200, 100, 400, 150, 250],
+              ['app views', 130, 340, 200, 500, 250, 350]
+          ]
+      },
+      axis: {
+          x: {
+              type: 'timeseries',
+              tick: {
+                  format: '%Y-%m-%d'
+              }
+          }
+      },
+      legend: {
+        item: {
+          onclick: function (id) { console.log('legend toggled', id) }
+        }
+      }
+    });
+    chartIsDrawn = true
+
+    }
+    return (
+      <div id="chart" className="c3"/>
+    );
+
 };
 
 TeamAnalyticsActivity.propTypes = {
