d93cf9623a63ca39550a686e296b6aa2ebc04249
Glitch (superb-guide) <none>
2018-05-31 15:46:16 +0000

    ⛱😨 Checkpoint ./public/styles/overlays.styl:97325/82 ./public/styles/globals.styl:97325/124 ./src/presenters/includes/project-buttons.jsx:97325/1101

--- src/presenters/includes/project-buttons.jsx
+++ src/presenters/includes/project-buttons.jsx
@@ -1,24 +1,38 @@
+/* global EDITOR_URL */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const showIcon = "https://cdn.gomix.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Fshow-app.svg";
 
-export const Preview = ({name}) => (
+export const PreviewButton = ({name}) => (
   <a className="button button-link" href={`https://${name}.glitch.me`}>
     <img src={showIcon} alt=""/>
     Preview
   </a>
 );
-Preview.propTypes = {
+PreviewButton.propTypes = {
   name: PropTypes.string.isRequired,
 };
 
-export const Buttons = ({name}) => (
+export const EditorButton = ({name, isMember}) => (
+  <a className="button button-link" href={`${EDITOR_URL}#!/${name}`}>
+    {isMember ? 'Edit Project' : 'View Source'}
+  </a>
+);
+EditorButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  isMember: PropTypes.bool,
+};
+
+export const Buttons = ({name, isMember}) => (
   <React.Fragment>
-    <Preview name={name}/>
+    <PreviewButton name={name}/>
+    <EditorButton name={name} isMember={isMember}/>
   </React.Fragment>
 );
 Buttons.propTypes = {
   name: PropTypes.string.isRequired,
+  isMember: PropTypes.bool,
 };
 export default Buttons;
