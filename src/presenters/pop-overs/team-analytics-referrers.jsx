deedfd58805a302f5c48421047b1c96c906a8a57
Glitch (community-new-analytics) <none>
2018-05-31 22:04:28 +0000

    🍯🐇 Checkpoint ./src/presenters/pop-overs/team-analytics-referrers.jsx:2/1116

--- src/presenters/pop-overs/team-analytics-referrers.jsx
+++ src/presenters/pop-overs/team-analytics-referrers.jsx
@@ -13,32 +13,35 @@ const countTotals = (data, countProperty) => {
 
 const ReferrerItem = ({referrer, countProperty, data}) => {
   const total = countTotals(data, countProperty)
+  console.log ('total', countProperty, total)
   const count = referrer[countProperty]
-  const progress = Math.min(Math.round(count / total * 100), 5)
+  const progress = Math.max(Math.round(count / total * 100), 5)
   return (
     <li>
-      {referrer.domain}, {count}
+      {referrer.domain}, {count.toLocaleString('en')}
       <progress value={progress} max="100" />
     </li>
   )
 }
 
 const filterReferrers = (referrers) => {
-  console.log ('referrers', referrers)
   let filteredReferrers = referrers.filter(referrer =>
     !referrer.self
   )
   filteredReferrers = filteredReferrers.slice(0,5)
-  console.log ('filteredReferrers', filteredReferrers)
   return filteredReferrers
 }
 
 const TeamAnalyticsReferrers = ({analytics}) => {
+  console.log ('🌹 analytics.referrers', analytics.referrers)
   const appViewReferrers = filterReferrers(analytics.referrers)
   const remixReferrers = filterReferrers(analytics.remixReferrers)
   return (
       <article className="referrers-column">
-        <h4>App Views</h4>
+        <h4>
+          <div className="legend-item"/>
+          App Views
+        </h4>
         <ul>
           { appViewReferrers.map((referrer, key) => (
             <ReferrerItem
@@ -49,7 +52,10 @@ const TeamAnalyticsReferrers = ({analytics}) => {
             />
           ))}
         </ul>
-        <h4>App Views</h4>
+        <h4>
+          <div className="legend-item"/>
+          App Views
+        </h4>
         <ul>
           { remixReferrers.map((referrer, key) => (
             <ReferrerItem
