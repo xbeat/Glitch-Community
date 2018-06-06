913dbbf6110ea1a94538d8abf011ad949674022d
Glitch (community) <none>
2018-06-06 09:52:43 +0000

    🍎🐑 Checkpoint ./src/curated/featured.js:11/5

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -15,7 +15,7 @@ export default [
   {
     title: 'Bringing your code to Glitch',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch-github-social-coding.jpg?1528120414579",
-    link: 'https://help-center.glitch.me/github/'
+    link: 'https://help-center.glitch.me/help/github/'
   }
   , {
     title: 'Test Your Spy Skills with Slaccusation',
