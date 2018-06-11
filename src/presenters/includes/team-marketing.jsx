e7a74f7e152106ef81399cc6c19e590e39c4d821
Glitch (community-new-analytics) <none>
2018-06-11 15:02:26 +0000

    🚤🚔 Checkpoint ./src/presenters/includes/team-marketing.jsx:2/1131 ./public/styles/pages/team.styl:2/30

--- src/presenters/includes/team-marketing.jsx
+++ src/presenters/includes/team-marketing.jsx
@@ -7,9 +7,16 @@ const TeamMarketing = ({currentUserIsOnTeam}) => {
   }
   return (
     <section className="team-marketing">
-      <img className="for-platforms-icon" src="https://cdn.glitch.com/be1ad2d2-68ab-404a-82f4-6d8e98d28d93%2Ffor-platforms-icon.svg?1506442305188"></img>
-      <span>Want your own team page, complete with detailed app analytics?</span>
-      
+      <p>
+        <img className="for-platforms-icon" src="https://cdn.glitch.com/be1ad2d2-68ab-404a-82f4-6d8e98d28d93%2Ffor-platforms-icon.svg?1506442305188"></img>
+        <span>Want your own team page, complete with detailed app analytics?</span>
+      </p>
+      <a href="https://glitch.com/forteams">
+        <button className="button has-emoji">
+          About Teams 
+          <span className="emoji fishing_pole" role="img" aria-label="emoji" />
+        </button>
+      </a>
     </section>
     
   )
