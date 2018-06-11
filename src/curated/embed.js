4a4916bb5a696fe6f8ff1fa9868ee17f86bff00a
Glitch (community) <none>
2018-06-11 11:50:13 +0000

    🌵💈 Checkpoint ./src/curated/featured.js:11/113 ./src/curated/embed.js:11/568

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 450px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/beautiful-cubes?path=README.md&previewSize=100" alt="beautiful-cubes on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 433px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/texturedtext?path=README.md&previewSize=100" alt="texturedtext on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,17 +13,17 @@ Example:
 
 export default [
   {
-    title: 'Building with Actions - June 12 at 10am PT',
+    title: 'Build Actions: June 12, 10am PT',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FslackWebinarFeatureBanner.png?1519739395528",
     link: 'https://buildingwithactions.splashthat.com/blog-post'
   }
   , {
-    title: 'Master Machine Learning with tensorflow',
+    title: 'Master Machine Learning',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fhello-tensorflow.jpg?1528715708433",
-    link: 'https://hello-tensorflow.glitch.me/'
+    link: 'https://glitch.com/~hello-tensorflow'
   }
   , {
-    title: 'Build Your Schedule for World Cup 2018',
+    title: 'Get Ready for the World Cup',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fwc18.jpg?1528716860364",
     link: 'https://wc18.glitch.me/'
   }   
