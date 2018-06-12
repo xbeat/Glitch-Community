c22edd792889f2b19c2bde4197b647f98610097e
Glitch (community-new-analytics) <none>
2018-06-12 13:29:47 +0000

    ☕️🤘 Checkpoint ./src/presenters/includes/team-analytics-summary.jsx:2/382 ./public/styles/includes/team-analytics.styl:2/195

--- src/presenters/includes/team-analytics-summary.jsx
+++ src/presenters/includes/team-analytics-summary.jsx
@@ -8,43 +8,33 @@ const blurEvent = new CustomEvent('mouseout', {bubbles: true, cancelable: true})
 class TeamAnalyticsSummary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      AppViewsActivityHidden: false,
-      RemixesActivityHidden: false,
-    };
+    // this.state = {
+    // };
   }
   
   toggleGraph(summaryType) {
     let element = document.querySelector(`.c3-legend-item-${summaryType}`)
-
-    console.log ('🌹', `.c3-legend-item-${summaryType}`, element)
-    
     element.dispatchEvent(clickEvent)
-    element.blur()
     element.dispatchEvent(blurEvent)
-    // toggle this state summaryType
   }
 
-//   componentDidMount() {
-//   }
-  
   render() {
     return (
       <React.Fragment>
-        <span onClick={() => {this.toggleGraph('App-Views')}}>
+        <span className="summary-item" onClick={() => {this.toggleGraph('App-Views')}}>
           <span className="total app-views">
             {this.props.totalAppViews.toLocaleString('en')}
           </span>{' '}
-          <Pluralize singular="App View" plural="App Views" count={this.props.totalAppViews} showCount={false} />
+          <Pluralize className="summary-label" singular="App View" plural="App Views" count={this.props.totalAppViews} showCount={false} />
         </span>
         
         ,{' '}
 
-        <span onClick={() => {this.toggleGraph('Remixes')}}>
+        <span className="summary-item" onClick={() => {this.toggleGraph('Remixes')}}>
           <span className="total remixes">
             {this.props.totalRemixes.toLocaleString('en')}
           </span>{' '}
-          <Pluralize singular="Remix" plural="Remixes" count={this.props.totalRemixes} showCount={false} />
+          <Pluralize className="summary-label" singular="Remix" plural="Remixes" count={this.props.totalRemixes} showCount={false} />
         </span>
       </React.Fragment>
     );
