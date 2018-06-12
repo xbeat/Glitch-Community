54db3f245f02f5d79cd7cde3254434bc60e4c17a
Glitch (community-new-analytics) <none>
2018-06-12 13:19:35 +0000

    🍔🐒 Checkpoint ./src/presenters/includes/team-analytics-summary.jsx:2/341

--- src/presenters/includes/team-analytics-summary.jsx
+++ src/presenters/includes/team-analytics-summary.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Pluralize from 'react-pluralize';
 
 const clickEvent = new CustomEvent('click', {bubbles: true, cancelable: true})
-const blurEvent = new CustomEvent('blur', {bubbles: true, cancelable: true})
+const blurEvent = new CustomEvent('mouseout', {bubbles: true, cancelable: true})
 
 class TeamAnalyticsSummary extends React.Component {
   constructor(props) {
@@ -20,6 +20,7 @@ class TeamAnalyticsSummary extends React.Component {
     console.log ('🌹', `.c3-legend-item-${summaryType}`, element)
     
     element.dispatchEvent(clickEvent)
+    element.blur()
     element.dispatchEvent(blurEvent)
     // toggle this state summaryType
   }
@@ -28,17 +29,9 @@ class TeamAnalyticsSummary extends React.Component {
 //   }
   
   render() {
-    if(!this.props.currentUserIsOnTeam) {
-      return null;
-    }
-
     return (
       <React.Fragment>
-        <span 
-          className={ this.state.AppViewsActivityHidden ? null : 'disabled-summary-item' }
-          onClick={() => {
-            this.toggleGraph('App-Views') 
-        }}>
+        <span onClick={() => {this.toggleGraph('App-Views')}}>
           <span className="total app-views">
             {this.props.totalAppViews.toLocaleString('en')}
           </span>{' '}
@@ -47,10 +40,7 @@ class TeamAnalyticsSummary extends React.Component {
         
         ,{' '}
 
-        <span className="summary-remixes"
-          onClick={() => {
-            this.toggleGraph('Remixes')
-         }}>
+        <span onClick={() => {this.toggleGraph('Remixes')}}>
           <span className="total remixes">
             {this.props.totalRemixes.toLocaleString('en')}
           </span>{' '}
