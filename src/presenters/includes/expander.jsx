d1206e41114f08bc3ac65de6823651d866d20f68
Glitch (nova-chance) <none>
2018-06-13 20:45:35 +0000

    ⛰🙆 Checkpoint ./src/presenters/includes/expander.jsx:97325/22

--- src/presenters/includes/expander.jsx
+++ src/presenters/includes/expander.jsx
@@ -13,7 +13,7 @@ export default class Expander extends React.Component {
   
   componentDidMount() {
     // If the area is only slightly too big don't bother cutting it off
-    const maxHeight = this.props.height + 50;
+    const maxHeight = this.props.height + 60;
     console.log(this.ref.current.scrollHeight, maxHeight);
     if (this.ref.current.scrollHeight <= maxHeight) {
       this.setState({
