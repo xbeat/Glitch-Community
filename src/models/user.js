d39fa38d5029bfa10d1788d2b1582ad5622fcecf
Glitch (community-new-analytics) <none>
2018-06-14 18:34:08 +0000

    🏞🤒 Checkpoint ./src/application.js:97325/62 ./src/models/user.js:97325/79

--- src/models/user.js
+++ src/models/user.js
@@ -17,6 +17,7 @@ export default User = function(I, self) {
   if (I == null) { I = {}; }
   if (self == null) { self = Model(I); }
   if (cache[I.id]) {
+    console.log('cache user', I.name, cache[I.id].userAvatarUrl())
     return cache[I.id];
   }
   
