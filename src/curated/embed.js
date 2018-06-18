689c15c17ba97a841a3b0f0b6bcdb3ef5dd051de
Glitch (community) <none>
2018-06-18 13:19:54 +0000

    🎡👀 Checkpoint ./src/curated/embed.js:725013/2392

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
 <div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/de-stijl?path=README.md&previewSize=100&sidebarCollapsed=true" alt="de-stijl on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+  <iframe src="https://glitch.com/embed/#!/embed/texturedtext?path=README.md&previewSize=100&sidebarCollapsed=true" alt="texturedtext on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
