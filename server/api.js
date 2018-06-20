772bca60ad1b92b1749e39b2ed80aa2905df04d7
Glitch (nova-chance) <none>
2018-06-20 15:19:23 +0000

    🌴📻 Checkpoint ./server/routes.js:97325/20 ./server/api.js:97325/1029

--- server/api.js
+++ server/api.js
@@ -7,29 +7,31 @@ const moment = require("moment-mini");
 const {API_URL} = require("./constants");
 
 const NOT_FOUND = Symbol();
-const CACHE_TIMEOUT = moment.duration(10, 'minutes').asMilliseconds()
+const CACHE_TIMEOUT = moment.duration(15, 'minutes').asMilliseconds()
 
 const projectCache = new Cache();
 const userCache = new Cache();
 
-async function getProject(domain) {
-  let project = projectCache.get(domain);
-  if (project === null) {
-    const response = await axios.get(`${API_URL}/projects/${domain}`);
-    project = response.data ? response.data : NOT_FOUND;
-    projectCache.put(domain, project, 10000);
+async function getFromCacheOrApi(cache, id, api) {
+  let item = cache.get(id);
+  if (item === null) {
+    item = (await api(id)) || NOT_FOUND;
+    cache.put(id, item, CACHE_TIMEOUT);
   }
-  return project !== NOT_FOUND ? project : null;
+  return item !== NOT_FOUND ? item : null;
 }
 
-async function getUser(login) {
-  let user = userCache.get(login);
-  if (user === null) {
-    const response = await axios.get(`${API_URL}/users/byLogins?logins=${login}`);
-    user = response.data.length ? response.data[0] : NOT_FOUND;
-    userCache.put(login, user, 10000);
-  }
-  return user !== NOT_FOUND ? user : null;
+async function getProjectFromApi(domain) {
+  const response = await axios.get(`${API_URL}/projects/${domain}`);
+  return response.data;
 }
 
-module.exports = {getProject, getUser};
+async function getUserFromApi(login) {
+  const response = await axios.get(`${API_URL}/users/byLogins?logins=${login}`);
+  return response.data.length ? response.data[0] : null;
+}
+
+module.exports = {
+  getProject: domai,
+  getUser,
+};
