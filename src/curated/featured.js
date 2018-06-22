827d32ea6fd8a8d4cc2d736f07cc560c1a8c4ce1
Glitch (community) <none>
2018-06-22 11:11:31 +0000

    🕌🎱 Checkpoint ./src/curated/featured.js:725013/1245

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,9 +13,9 @@ Example:
 
 export default [
   {
-    title: 'Back Up Your Twitter Likes',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Ftwitter-likes-backup.jpg?1529320668955",
-    link: 'https://twitter-likes-backup.glitch.me/'
+    title: 'You Got This! Zine: React Edition',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FreactZinesStackFeatureBanner.png?1529489480468",
+    link: 'https://glitch.com/you-got-this/2'
   }
   , {
     title: 'Hello Kitty Calculator',
