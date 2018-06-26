33924c109768c490aafabe284036412825e114ae
Glitch (community) <none>
2018-06-26 09:01:39 +0000

    ⚓️🏦 Checkpoint ./.glitch-assets:11/456 ./src/curated/featured.js:11/691 ./webpack.config.js:11/2

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,18 +13,18 @@ Example:
 
 export default [
   {
-    title: 'Home Again, Home Again',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fhome-again.jpg?1529928234373",
-    link: 'https://glitch.com/~home-again'
-  }
-  , {
-    title: 'Generative Art Twitter Bot',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fgenerative-twitterbot.jpg?1529928234615",
-    link: 'https://glitch.com/~generative-twitterbot'
+    title: 'Create Art like Mondrian',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fde-stijl.jpg?1530003302894",
+    link: 'https://glitch.com/~de-stijl'
   }
   , {
     title: 'Track Congress\' Progress',
     img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fcongress-tracker-1.jpg?1529928449588",
     link: 'https://glitch.com/~congress-tracker'
-  }   
+  }
+  , {
+    title: 'Generative Art Twitter Bot',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fgenerative-twitterbot.jpg?1529928234615",
+    link: 'https://glitch.com/~generative-twitterbot'
+  }  
 ];
