89c4eae9d0f5f9291e40c033e989df090dc9df64
Glitch (community) <none>
2018-07-02 11:18:47 +0000

    ⛄️🍦 Checkpoint ./src/curated/featured.js:725013/352

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -23,8 +23,8 @@ export default [
     link: 'https://glitch.com/~8ball'
   }
   , {
-    title: 'Meet Friendly Tech & Design Pros',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fnewbietalk.jpg?1530183290324",
-    link: 'https://glitch.com/~newbietalk'
+    title: 'Beautiful Feathers On A Curve',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglobeards-feather-curve.jpg?1530529470277",
+    link: 'https://globeards.glitch.me/drawcurve.html'
   }  
 ];
