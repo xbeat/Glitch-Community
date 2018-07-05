e48735cc666038b032b40f7a7539b98278b06609
Glitch (community-pirijan) <none>
2018-07-05 14:42:13 +0000

    ⚓️🍛 Checkpoint ./src/presenters/notifications.jsx:2/156

--- src/presenters/notifications.jsx
+++ src/presenters/notifications.jsx
@@ -1,61 +1,63 @@
 // eventually replaces notifications.js, once all pages are react-ified
 
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-const NotificationContext = React.createContext();
+// const NotificationContext = React.createContext();
 
-export class NotificationProvider extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      isVisible: false,
-      message: 'yolooo',
-    };
-  };
 
-  show(message) {
-    this.setState({
-      message,
-      isVisible: true,
-    });
-  };
+// export class NotificationProvider extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-  hide() {
-    this.setState({
-      message: '',
-      isVisible: false,
-    });
-  };
+//     this.state = {
+//       isVisible: false,
+//       message: 'yolooo',
+//     };
+//   };
 
-  render() {
-    const { children } = this.props;
+//   show(message) {
+//     this.setState({
+//       message,
+//       isVisible: true,
+//     });
+//   };
 
-    return (
-      <NotificationContext.Provider
-        value={{
-          show: this.show,
-          hide: this.hide,
-          isVisible: this.state.isVisible,
-          message: this.state.message,
-        }}
-      >
-        <Notification />
-        {children}
-      </NotificationContext.Provider>
-    );
-  }
-}
+//   hide() {
+//     this.setState({
+//       message: '',
+//       isVisible: false,
+//     });
+//   };
 
-// export const NotificationConsumer = NotificationContext.Consumer;
-export const Notification = () => (
-  <NotificationContext.Consumer>
-    {({isVisible, message, hide}) => (
-      <Notification 
-        isVisible={isVisible}
-        message={message}
-      />
-    )}
-  </NotificationContext.Consumer>
-)
+//   render() {
+//     const { children } = this.props;
+
+//     return (
+//       <NotificationContext.Provider
+//         value={{
+//           show: this.show,
+//           hide: this.hide,
+//           isVisible: this.state.isVisible,
+//           message: this.state.message,
+//         }}
+//       >
+//         <Notification />
+//         {children}
+//       </NotificationContext.Provider>
+//     );
+//   }
+// }
+
+// // export const NotificationConsumer = NotificationContext.Consumer;
+// export const Notification = () => (
+//   <NotificationContext.Consumer>
+//     {({isVisible, message, hide}) => (
+//       <Notification 
+//         isVisible={isVisible}
+//         message={message}
+//       />
+//     )}
+//   </NotificationContext.Consumer>
+// )
