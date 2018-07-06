a19de781b8ad933c829f27a997ae5f2b2b5c2658
Glitch (community-pirijan) <none>
2018-07-06 12:40:08 +0000

    😹😬 Checkpoint ./src/presenters/pop-overs/create-team-pop.jsx:2/2401

--- src/presenters/pop-overs/create-team-pop.jsx
+++ src/presenters/pop-overs/create-team-pop.jsx
@@ -4,34 +4,48 @@ import PopoverContainer from './popover-container.jsx';
 
 // move to a seperate file
 // needs api
+// has to handle team name taken error case
 
-class AddTeamUserPop extends React.Component {
-    constructor(props) {
+class CreateTeamPop extends React.Component {
+  constructor(props) {
     super(props);
-    
-    this.state = {
-      teamName: ''
-    };
-    
-    this.handleChange = this.handleChange.bind(this);
-    this.clearSearch = this.clearSearch.bind(this);
-    this.startSearch = debounce(this.startSearch.bind(this), 300);
-    this.onClick = this.onClick.bind(this);
-  }
 
-const CreateTeamPop = () => {
-  return (
-    <dialog className="pop-over user-options-pop">
-      <section className="pop-over-actions">
-        <p>yoyoyoyo</p>
-      </section>
-    </dialog>
-  )
+    this.state = {
+      teamName: '',
+      teamUrl: '',
+    };
+  }
+  
+  render() {
+    return (
+      <dialog className="pop-over user-options-pop">
+        <section className="pop-over-info">
+          <div className="pop-title">
+            Create Team
+          </div>
+        </section>
+        <section className="pop-over-actions">
+          
+          <input className="pop-over-input" placeholder="Team name" value={this.state.teamName} />
+
+          <p className="action-description">
+            /{this.state.teamUrl}
+          </p>
+
+        </section>
+        <section className="pop-over-info">
+          <p className="info-description">
+            You can change this later
+          </p>
+        </section>      
+      </dialog>
+    )
+  }
 }
 
+
 // CreateTeamPop.propTypes = {
-//   api: PropTypes.string.isRequired,
-//   avatarStyle: PropTypes.object.isRequired,
+//   api: PropTypes.func.isRequired,
 // };
 
 export default CreateTeamPop;
