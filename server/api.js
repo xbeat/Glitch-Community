e21f5ecf725e5e3fd53ac0bec9b4c18ae063c032
Glitch (nova-chance) <none>
2018-07-10 19:50:04 +0000

    🌿📖 Checkpoint ./server/api.js:97325/477

--- server/api.js
+++ server/api.js
@@ -15,19 +15,28 @@ const userCache = new Cache();
 async function getFromCacheOrApi(id, cache, api) {
   let item = cache.get(id);
   if (item === null) {
-    item = (await api(id)) || NOT_FOUND;
-    cache.put(id, item, CACHE_TIMEOUT);
+    try {
+      item = (await api(id)) || NOT_FOUND;
+      cache.put(id, item, CACHE_TIMEOUT);
+    } catch (e) {
+      item = NOT_FOUND;
+    }
   }
   return item !== NOT_FOUND ? item : null;
 }
 
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 5000,
+});
+
 async function getProjectFromApi(domain) {
-  const response = await axios.get(`${API_URL}/projects/${domain}`);
+  const response = await api.get(`/projects/${domain}`);
   return response.data;
 }
 
 async function getUserFromApi(login) {
-  const response = await axios.get(`${API_URL}/users/byLogins?logins=${login}`);
+  const response = await api.get(`/users/byLogins?logins=${login}`);
   return response.data.length ? response.data[0] : null;
 }
 
