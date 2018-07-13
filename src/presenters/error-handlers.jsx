9080189fac4a0d49d9ecb91771c64ee9737a90f6
Glitch (nova-chance) <none>
2018-07-13 14:57:41 +0000

    💶💨 Checkpoint ./src/presenters/error-handlers.jsx:97325/2596 ./src/presenters/project-editor.jsx:97325/1100

--- src/presenters/error-handlers.jsx
+++ src/presenters/error-handlers.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {mapValues} from 'lodash';
+
+import Notifications from './notifications.jsx';
+
 function handleError(notify, error) {
   console.error(error);
   notify();
@@ -16,8 +20,40 @@ function handleErrorForInput(notify, error) {
   return Promise.reject();
 }
 
+function addCatch(func, handler) {
+  return (...args) => func(...args).catch(handler);
+}
+
+function addCatchToAll(obj, handler) {
+  return mapValues(obj, func => addCatch(func, handler));
+}
+
 const ErrorHandler = ({children, createErrorNotification}) => {
-  children({
-    handleError, handleErrorForInput,
-    handleErrorWrap: obj => 
+  const handleErrorBound = handleError.bind(null, createErrorNotification);
+  const handleErrorForInputBound = handleErrorForInput.bind(null, createErrorNotification);
+  return children({
+    handleError: error => handleErrorBound(error),
+    handleErrorForInput: error => handleErrorForInputBound(error),
+    addHandleError: obj => addCatchToAll(obj, handleErrorBound),
+    addHandleErrorForInput: obj => addCatchToAll(obj, handleErrorForInputBound),
+  });
 };
+ErrorHandler.propTypes = {
+  children: PropTypes.func.isRequired,
+  createErrorNotification: PropTypes.func.isRequired,
+};
+
+const ErrorHandlerContainer = ({children}) => (
+  <Notifications>
+    {notify => (
+      <ErrorHandler {...notify}>
+        {children}
+      </ErrorHandler>
+    )}
+  </Notifications>
+);
+ErrorHandlerContainer.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+export default ErrorHandlerContainer;
--- src/presenters/project-editor.jsx
+++ src/presenters/project-editor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Notifications from './notifications.jsx';
+import ErrorHandlers from './error-handlers.jsx';
 
 class ProjectEditor extends React.Component {
   constructor(props) {
@@ -16,52 +16,40 @@ class ProjectEditor extends React.Component {
     return this.state.users.some(({id}) => currentUserId === id);
   }
   
-  handleError(error) {
-    console.error(error);
-    this.props.createErrorNotification();
-    return Promise.reject(error);
-  }
-
-  handleErrorForInput(error) {
-    if (error && error.response && error.response.data) {
-      return Promise.reject(error.response.data.message);
-    }
-    this.props.createErrorNotification();
-    return Promise.reject();
-  }
-  
   async updateFields(changes) {
     await this.props.api.patch(`projects/${this.state.id}`, changes);
     this.setState(changes);
   }
   
   render() {
-    const handleError = this.handleError.bind(this);
-    const handleErrorForInput = this.handleErrorForInput.bind(this);
-    const funcs = {
-      updateDomain: domain => this.updateFields({domain}).catch(handleErrorForInput),
-      updateDescription: description => this.updateFields({description}).catch(handleError),
-      updatePrivate: isPrivate => this.updateFields({private: isPrivate}).catch(handleError),
-    };
-    return this.props.children(this.state, funcs, this.userIsMember());
+    const inputFuncs = this.props.addHandleErrorForInput({
+      updateDomain: domain => this.updateFields({domain}),
+    });
+    const funcs = this.props.addHandleError({
+      updateDomain: domain => this.updateFields({domain}),
+      updateDescription: description => this.updateFields({description}),
+      updatePrivate: isPrivate => this.updateFields({private: isPrivate}),
+    });
+    return this.props.children(this.state, {...inputFuncs, ...funcs}, this.userIsMember());
   }
 }
 ProjectEditor.propTypes = {
+  addHandleError: PropTypes.func.isRequired,
+  addHandleErrorForInput: PropTypes.func.isRequired,
   api: PropTypes.any.isRequired,
   children: PropTypes.func.isRequired,
-  createErrorNotification: PropTypes.func.isRequired,
   currentUserModel: PropTypes.object.isRequired,
   initialProject: PropTypes.object.isRequired,
 };
 
 const ProjectEditorContainer = ({api, children, currentUserModel, initialProject}) => (
-  <Notifications>
-    {notifyFuncs => (
-      <ProjectEditor api={api} currentUserModel={currentUserModel} initialProject={initialProject} {...notifyFuncs}>
+  <ErrorHandlers>
+    {wrapErrors => (
+      <ProjectEditor api={api} currentUserModel={currentUserModel} initialProject={initialProject} {...wrapErrors}>
         {children}
       </ProjectEditor>
     )}
-  </Notifications>
+  </ErrorHandlers>
 );
 ProjectEditorContainer.propTypes = {
   api: PropTypes.any.isRequired,
