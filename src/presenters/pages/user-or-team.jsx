ed59e10389d27b322815df5e4ec5396adae20aad
Glitch (superb-guide) <none>
2018-07-16 20:57:53 +0000

    🍔💺 Checkpoint ./src/presenters/pages/user-or-team.jsx:97325/453

--- src/presenters/pages/user-or-team.jsx
+++ src/presenters/pages/user-or-team.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {DataLoader} from '../includes/loader.jsx';
+import NotFound from '../includes/not-found.jsx';
+
+import UserPage from './user.jsx';
+import TeamPage from './team.jsx';
 
 const getUser = async (api, name) => {
   const {data: id} = await api.get(`userId/byLogin/${name}`);
@@ -21,7 +25,17 @@ const UserOrTeamPage = ({api, name}) => (
     ) : (
       <DataLoader get={() => getUser(api, name)}>
         {user => user ? (
-          <UserPage ap
+          <UserPage api={api} user={user}/>
+        ) : (
+          <NotFound name={name}/>
+        )}
+      </DataLoader>
     )}
   </DataLoader>
 );
+UserOrTeamPage.propTypes = {
+  api: PropTypes.any.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default UserOrTeamPage;
