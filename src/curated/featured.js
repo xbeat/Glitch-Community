e96c0a957007f8dc22331a68dc95657e48a07557
Glitch (community) <none>
2018-07-19 11:41:20 +0000

    🍟🌵 Checkpoint ./.glitch-assets:725013/1301 ./src/curated/embed.js:725013/1292 ./src/curated/featured.js:725013/949

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,18 +13,18 @@ Example:
 
 export default [
   {
-    title: 'Random Acts of Kindness',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Frandom-acts-of-kindness-generator.jpg?1531740318993",
-    link: 'https://glitch.com/~random-acts-of-kindness-generator'
+    title: 'Guess The Right Color',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fcolor-guessing-game.jpg?1531999961697",
+    link: 'https://glitch.com/~color-guessing-game'
   }
   , {
-    title: 'Learn While You Road Trip',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Froadtrip.jpg?1531740320479",
-    link: 'https://glitch.com/~roadtrip'
+    title: 'Corgis On Demand',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fcorgi-button.jpg?1531999962386",
+    link: 'https://glitch.com/~corgi-button'
   }
   , {
-    title: 'Track Recent Earthquakes',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fusgs-earthquakes.jpg?1531740319665",
-    link: 'https://glitch.com/~usgs-earthquakes'
+    title: 'Create Reveal.js Presentations',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Freveal-glitch.jpg?1531999963191",
+    link: 'https://glitch.com/~reveal-glitch'
   }  
 ];
