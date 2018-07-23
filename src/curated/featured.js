de6bba2b78de6d276e81e803d9174c2325d99dd8
Glitch (community) <none>
2018-07-23 10:15:42 +0000

    ⚾️🚃 Checkpoint ./src/curated/featured.js:11/583 ./.glitch-assets:11/420

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -18,13 +18,13 @@ export default [
     link: 'https://glitch.com/~countdown-oclock'
   }
   , {
-    title: 'Corgis On Demand',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fcorgi-button.jpg?1531999962386",
-    link: 'https://glitch.com/~corgi-button'
+    title: 'Create Slick Beats',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ftenori-off.jpg?1532340207849",
+    link: 'https://glitch.com/~tenori-off'
   }
   , {
-    title: 'Create Reveal.js Presentations',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Freveal-glitch.jpg?1531999963191",
-    link: 'https://glitch.com/~reveal-glitch'
+    title: 'From Slack to Things ✅',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fthings-cmd.jpg?1532340416668",
+    link: 'https://glitch.com/edit/#!/things-cmd'
   }  
 ];
