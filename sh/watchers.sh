8646c1cf5c10fcbc342eb80070cfe9b06cc24e9b
Glitch (thundering-way) <none>
2018-07-25 18:14:09 +0000

    🏝🚁 Checkpoint ./sh/watchers.sh:8/1 ./package.json:8/4505 ./webpack.config.js:8/2641

--- sh/watchers.sh
+++ sh/watchers.sh
@@ -2,7 +2,7 @@
 set -e
 
 nodemon --exec "bash sh/compile-jade.sh" --watch src/templates --ext .jade &
-bash sh/stylus.sh &
+#bash sh/stylus.sh &
 bash sh/webpack.sh &
 # ESLint the server files:
 nodemon --exec "eslint --config server/.eslintrc.server.js server --cache" --watch server &
