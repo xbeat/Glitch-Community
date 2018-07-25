a6a79fa62c44b589c371d8881d5c2980ed8ebe47
Glitch (thundering-way) <none>
2018-07-25 18:35:38 +0000

    🚂🌕 Checkpoint ./sh/compile-stylus.sh:8/956

--- sh/compile-stylus.sh
+++ sh/compile-stylus.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 # Stylus --watch has two problems that we're working around:
-# 1. it t Stylus tends to crash, so run it in a loop so that it'll come back to life on failure.
+# 1. it tends to crash
+# 2. while the build it underway, in mangles the output file
+#
+# To fix this, we watch .styl with nodemon and call this build script,
+# and then after a successful build, we move our compliation to the output.
 
-echo "Starting stylus watcher"
+echo "Starting Stylus Build"
 
-while true; do
-  stylus \
-    --watch \
-    --use autoprefixer-stylus \
-    --sourcemap \
-    --compress public/styles/styles.styl \
-    --out public/styles.css
-  
-  echo "Stylus crashed; Restarting stylus --watch"
-done
+# move to app folder so this script is dependant on the location of the caller
+cd ~/
+
+# Build it!
+stylus \
+  --use autoprefixer-stylus \
+  --sourcemap \
+  --compress styles/styles.styl \
+  --out styles/styles.css.tmp
+
+stylus_status = $?
+# If it built, update styles.css
+if [ $stylus_status -eq 0 ]; then
+  mv styles/styles.css.tmp public/styles.css
+  echo "Styles updated."
+else
+  echo "Stylus build failed."
+fi
