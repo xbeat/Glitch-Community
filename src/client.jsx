c0bc686d62c608e8d52e3c52d9bf4c367a3ccce4
Glitch (nova-chance) <none>
2018-08-07 17:59:52 +0000

    ⏰📫 Checkpoint ./src/client.jsx:97325/647 ./src/models/user.js:97325/2452 ./src/models/model.js:97325/4 ./package.json:97325/27

--- src/client.jsx
+++ src/client.jsx
@@ -12,38 +12,32 @@ import {Notifications} from './presenters/notifications.jsx';
 
 import Router from './presenters/pages/router.jsx';
 
-
-// client-side routing:
-
-async function route(location) {
-  const normalizedRoute = location.pathname.replace(/^\/|\/$/g, "");
-  console.log(`normalizedRoute is ${normalizedRoute}`);
-
-  //
-  // Redirects
-  //
-  if (location.hash.startsWith("#!/")) {
-    window.location = EDITOR_URL + window.location.hash;
-    return;
-  }
-  
-  //
-  //  Page Routing
-  //
-  const dom = document.createElement('div');
-  document.body.appendChild(dom);
-  const App = () => (
-    <BrowserRouter>
-      <Notifications>
-        <UserPrefsProvider>
-          <CurrentUserProvider>
-            {api => <Router api={api}/>}
-          </CurrentUserProvider>
-        </UserPrefsProvider>
-      </Notifications>
-    </BrowserRouter>
-  );
-  render(<App/>, dom);
+//
+// Redirects
+//
+if (location.hash.startsWith("#!/")) {
+  window.location.replace(EDITOR_URL + window.location.hash);
 }
 
-route(window.location);
+//
+// Client side react
+//
+const App = () => (
+  <BrowserRouter>
+    <Notifications>
+      <UserPrefsProvider>
+        <CurrentUserProvider>
+          {api => <Router api={api}/>}
+        </CurrentUserProvider>
+      </UserPrefsProvider>
+    </Notifications>
+  </BrowserRouter>
+);
+
+
+//
+// Init the app
+//
+const dom = document.createElement('div');
+document.body.appendChild(dom);
+render(<App/>, dom);
--- src/models/user.js
+++ src/models/user.js
@@ -1,86 +1,19 @@
 /* globals CDN_URL */
-
-let User;
-
-import Model from './model';
-const cache = {};
 const cacheBuster = Math.floor(Math.random() * 1000);
 
 export const ANON_AVATAR_URL = "https://cdn.glitch.com/f6949da2-781d-4fd5-81e6-1fdd56350165%2Fanon-user-on-project-avatar.svg?1488556279399";
 
-export default User = function(I, self) {
-
-  if (I == null) { I = {}; }
-  if (self == null) { self = Model(I); }
-  if (cache[I.id]) {
-    return cache[I.id];
-  }
-  
-  self.defaults(I, {
-    id: undefined,
-    avatarUrl: undefined,
-    avatarThumbnailUrl: undefined,
-    color: undefined,
-    hasCoverImage: false,
-    coverColor: "#1F33D9",
-    login: null,
-    name: null,
-    description: "",
-    projects: undefined,
-    teams: [],
-    thanksCount: 0,
-    fetched: false,
-    persistentToken: null,
-    pins: [],
-  });
-
-  self.attrObservable(...Array.from(Object.keys(I) || []));
-  self.attrModels('projects', Project);
-
-  self.extend({
-    asProps() {
-      return {
-        get teams() { return self.teams.filter(({asProps}) => !!asProps).map(({asProps}) => asProps()); },
-        get projects() { return self.projects.filter(({asProps}) => !!asProps).map(({asProps}) => asProps()); },
-        get pins() { return self.pins(); },
-
-        avatarUrl: self.avatarUrl(),
-        avatarThumbnailUrl: self.avatarThumbnailUrl(),
-        color: self.color(),
-        coverColor: self.coverColor(),
-        description: self.description(),
-        hasCoverImage: self.hasCoverImage(),
-        id: self.id(),
-        login: self.login(),
-        name: self.name(),
-        thanksCount: self.thanksCount(),
-      };
-    },
-  });
-
-
-  if (I.id) {
-    cache[I.id] = self;
-  }
-  // console.log '☎️ user cache', cache
-
-  return self;
-};
-
-User.getUserById = function(application, id) {
-  const userPath = `users/${id}`;
-  const promise = new Promise((resolve, reject) => {
-    return application.api().get(userPath)
-      .then(({data}) => resolve(data)).catch(function(error) {
-        console.error(`getUserById GET ${userPath}`, error);
-        return reject();
-      });
-  });
-  return promise;
-};
-
-
-User._cache = cache;
+export default function User({projects, teams, ...user}) {
+  const props = {
+    get projects() { return projects ? projects.map(project => Project(project).asProps()) : []; },
+    get teams() { return teams ? teams.map(team => Team(team).asProps()) : []; },
+    ...user
+  };
+  return {
+    update: user => User(user),
+    asProps: () => props,
+  };
+}
 
 export function getDisplayName({login, name}) {
   if (name) {
@@ -130,3 +63,4 @@ export function getProfileStyle({id, hasCoverImage, coverColor, cache=cacheBuste
 
 // Circular dependencies must go below module.exports
 import Project from './project';
+import Team from './team';
