41a61e03aef97b61ea3d94410b165f41139d3604
Glitch (community-staging) <none>
2018-08-08 21:20:12 +0000

    🎢🎳 Checkpoint ./src/curated/featured.js:11/7

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,17 +13,17 @@ Example:
 
 export default [
   {
-    title: 'Marlow Moss\'s Minimalist Art',
+    title: 'Marlow Moss\' Minimalist Art',
     img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fmarlow-moss.jpg?1533554478016",
     link: 'http://marlow-moss.glitch.me/'
   }
   , {
-    title: 'Arithmetic With Inches and Feet',
+    title: 'Arithmetic with Inches and Feet',
     img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fshopcalc.jpg?1533554478824",
     link: 'https://shopcalc.glitch.me/'
   } 
   , {
-    title: 'Interact With A 3D Book',
+    title: 'Interact with a 3D Book',
     img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fview-3d.jpg?1533554476536",
     link: 'https://glitch.com/~view-3d'
   }  
