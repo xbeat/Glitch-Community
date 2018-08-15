27808046883679c532aa51e4d7025e9914db4ee3
Glitch (superb-guide) <none>
2018-08-15 14:47:45 +0000

    💳🎮 Checkpoint ./src/presenters/includes/team-elements.jsx:97325/1901

--- src/presenters/includes/team-elements.jsx
+++ src/presenters/includes/team-elements.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import axios from 'axios';
+import {
 
 export const TeamMarketing = () => {
   const forPlatformsIcon = 'https://cdn.glitch.com/be1ad2d2-68ab-404a-82f4-6d8e98d28d93%2Ffor-platforms-icon.svg?1506442305188';
@@ -35,23 +36,28 @@ export class WhitelistedDomainIcon extends React.Component {
   constructor(props) {
     super(props);
     this.state = {src: null};
+    this.load = throttle(() => this.load(), 100);
   }
   
   async load() {
-    let data = {domain: this.props.domain, icons: []
+    let data = {
+      domain: this.props.domain,
+      icons: []
+    };
     try {
       // do not use the normal api here, we don't want to send our auth around
-      {data} = await axios.get(`https://favicongrabber.com/api/grab/${this.props.domain}`);
+      ({data} = await axios.get(`https://favicongrabber.com/api/grab/${this.props.domain}`));
     } catch (error) {
-      console.error(error);
+      // The api returns an error when the domain doesn't exist, so ignore it
+      console.log(error);
     }
-      if (data.domain === this.props.domain) {
-        if (data.icons.length) {
-          this.setState({src: data.icons[0].src});
-        } else {
-          this.setState({src: null});
-        }
+    if (data.domain === this.props.domain) {
+      if (data.icons.length) {
+        this.setState({src: data.icons[0].src});
+      } else {
+        this.setState({src: null});
       }
+    }
   }
   
   componentDidMount() {
