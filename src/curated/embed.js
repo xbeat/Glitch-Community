e5344498cafa10b891ae705244ca7a205a9086be
Glitch (community) <none>
2018-08-16 13:49:01 +0000

    Rewound to commit 076cad666c258124407a0b8d027889931a958b69 + 0 edits

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/canvas-sketches?path=scripts/createLoop.js&previewSize=100" alt="canvas-sketches on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 540px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/swanky-bridge?path=README.md&previewSize=100" alt="swanky-bridge on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
