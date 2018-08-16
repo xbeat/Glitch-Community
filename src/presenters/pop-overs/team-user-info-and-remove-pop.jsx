c067ad52f2926e4de96efa0d6531e9519f2c746c
Glitch (remove-team-users) <none>
2018-08-16 20:06:30 +0000

    💐🐉 Checkpoint ./src/presenters/pop-overs/team-user-info-and-remove-pop.jsx:2/3857

--- src/presenters/pop-overs/team-user-info-and-remove-pop.jsx
+++ src/presenters/pop-overs/team-user-info-and-remove-pop.jsx
@@ -9,9 +9,9 @@ const ADMIN_ACCESS_LEVEL = 30;
 
 // Remove from Team 👋
 
-const RemoveFromTeam = ({toggleRemoveTeamUserVisible}) => (
+const RemoveFromTeam = ({toggleUserInfoVisible}) => (
   <section className="pop-over-actions danger-zone">
-    <button className="button-small has-emoji button-tertiary button-on-secondary-background" onClick={toggleRemoveTeamUserVisible}>
+    <button className="button-small has-emoji button-tertiary button-on-secondary-background" onClick={toggleUserInfoVisible}>
       Remove from Team
       <span className="emoji wave" />
     </button>
@@ -19,7 +19,7 @@ const RemoveFromTeam = ({toggleRemoveTeamUserVisible}) => (
 );
 
 RemoveFromTeam.propTypes = {
-  toggleRemoveTeamUserVisible: PropTypes.func.isRequired,
+  toggleUserInfoVisible: PropTypes.func.isRequired,
 };
 
 
@@ -102,75 +102,98 @@ const ThanksCount = ({count}) => (
 );
 
 
-const TeamUserInfo = (props, toggleRemoveTeamUserVisible) => {
-  const userAvatarStyle = {backgroundColor: props.user.color};
+// Team User Info 😍
 
-  return(
-          <dialog className="pop-over team-user-info-pop">
-            <section className="pop-over-info">
-              <a href={getLink(props.user)}>
-                <img className="avatar" src={getAvatarThumbnailUrl(props.user)} alt={props.user.login} style={userAvatarStyle}/>
-              </a>
-              <div className="info-container">
-                <p className="name" title={props.user.name}>{props.user.name || "Anonymous"}</p>
-                { props.user.login &&
-                  <p className="user-login" title={props.user.login}>@{props.user.login}</p>
-                }
-                { props.userIsTeamAdmin && 
-                  <div className="status-badge">
-                    <span className="status admin" data-tooltip="Can edit team info and billing">
-                      Team Admin
-                    </span>
-                  </div>
-                }
-              </div>
-            </section>
-            { props.user.thanksCount > 0 && <ThanksCount count={props.user.thanksCount} /> }
-            <UserActions user={props.user} />
-            { props.currentUserIsTeamAdmin &&
-              <AdminActions 
-                user={props.user}
-                userIsTeamAdmin={props.userIsTeamAdmin}
-                updateUserPermissions={props.updateUserPermissions}
-              />
-            }
-            { props.currentUserIsTeamAdmin && <RemoveFromTeam toggleRemoveTeamUserVisible={toggleRemoveTeamUserVisible} /> }
-          </dialog>
-)
+const TeamUserInfo = (props, toggleUserInfoVisible) => {
+  const userAvatarStyle = {backgroundColor: props.user.color};
+  return (
+    <dialog className="pop-over team-user-info-pop">
+      <section className="pop-over-info">
+        <a href={getLink(props.user)}>
+          <img className="avatar" src={getAvatarThumbnailUrl(props.user)} alt={props.user.login} style={userAvatarStyle}/>
+        </a>
+        <div className="info-container">
+          <p className="name" title={props.user.name}>{props.user.name || "Anonymous"}</p>
+          { props.user.login &&
+            <p className="user-login" title={props.user.login}>@{props.user.login}</p>
+          }
+          { props.userIsTeamAdmin && 
+            <div className="status-badge">
+              <span className="status admin" data-tooltip="Can edit team info and billing">
+                Team Admin
+              </span>
+            </div>
+          }
+        </div>
+      </section>
+      { props.user.thanksCount > 0 && <ThanksCount count={props.user.thanksCount} /> }
+      <UserActions user={props.user} />
+      { props.currentUserIsTeamAdmin &&
+        <AdminActions 
+          user={props.user}
+          userIsTeamAdmin={props.userIsTeamAdmin}
+          updateUserPermissions={props.updateUserPermissions}
+        />
+      }
+      { props.currentUserIsTeamAdmin && <RemoveFromTeam toggleUserInfoVisible={toggleUserInfoVisible} /> }
+    </dialog>
+  )
+}
+
+TeamUserInfo.propTypes = {
+  toggleUserInfoVisible: PropTypes.func.isRequired,
 }
 
 
+// Team User Remove 💣
 
-// Team User Info
+const TeamUserRemove = (props, toggleUserInfoVisible) => {
+  return (
+    <p>yolooo</p>
+  )
+}
+
+TeamUserRemove.propTypes = {
+  toggleUserInfoVisible: PropTypes.func.isRequired,
+}
+
+
+// Team User Info or Remove
+// uses removeTeamUserVisible state to toggle between showing user info and remove views
 
 export default class TeamUserInfoAndRemovePop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      removeTeamUserVisible: false
+      userInfoVisible: true
     };
-    this.removeFromTeam = this.removeFromTeam.bind(this);
-    this.toggleRemoveTeamUserVisible = this.toggleRemoveTeamUserVisible.bind(this);
+    // this.removeFromTeam = this.removeFromTeam.bind(this);
+    this.toggleUserInfoVisible = this.toggleUserInfoVisible.bind(this);
   }
   
-  removeFromTeam() {
-    this.props.togglePopover();
-    this.props.removeUserFromTeam(this.props.user.id);
-  }
+  // removeFromTeam() {
+  //   this.props.togglePopover();
+  //   this.props.removeUserFromTeam(this.props.user.id);
+  // }
   
-  toggleRemoveTeamUserVisible(prevState) {
+  toggleUserInfoVisible(prevState) {
     this.setState({
-      removeTeamUserVisible: !prevState.removeTeamUserVisible
+      userInfoVisible: !prevState.userInfoVisible
     })
   }
   
   render() {
     return (
       <React.Fragment>
-        { !this.state.removeTeamUserVisible &&
+        { this.state.userInfoVisible &&
           <TeamUserInfo
             {...this.props}
-            toggleRemoveTeamUserVisible = {() => this.toggleRemoveTeamUserVisible()}
+            toggleUserInfoVisible={() => this.toggleUserInfoVisible()}
+          />
+        ||
+          <TeamUserRemove 
+            {...this.props}
+            toggleUserInfoVisible={() => this.toggleUserInfoVisible()}
           />
          }
      </React.Fragment>
