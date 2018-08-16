076cad666c258124407a0b8d027889931a958b69
Glitch (community) <none>
2018-08-16 11:35:03 +0000

    🚂🌽 Checkpoint ./src/curated/featured.js:725013/937 ./src/curated/embed.js:725013/604

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/take-a-walk?path=README.md&previewSize=100&previewFirst=true&sidebarCollapsed=true" alt="take-a-walk on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 540px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/swanky-bridge?path=README.md&previewSize=100" alt="swanky-bridge on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
