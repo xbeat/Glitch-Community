1061e1c62b036be37953f75a181020e5c82ec6bf
Glitch (nova-chance) <none>
2018-08-16 15:37:54 +0000

    🌾💺 Checkpoint ./server/cache.js:97325/524

--- server/cache.js
+++ server/cache.js
@@ -7,26 +7,29 @@ const {API_URL} = require('./constants');
 
 const fs_writeFile = util.promisify(fs.writeFile);
 
-const cache = null;
+const categories = Promise.resolve('[]');
 
 const getCategories = async () => {
   let response = await axios.get('categories', {
     baseUrl: API_URL,
     transformResponse: (data) => data // Don't bother parsing the JSON
   });
+  console.log("☂️ categories updated");
   return response.data;
 };
 
 const updateCaches = async () => {
-  await updateCache('categories');
-  console.log("☂️ cache updated");
+  // Wait until the catgegories are loaded then instantly swap
+  // Otherwise every ten minutes requests would stall waiting on the api
+  const newCategories = await getCategories();
+  categories = Promise.resolve(newCategories);
 };
 
 
 const initCache = () => {
   const interval = moment.duration(10, 'minutes').asMilliseconds();
   setInterval(updateCaches, interval);
-  updateCaches();
+  categories = getCategories();
 };
 
 const getCache = () => {
