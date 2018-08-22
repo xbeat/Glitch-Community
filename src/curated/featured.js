ae9d278e51fc142b5c8b7e7667c832b758943bb0
Glitch (community) <none>
2018-08-22 10:32:47 +0000

    🐃⛸ Checkpoint ./.glitch-assets:11/415 ./src/curated/featured.js:11/680

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,13 +13,13 @@ Example:
 
 export default [
   {
-    title: 'Learn American Sign Language',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fasl-spell.jpg?1534767236249",
-    link: 'https://glitch.com/~asl-spell'
-  }, {
     title: 'Generate A List of Peoples\' Names',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Frandom-people.jpg?1534767237598",
     link: 'https://glitch.com/~random-people'
+  },{
+    title: 'Empower Black Voters #Remix4Change',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FcocBanner.png?1534933464755",
+    link: 'https://about.glitch.me/remix4change'
   }, {
     title: 'Play A Game of Sneaky Epic',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fsneaky-epic.jpg?1534767236910",
