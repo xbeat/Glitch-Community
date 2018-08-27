eb622492350a29121f9e75d40d28cdf017122852
Glitch (aquatic-thyme) <none>
2018-08-27 17:25:34 +0000

    🎟⛩ Checkpoint ./src/presenters/pages/secret.jsx:8/651

--- src/presenters/pages/secret.jsx
+++ src/presenters/pages/secret.jsx
@@ -4,16 +4,29 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Layout from '../layout.jsx';
 
-const SecretPageContainer = ({api}) => (
-  <Secret toggles={{fish: true, cakes: false, fishcakes: false}}></Secret>
-);
+const SecretPageContainer = ({api}) => {
+  const toggles = [
+    {name: "fish", description: "Whether or not fish.", enabled: true},
+    {name: "cakes", description: "Whether or not cakes.", enabled: false},
+    {name: "fishcakes", description: "opinions on if it's a cake or not", enabled: true},
+  ];
+  
+  
+  return (
+    <Secret toggles={toggles}></Secret>
+  );
+};
 
 const Secret = ({toggles}) => {
   
   return (
     <section style={{backgroundColor: "black"}}>
       
-      
+      <ul>
+        {toggles.map((toggle) => (
+          
+        ))}
+      </ul>
       
       
     </section>
