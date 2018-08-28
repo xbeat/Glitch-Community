2550c6c284516d260c18b5eed7ed2ec3adc261fb
Glitch (humorous-spaghetti) <none>
2018-08-28 20:48:02 +0000

    🕹🖼 Checkpoint ./src/presenters/entity-page-collections.jsx:867163/2341 ./src/presenters/pages/user.jsx:867163/157

--- src/presenters/entity-page-collections.jsx
+++ src/presenters/entity-page-collections.jsx
@@ -16,9 +16,42 @@ const EntityPageCollections = ({collections}) => {
   //   };
   // });
   
-  collections; // define some dummy collections here
+  // define a dummy collection
   collections = {
-    
+    "teams": [],
+    "users": [],
+    "id": "c185d921-3271-4182-bf6c-6cba2f40f693",
+    "inviteToken": "ea280688-a5e1-4c40-bfd7-a4df77b56f36",
+    "description": "My First Collection",
+    "name": "my-first-collection",
+    "domain": "my-first-collection",
+    "baseId": "2bdfb3f8-05ef-4035-a06e-2043962a3a13",
+    "private": false,
+    "createdAt": "2018-08-28T14:38:33.919Z",
+    "updatedAt": "2018-08-28T20:18:25.605Z",
+    "deletedAt": null,
+    "lastAccess": "2018-08-28T20:18:36.280Z",
+    "avatarUpdatedAt": "2018-08-28T14:38:34.387Z",    
+    "showAsGlitchTeam": false,
+    "projectPermission": {
+      "userId": 867163,
+      "projectId": "c185d921-3271-4182-bf6c-6cba2f40f693",
+      "accessLevel": 30,
+      "createdAt": "2018-08-28T14:38:33.932Z",
+      "updatedAt": "2018-08-28T14:38:33.932Z"
+    },
+    "projectActions": [
+      {
+        "id": 9554811,
+        "projectId": "c185d921-3271-4182-bf6c-6cba2f40f693",
+        "userId": 867163,
+        "action": "editor.access",
+        "data": "edit",
+        "createdAt": "2018-08-28T20:16:32.896Z",
+        "updatedAt": "2018-08-28T20:16:32.896Z"
+      }
+    ]
+}
 
   return (
     <React.Fragment>
--- src/presenters/pages/user.jsx
+++ src/presenters/pages/user.jsx
@@ -86,7 +86,11 @@ const UserPage = ({
       </ProfileContainer>
     </section>
     {/* Dummy section for collections */}
-    
+    {/*
+    <EntityPageCollections
+      collections={collections}
+    />
+    */}
     <EntityPageProjects
       projects={projects} 
       pins={pins} 
