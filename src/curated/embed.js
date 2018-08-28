122fc30636b693dd06e3ff243924cc3ad492bbc7
Glitch (community) <none>
2018-08-28 12:49:49 +0000

    📚🦃 Checkpoint ./src/curated/embed.js:11/586

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 520px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/css-js-clock?path=README.md&previewSize=100&previewFirst=true&sidebarCollapsed=true" alt="css-js-clock on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 549px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/nasa?path=README.md&previewSize=100" alt="nasa on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
