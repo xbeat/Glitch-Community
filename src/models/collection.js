74061279921213facd46305018e45af518c05e5a
Glitch (humorous-spaghetti) <none>
2018-08-28 19:23:49 +0000

    ⚽️💼 Checkpoint ./src/presenters/collection-item.jsx:867163/1959 ./src/models/collection.js:867163/1216

---
+++ src/models/collection.js
@@ -0,0 +1,42 @@
+/* global CDN_URL EDITOR_URL*/
+
+export const FALLBACK_AVATAR_URL = "https://cdn.glitch.com/c53fd895-ee00-4295-b111-7e024967a033%2Ffallback-project-avatar.svg?1528812220123";
+
+export default function Collection({teams, users, ...project}) {
+  const props = {
+    get teams() { return teams ? teams.map(team => Team(team).asProps()) : []; },
+    get users() { return users ? users.map(user => User(user).asProps()) : []; },
+    ...project
+  };
+  return {
+    update: data => Project(data),
+    asProps: () => props,
+  };
+}
+
+export function getAvatarUrl(id) {
+  return `${CDN_URL}/project-avatar/${id}.png`;
+}
+
+export function getLink(domain) {
+  return `/~${domain}`;
+}
+
+export function getShowUrl(domain) {
+  return `//${domain}.glitch.me`;
+}
+
+export function getEditorUrl(domain, path, line, character) {
+  if (path && !isNaN(line) && !isNaN(character)) {
+    return `${EDITOR_URL}#!/${domain}?path=${path}:${line}:${character}`;
+  }
+  return `${EDITOR_URL}#!/${domain}`;
+}
+
+export function getRemixUrl(domain) {
+  return `${EDITOR_URL}#!/remix/${domain}`;
+}
+
+// Circular dependencies must go below module.exports
+import Team from './team';
+import User from './user';
---
+++ src/presenters/collection-item.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {getAvatarUrl, getLink} from '../models/project.js';
+
+import {TruncatedMarkdown} from './includes/markdown.jsx';
+import ProjectOptionsContainer from "./pop-overs/project-options-pop.jsx";
+import UsersList from "./users-list.jsx";
+
+export const ProjectItem = ({project, categoryColor, projectOptions}) => {
+  return (
+    <li>
+      <UsersList glitchTeam={project.showAsGlitchTeam} users={project.users} extraClass="single-line"/>
+      <ProjectOptionsContainer project={project} projectOptions={projectOptions}></ProjectOptionsContainer>
+
+      <a href={getLink(project.domain)}>
+        <div className={['project', project.private ? 'private-project' : ''].join(' ')} 
+          style={{backgroundColor: categoryColor, borderBottomColor:categoryColor}}
+          data-track="project" data-track-label={project.domain}>
+          <div className="project-container">
+            <img className="avatar" src={getAvatarUrl(project.id)} alt={`${project.domain} avatar`}/>
+            <div className="button">
+              <span className="project-badge private-project-badge" aria-label="private"></span>
+              <div className="project-name">{project.domain}</div>
+            </div>
+            <div className="description"><TruncatedMarkdown length={96}>{project.description}</TruncatedMarkdown></div>
+            <div className="overflow-mask" style={{backgroundColor: categoryColor}}></div>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+};
+
+ProjectItem.propTypes = {
+  project: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    domain: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    private: PropTypes.bool.isRequired,
+    showAsGlitchTeam: PropTypes.bool.isRequired,
+    users: PropTypes.array.isRequired,
+  }).isRequired,
+  categoryColor: PropTypes.string,
+  projectOptions: PropTypes.object,
+};
+
+
+export default ProjectItem;
