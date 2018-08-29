00bdee4d3478f5def3f0cb816360d998393844f0
Glitch (nova-chance) <none>
2018-08-29 20:57:41 +0000

    🛋😜 Checkpoint ./src/client.jsx:97325/369

--- src/client.jsx
+++ src/client.jsx
@@ -7,11 +7,23 @@ import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './app.jsx';
 
-console.log('asdf')
-if (location.hash.startsWith("#!/")) {
-  window.location.replace(EDITOR_URL + window.location.hash);
-} else {
-  const dom = document.createElement('div');
-  document.body.appendChild(dom);
-  render(<BrowserRouter><App/></BrowserRouter>, dom);
+var passed = false;
+try {
+  const x = {a: 1, b: 2}; //Can we use const?
+  let y = [1, 2, 3]; //Can we use let?
+  var {a, ...b} = x; //Can we use object destructuring?
+  
+  passed = true;
+} catch (error) {
+  passed = false;
+}
+
+if (passed) {
+  if (location.hash.startsWith("#!/")) {
+    window.location.replace(EDITOR_URL + window.location.hash);
+  } else {
+    const dom = document.createElement('div');
+    document.body.appendChild(dom);
+    render(<BrowserRouter><App/></BrowserRouter>, dom);
+  }
 }
