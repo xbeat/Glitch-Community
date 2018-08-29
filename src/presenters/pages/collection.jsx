d4d4bdfbb5de743c40d2d2fe8c1da2eb23bf4357
Glitch (humorous-spaghetti) <none>
2018-08-29 17:57:22 +0000

    🌏🍌 Checkpoint ./src/presenters/pages/collection.jsx:867163/2718 ./src/presenters/random-categories.jsx:867163/32

---
+++ src/presenters/pages/collection.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Helmet from 'react-helmet';
+import Layout from '../layout.jsx';
+import ProjectModel from '../../models/project';
+
+import Loader, {DataLoader} from '../includes/loader.jsx';
+import {ProjectsUL} from '../projects-list.jsx';
+import ProjectsLoader from '../projects-loader.jsx';
+import Categories from '../categories.jsx';
+
+const CategoryPageWrap = ({category, children}) => (
+  <React.Fragment>
+    <Helmet>
+      <title>{category.name}</title>
+    </Helmet>
+    <main className="category-page hello-world">
+      <article className="projects" style={{backgroundColor: category.backgroundColor}}>
+        <header className="category">
+          <h2 className="category-name">{category.name}</h2>
+          <span className="category-image-container">
+            <img src={category.avatarUrl} alt=""/>
+          </span>
+          <p className="description">{category.description}</p>
+        </header>
+        {children}
+      </article>
+    </main>
+    <Categories/>
+  </React.Fragment>
+);
+CategoryPageWrap.propTypes = {
+  category: PropTypes.shape({
+    avatarUrl: PropTypes.string.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const CategoryPageLoader = ({...props}) => (
+  <CategoryPageWrap {...props}>
+    <Loader/>
+  </CategoryPageWrap>
+);
+
+const CategoryPageError = ({...props}) => (
+  <CategoryPageWrap {...props}>
+    Something went wrong. Try refreshing?
+  </CategoryPageWrap>
+);
+
+async function loadCategory(api, id) {
+  const {data} = await api.get(`categories/${id}`);
+  data.projects = data.projects.map(project => ProjectModel(project).update(project).asProps());
+  return data;
+}
+
+const CategoryPage = ({api, category, ...props}) => (
+  <DataLoader
+    get={() => loadCategory(api, category.id)}
+    renderLoader={() => <CategoryPageLoader category={category} {...props}/>}
+    renderError={() => <CategoryPageError category={category} {...props}/>}
+  >
+    {category => (
+      <CategoryPageWrap category={category} {...props}>
+        <ProjectsLoader api={api} projects={category.projects}>
+          {projects => <ProjectsUL projects={projects} categoryColor={category.color}/>}
+        </ProjectsLoader>
+      </CategoryPageWrap>
+    )}
+  </DataLoader>
+);
+CategoryPage.propTypes = {
+  api: PropTypes.any.isRequired,
+  category: PropTypes.object.isRequired,
+};
+
+const CategoryPageContainer = ({api, category}) => (
+  <Layout api={api}>
+    <CategoryPage api={api} category={category}/>
+  </Layout>
+);
+
+export default CategoryPageContainer;
