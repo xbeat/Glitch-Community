e4b544727b0cfc2612b30b0fee3a0123643ff262
Glitch (humorous-spaghetti) <none>
2018-08-30 15:38:54 +0000

    🌏💀 Checkpoint ./src/presenters/pop-overs/add-collection-project-pop.jsx:867163/11943 ./src/presenters/includes/project-result-item.jsx:867163/1227

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -1,33 +1,69 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import {getAvatarUrl} from  '../../models/project';
+// import UsersList from '../users-list.jsx';
+
+// const ProjectResultItem = ({id, domain, description, users, action, isActive}) => {
+//   var resultClass = "button-unstyled result ";
+//   if(isActive) {
+//     resultClass += " active";
+//   }
+
+//   return (
+//     <button className={resultClass} onClick={action}>
+//       <img className="avatar" src={getAvatarUrl(id)} alt={`Project avatar for ${domain}`}/>
+//       <div className="result-name" title={domain}>{domain}</div>
+      
+//       { description.length > 0 && <div className="result-description">{description}</div> }
+//       { users.length > 0 && <UsersList users={users} /> }
+//     </button>
+//   );
+// };
+
+// ProjectResultItem.propTypes = {
+//   action: PropTypes.func.isRequired,
+//   domain: PropTypes.string.isRequired,
+//   description: PropTypes.string.isRequired,
+//   id: PropTypes.string.isRequired,
+//   users: PropTypes.array.isRequired,
+//   isActive: PropTypes.bool
+// };
+
+// export default ProjectResultItem;
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {getAvatarUrl} from  '../../models/project';
-import UsersList from '../users-list.jsx';
+import {getAvatarThumbnailUrl} from '../../models/user';
 
-const ProjectResultItem = ({id, domain, description, users, action, isActive}) => {
-  var resultClass = "button-unstyled result ";
-  if(isActive) {
-    resultClass += " active";
-  }
+const UserResultItem = ({user, action}) => {
+  const {name, login, thanksCount} = user;
+  
+  const handleClick = (event) => {
+    console.log('💣');
+    action(event);
+  };
 
   return (
-    <button className={resultClass} onClick={action}>
-      <img className="avatar" src={getAvatarUrl(id)} alt={`Project avatar for ${domain}`}/>
-      <div className="result-name" title={domain}>{domain}</div>
-      
-      { description.length > 0 && <div className="result-description">{description}</div> }
-      { users.length > 0 && <UsersList users={users} /> }
+    <button onClick={handleClick} className="button-unstyled result">
+      <img className="avatar" src={getAvatarThumbnailUrl(user)} alt={`User avatar for ${login}`}/>
+      <div className="result-name" title={name}>{name}</div>
+      <div className="result-description" title={login}>@{login}</div>
     </button>
   );
 };
 
-ProjectResultItem.propTypes = {
+UserResultItem.propTypes = {
+  user: PropTypes.shape({
+    avatarThumbnailUrl: PropTypes.string,
+    name: PropTypes.string,
+    login: PropTypes.string.isRequired,
+    thanksCount: PropTypes.number.isRequired,
+  }).isRequired,
   action: PropTypes.func.isRequired,
-  domain: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  users: PropTypes.array.isRequired,
-  isActive: PropTypes.bool
 };
 
-export default ProjectResultItem;
+
+export default UserResultItem;
+
