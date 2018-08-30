607d80d67a86ae23dc2eef10d371459286210759
Glitch (remove-team-users) <none>
2018-08-30 21:11:33 +0000

    🦁🙆 Checkpoint ./src/presenters/pop-overs/team-user-info-and-remove-pop.jsx:97325/83

--- src/presenters/pop-overs/team-user-info-and-remove-pop.jsx
+++ src/presenters/pop-overs/team-user-info-and-remove-pop.jsx
@@ -206,7 +206,7 @@ class TeamUserRemove extends React.Component {
         </React.Fragment>
       );
     } else {
-      projects = <p className="action-description">(they aren't in any projects here)</p>;
+      projects = null; // <p className="action-description">(they aren't in any projects here)</p>;
     }
     
     return (
@@ -219,12 +219,14 @@ class TeamUserRemove extends React.Component {
           </section>
         </button>
         
+        {!!projects && (
         <section className="pop-over-actions" id="user-team-projects">
           <p className="action-description">
             Also remove them from these projects
           </p>
           {projects}
         </section>
+        )}
         
         <section className="pop-over-actions danger-zone">
           <button className="button-small has-emoji" onClick={this.removeUser}>
