0a63f2d6cf76816e5edf73f63299f9c7d32f47f1
Glitch (superb-guide) <none>
2018-09-04 17:44:12 +0000

    👫💸 Checkpoint ./src/presenters/team-editor.jsx:97325/150 ./src/presenters/pop-overs/add-team-user-pop.jsx:97325/67 ./src/presenters/includes/team-users.jsx:97325/8

--- src/presenters/includes/team-users.jsx
+++ src/presenters/includes/team-users.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//import {getDisplayName} from '../../models/user';
+import {getDisplayName} from '../../models/user';
 import {WhitelistedDomainIcon} from './team-elements.jsx';
 import AddTeamUserPop from '../pop-overs/add-team-user-pop.jsx';
 import PopoverContainer from '../pop-overs/popover-container.jsx';
@@ -99,7 +99,7 @@ export class AddTeamUser extends React.Component {
     togglePopover();
     await this.props.inviteUser(user);
     this.setState({
-      invitee: '', //getDisplayName(user),
+      invitee: getDisplayName(user),
     });
   }
   
