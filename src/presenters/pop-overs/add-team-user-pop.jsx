8a15cf259430c5bc3a8df05c9693608e67d79c01
Glitch (superb-guide) <none>
2018-09-04 19:04:23 +0000

    🚠📫 Checkpoint ./src/presenters/pop-overs/add-team-user-pop.jsx:97325/45

--- src/presenters/pop-overs/add-team-user-pop.jsx
+++ src/presenters/pop-overs/add-team-user-pop.jsx
@@ -71,7 +71,7 @@ class AddTeamUserPop extends React.Component {
     const email = parseOneAddress(query);
     let domain = null;
     if (email) {
-      console.log({ //results.push({
+      ({ //results.push({
         key: 'invite-by-email',
         item: <InviteByEmail email={email.address} onClick={() => inviteEmail(email.address)}/>,
       });
