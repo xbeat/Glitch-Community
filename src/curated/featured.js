63926d8087a093d999c5e50ae129bdc35c887047
Glitch (community) <none>
2018-09-06 09:20:00 +0000

    💀💠 Checkpoint ./src/curated/featured.js:11/475 ./styles/includes/featured.styl:11/12

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,16 +13,16 @@ Example:
 
 export default [
   {
-    title: 'Crystalis-VR',
+    title: 'Crystalis VR',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fcrystalis-vr.png?1535971898756",
     link: 'https://crystalis-vr.glitch.me/'
   },{
-    title: 'Turn Off-retweets',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fhydra.png?1535971898489",
+    title: 'Turn Off Retweets',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fturn-off-retweets.png?1535971899505",
     link: 'https://turn-off-retweets.glitch.me/'
   }, {
-    title: 'Subscribe2Me',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fsubscribe2me.png?1535971898937",
-    link: 'https://subscribe2me.glitch.me'
+    title: 'Literature Clock',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fliterature-clock.png?1535971899672",
+    link: 'https://literature-clock.glitch.me/'
   }  
 ];
