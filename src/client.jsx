a1de5c761f289923ddcfae70e8643897499b6e8e
Glitch (nova-chance) <none>
2018-09-07 20:41:01 +0000

    🍠🏑 Checkpoint ./src/app.jsx:97325/594 ./src/client.jsx:97325/822

--- src/client.jsx
+++ src/client.jsx
@@ -3,9 +3,28 @@ import 'details-element-polyfill';
 
 import React from 'react';
 import {render} from 'react-dom';
-
 import {BrowserRouter} from 'react-router-dom';
-import App from './app.jsx';
+
+import {CurrentUserProvider} from './presenters/current-user.jsx';
+import {UserPrefsProvider} from './presenters/includes/user-prefs.jsx';
+import {DevTogglesProvider} from './presenters/includes/dev-toggles.jsx';
+import {Notifications} from './presenters/notifications.jsx';
+
+import Router from './presenters/pages/router.jsx';
+
+const App = () => (
+  <BrowserRouter>
+    <Notifications>
+      <UserPrefsProvider>
+        <DevTogglesProvider>
+          <CurrentUserProvider>
+            {api => <Router api={api}/>}
+          </CurrentUserProvider>
+        </DevTogglesProvider>
+      </UserPrefsProvider>
+    </Notifications>
+  </BrowserRouter>
+);
 
 // Here's a bunch of browser support tests
 // If any of them don't work we can't run in this browser
@@ -28,6 +47,6 @@ window.bootstrap = () => {
   } else {
     const dom = document.createElement('div');
     document.body.appendChild(dom);
-    render(<BrowserRouter><App/></BrowserRouter>, dom);
+    render(<App/>, dom);
   }
 };
