665604e332880d806bec8defd2d0ef48a88ed75d
Glitch (community) <none>
2018-09-13 11:17:45 +0000

    🌊✍️ Checkpoint ./src/curated/embed.js:725013/572

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 450px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/flamingo-sunset?path=README.md&previewSize=100" alt="flamingo-sunset on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+<div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
+  <iframe src="https://glitch.com/embed/#!/embed/familiar-violet?path=server.js&previewSize=100" alt="familiar-violet on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
