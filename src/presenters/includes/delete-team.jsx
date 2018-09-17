285865e7c44c0c2031866dff5fb5dd7985a1d488
Glitch (expose-delete-team) <none>
2018-09-17 14:09:21 +0000

    🌱🏕 Checkpoint ./src/presenters/includes/delete-team.jsx:97325/407

--- src/presenters/includes/delete-team.jsx
+++ src/presenters/includes/delete-team.jsx
@@ -7,19 +7,17 @@ import PopoverContainer from '../pop-overs/popover-container.jsx';
 const DeleteTeam = ({...props}) => {
   
   return (
-    <section className="add-project-container">
-      <PopoverContainer>
-        {({visible, togglePopover}) => (
-          <div className="button-wrap">
-            <button className="button button-small button-tertiary has-emoji opens-pop-over danger-zone" onClick={togglePopover}>
-              <span>Delete {props.teamName} </span>
-              <span className="emoji bomb" role="img" aria-label="" />
-            </button>
-            { visible && <DeleteTeamPop {...props} togglePopover={togglePopover} /> }
-          </div>
-        )}
-      </PopoverContainer>
-    </section>
+    <PopoverContainer>
+      {({visible, togglePopover}) => (
+        <section className="button-wrap">
+          <button className="button button-small button-tertiary has-emoji opens-pop-over danger-zone" onClick={togglePopover}>
+            Delete {props.teamName}&nbsp;
+            <span className="emoji bomb" role="img" aria-label="" />
+          </button>
+          { visible && <DeleteTeamPop {...props} togglePopover={togglePopover} /> }
+        </section>
+      )}
+    </PopoverContainer>
   );
 };
 
