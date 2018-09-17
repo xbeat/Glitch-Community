500f44fe471ce0c7f2f7ca88c4e92f27f42281f0
Glitch (humorous-spaghetti) <none>
2018-09-17 17:58:57 +0000

    🍻👫 Checkpoint ./src/presenters/pop-overs/add-collection-avatar-pop.jsx:867163/3939

--- src/presenters/pop-overs/add-collection-avatar-pop.jsx
+++ src/presenters/pop-overs/add-collection-avatar-pop.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {debounce} from 'lodash';
 
+const avatars = {
+  computer: "https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fcomputer.svg",
+  tetris: "https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Ftetris.svg",
+  shapes: "https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fshapes.svg",
+  education: "https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Feducation.svg",
+  coffee: "https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fcoffee.svg",
+  diamond: "https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fdiamond.svg",
+  robot: "https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Frobot.svg",
+  hardware: "https://cdn.gomix.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Fhardware.svg",
+  art: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fart.svg?1499357014248",
+  music: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fmusic.svg?1502555440002",
+}
+
+const computer = "https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fcomputer.svg";
+const tetris = "https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Ftetris.svg";
+const shapes = "https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fshapes.svg";
+const education = "https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Feducation.svg";
+const coffee = "https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fcoffee.svg";
+const diamond = "https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fdiamond.svg";
+const robot = "https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Frobot.svg";
+const hardware = "https://cdn.gomix.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Fhardware.svg";
+const art = "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fart.svg?1499357014248";
+const music = "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fmusic.svg?1502555440002";
 
 class AddCollectionAvatarPop extends React.Component {
   constructor(props) {
@@ -24,6 +47,18 @@ class AddCollectionAvatarPop extends React.Component {
   onClick(type) {
     console.log('toggling popover');
     // change the avatar on the page
+    switch(type){
+      case "computer":
+        break;
+    case "computer":
+        break;
+    case "computer":
+        break;
+    case "computer":
+        break;
+    case "computer":
+        break;
+    }
     
     // toggle the pop-over
     this.props.togglePopover();      
@@ -33,35 +68,38 @@ class AddCollectionAvatarPop extends React.Component {
     return (
       <dialog className="pop-over add-collection-avatar-pop wide-pop">
         <section className="pop-over-info">
+          { avatars.forEach( type => {
+            
+          }) }
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("computer")}>
-            <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fcomputer.svg"/>
+            <img src={avatars["computer"]}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("tetris")}>
-            <img src="https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Ftetris.svg"/>
+            <img src={avatars["tetris"]}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("shapes")}>
-            <img src="https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fshapes.svg"/>
+            <img src={shapes}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("education")}>
-            <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Feducation.svg"/>
+            <img src={education}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("coffee")}>
-            <img src="https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fcoffee.svg"/>
+            <img src={coffee}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("diamond")}>
-            <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fdiamond.svg"/>
+            <img src={diamond}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("robot")}>
-            <img src="https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Frobot.svg"/>
+            <img src={robot}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("hardware")}>
-            <img src="https://cdn.gomix.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Fhardware.svg"/>
+            <img src={hardware}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("art")}>
-            <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fart.svg?1499357014248"/>
+            <img src={art}/>
           </button>
           <button className="collection-avatar button-tertiary" onClick={() => this.onClick("'music")}>
-            <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fmusic.svg?1502555440002"/>
+            <img src={music}/>
           </button>
         </section>
       </dialog>
