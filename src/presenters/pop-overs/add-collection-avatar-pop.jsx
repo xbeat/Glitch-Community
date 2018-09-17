23943a543f7005a5ae4eeda6314b61cc7b99603e
Glitch (humorous-spaghetti) <none>
2018-09-17 17:47:56 +0000

    🐙🐯 Checkpoint ./src/presenters/pop-overs/add-collection-avatar-pop.jsx:867163/1112

--- src/presenters/pop-overs/add-collection-avatar-pop.jsx
+++ src/presenters/pop-overs/add-collection-avatar-pop.jsx
@@ -21,43 +21,46 @@ class AddCollectionAvatarPop extends React.Component {
     // apply styling change to selected button
   }
   
-  onClick() {
+  onClick(type) {
     console.log('toggling popover');
-    this.props.togglePopover();    
+    // change the avatar on the page
+    
+    // toggle the pop-over
+    this.props.togglePopover();      
   }
   
   render() {
     return (
       <dialog className="pop-over add-collection-avatar-pop wide-pop">
         <section className="pop-over-info">
-          <button className="collection-avatar button-tertiary" onClick={changeAvatar}>
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("computer")}>
             <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fcomputer.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("tetris")}>
             <img src="https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Ftetris.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("shapes")}>
             <img src="https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fshapes.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("education")}>
             <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Feducation.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("coffee")}>
             <img src="https://cdn.hyperdev.com/0a15009e-17ee-4915-bc29-5ba03bb09517%2Fcoffee.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("diamond")}>
             <img src="https://cdn.hyperdev.com/us-east-1%3Acba180f4-ee65-4dfc-8dd5-f143280d3c10%2Fdiamond.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("robot")}>
             <img src="https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Frobot.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("hardware")}>
             <img src="https://cdn.gomix.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Fhardware.svg"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("art")}>
             <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fart.svg?1499357014248"/>
           </button>
-          <button className="collection-avatar button-tertiary">
+          <button className="collection-avatar button-tertiary" onClick={() => this.onClick("'music")}>
             <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fmusic.svg?1502555440002"/>
           </button>
         </section>
@@ -69,10 +72,5 @@ class AddCollectionAvatarPop extends React.Component {
 AddCollectionAvatarPop.propTypes = {
   api: PropTypes.func.isRequired,
 };
-
-const changeAvatar = () => {
-  // change the avatar on the page
-  // toggle the pop-over
-}
 
 export default AddCollectionAvatarPop;
