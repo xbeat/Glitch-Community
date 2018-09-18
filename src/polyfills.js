8ec3bc3d36b373787c99ab13bd9bde07ed882737
Glitch (profuse-tea) <none>
2018-09-18 16:16:10 +0000

    🏓🚍 Checkpoint ./src/polyfills.js:97325/295

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,7 +1,4 @@
-/// Polyfills go here
-// In general we shouldn't be using features not supported by browsers
-// but some things are really convenient, or pretty much supported everywhere
-// Make sure to re-evaluate these from time to time!
+/// Polyfills that aren't done using babel go here
 
 import 'details-element-polyfill';
 
