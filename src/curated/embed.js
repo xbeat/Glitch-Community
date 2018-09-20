9ccf3b7d1dff3af97a833fd673abd9c616100f86
Glitch (community) <none>
2018-09-20 11:41:20 +0000

    🚜👭 Checkpoint ./src/curated/embed.js:725013/578 ./src/curated/featured.js:725013/26

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
 <div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/familiar-violet?path=server.js&previewSize=100" alt="familiar-violet on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+  <iframe src="https://glitch.com/embed/#!/embed/space-is-the-place?path=README.md&previewSize=100" alt="space-is-the-place on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,15 +13,15 @@ Example:
 
 export default [
   {
-    title: 'Your Own IoT-Powered Mood Ring',
+    title: 'A DIY IoT-Powered Mood Ring',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fring.jpg?1537183211561",
     link: 'https://glitch.com/~mood-ring-light'
   },{
-    title: 'WikiBot for WhatsApp',
+    title: 'Play WikiBot for WhatsApp',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fwiki-bot.jpg?1537183215946",
     link: 'https://glitch.com/~wealthy-plate'
   }, {
-    title: 'Remember That Friends Episode?',
+    title: 'Remember That One Friends Episode?',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffriends.jpg?1537183216935",
     link: 'https://glitch.com/~arandomone'
   }  
