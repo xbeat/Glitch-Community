403b8819a0cdfaf91e0261eec6b27f90e3f20284
Glitch (community) <none>
2018-09-20 11:30:56 +0000

    🏢🚲 Checkpoint ./src/curated/featured.js:725013/922

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,16 +13,16 @@ Example:
 
 export default [
   {
-    title: 'AI-Powered Musical Spinners',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fspinners.jpg?1537183212696",
-    link: 'https://glitch.com/~incredible-spinners'
+    title: 'Your Own IoT-Powered Mood Ring',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fring.jpg?1537183211561",
+    link: 'https://glitch.com/~mood-ring-light'
   },{
-    title: 'Make Your Own Space Invaders',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fspace.jpg?1537183214030",
-    link: 'https://glitch.com/~remixable-space-invaders'
+    title: 'WikiBot for WhatsApp',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fwiki-bot.jpg?1537183215946",
+    link: 'https://glitch.com/~wealthy-plate'
   }, {
-    title: 'Play the Wikipedia Game',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fwiki-game.jpg?1537183217930",
-    link: 'https://glitch.com/~wikipedia-game'
+    title: 'Remember That Friends Episode?',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffriends.jpg?1537183216935",
+    link: 'https://glitch.com/~arandomone'
   }  
 ];
