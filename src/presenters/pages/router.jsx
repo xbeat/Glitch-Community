253a5a1a8b431b69b6e8b07bfdd49ab59764beab
Glitch (profuse-tea) <none>
2018-09-21 20:13:15 +0000

    🏜🍘 Checkpoint ./src/presenters/pages/router.jsx:97325/266 ./package.json:97325/32

--- src/presenters/pages/router.jsx
+++ src/presenters/pages/router.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import qs from 'querystringify';
 import {Route, Switch, withRouter} from 'react-router-dom';
 import Helmet from 'react-helmet';
 
@@ -20,6 +19,11 @@ import CategoryPage from './category.jsx';
 import ErrorPage from './error.jsx';
 import SecretPage from './secret.jsx';
 
+const parse = (search, name) => {
+  const params = new URLSearchParams(search);
+  return params.get(name);
+};
+
 const NotFoundPage = () => (
   <React.Fragment>
     <ErrorPage title="Page Not Found" description="Maybe a typo? Or perhaps it's moved?"/>
@@ -29,7 +33,7 @@ const NotFoundPage = () => (
   </React.Fragment>
 );
 
-class PageChangeHandlerImpl extends React.Component {
+class PageChangeHandlerBase extends React.Component {
   componentDidUpdate(prev) {
     if (this.props.location.key !== prev.location.key) {
       window.scrollTo({left: 0, top: 0, behavior: 'instant'});
@@ -42,7 +46,7 @@ class PageChangeHandlerImpl extends React.Component {
 }
 const PageChangeHandler = withRouter(({location}) => (
   <CurrentUserConsumer>
-    {(user, fetched, {reload}) => <PageChangeHandlerImpl location={location} reloadCurrentUser={reload}/>}
+    {(user, fetched, {reload}) => <PageChangeHandlerBase location={location} reloadCurrentUser={reload}/>}
   </CurrentUserConsumer>
 ));
 
@@ -53,8 +57,8 @@ const Router = ({api}) => (
       <Route path="/" exact render={({location}) => <IndexPage key={location.key} api={api}/>}/>
       <Route path="/index.html" exact strict render={({location}) => <IndexPage key={location.key} api={api}/>}/>
 
-      <Route path="/login/facebook" exact render={({location}) => <FacebookLoginPage key={location.key} api={api} code={qs.parse(location.search).code}/>}/>
-      <Route path="/login/github" exact render={({location}) => <GitHubLoginPage key={location.key} api={api} code={qs.parse(location.search).code}/>}/>
+      <Route path="/login/facebook" exact render={({location}) => <FacebookLoginPage key={location.key} api={api} code={parse(location.search, 'code')}/>}/>
+      <Route path="/login/github" exact render={({location}) => <GitHubLoginPage key={location.key} api={api} code={parse(location.search, 'code')}/>}/>
 
       <Route path="/questions" exact render={({location}) => <QuestionsPage key={location.key} api={api}/>}/>
 
@@ -68,7 +72,7 @@ const Router = ({api}) => (
         <Route key={name} path={`/${name}`} exact render={({location}) => <TeamPage key={location.key} api={api} id={rootTeams[name]} name={name}/>}/>
       ))}
 
-      <Route path="/search" exact render={({location}) => <SearchPage key={location.key} api={api} query={qs.parse(location.search).q}/>}/>
+      <Route path="/search" exact render={({location}) => <SearchPage key={location.key} api={api} query={parse(location.search, 'q')}/>}/>
 
       {categories.map(category => (
         <Route key={category.url} path={`/${category.url}`} exact render={({location}) => <CategoryPage key={location.key} api={api} category={category}/>}/>
