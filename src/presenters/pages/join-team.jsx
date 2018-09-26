3696e23ec3f55b3ce962ca14f35bcf9f2eccc246
Glitch (profuse-tea) <none>
2018-09-26 20:11:25 +0000

    🖼💴 Checkpoint ./src/presenters/pages/join-team.jsx:97325/1566

--- src/presenters/pages/join-team.jsx
+++ src/presenters/pages/join-team.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+/* global Raven */
 
 import {Redirect} from 'react-router-dom';
 import {getLink} from '../../models/team';
+import NotificationsConsumer from '../notifications.jsx';
 
-class JoinTeamPage extends React.Component {
+class JoinTeamPageBase extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,10 +15,22 @@ class JoinTeamPage extends React.Component {
   }
   
   async componentDidMount() {
-    const {data: team} = await this.props.api.get(`/teams/byUrl/${this.props.teamUrl}`);
     try {
-      const {data} = await this.props.api.post(`/teams/${team.id}/join/${this.props.joinToken}`);
+      throw 'asdf';
+      var {data: team} = await this.props.api.get(`/teams/byUrl/${this.props.teamUrl}`);
     } catch (error) {
+      Raven.captureException(error);
+    }
+    if (!team) {
+      // There was an api error or the team doesn't exist
+      // Either way we can't process the join request
+      this.setState({redirect: getLink({url: this.props.teamUrl})});
+      return;
+    }
+    try {
+      var {data} = await this.props.api.post(`/teams/${team.id}/join/${this.props.joinToken}`);
+    } catch (error) {
+
     }
     this.setState({redirect: getLink(team)});
   }
@@ -28,10 +42,17 @@ class JoinTeamPage extends React.Component {
     return null;
   }
 }
-JoinTeamPage.propTypes = {
+JoinTeamPageBase.propTypes = {
   api: PropTypes.any.isRequired,
   teamUrl: PropTypes.string.isRequired,
   joinToken: PropTypes.string.isRequired,
+  createErrorNotification: PropTypes.func.isRequired,
 };
+
+export const JoinTeamPage = (props) => (
+  <NotificationsConsumer>
+    {notify => <JoinTeamPageBase {...notify} {...props}/>}
+  </NotificationsConsumer>
+);
 
 export default JoinTeamPage;
