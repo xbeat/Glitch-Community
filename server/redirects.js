61920a6861434b08d794cd2b24a0f7cc3e334409
Glitch (community) <none>
2018-10-02 09:32:07 +0000

    🎮👄 Checkpoint ./server/redirects.js:11/466

--- server/redirects.js
+++ server/redirects.js
@@ -2,6 +2,10 @@ module.exports = function(app) {
   redirect(app, '/partners*', '/teams');
   redirect(app, '/foryourapi*', '/teams');
   redirect(app, '/forteams*', '/teams');
+  redirect(app, '/react-starter-kit*', '/featured/react-starter-kit');
+  redirect(app, '/website-starter-kit*', '/featured/website-starter-kit');  
+  redirect(app, '/you-got-this*', '/featured/you-got-this-zine');  
+  redirect(app, '/you-got-this/2*', '/featured/you-got-this-zine-2');  
 }
 
 function redirect(app, route, target) {
