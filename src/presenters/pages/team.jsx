beacbd9e7a16b611880c719e39e2eb86d73c6b31
Glitch (nova-chance) <none>
2018-10-02 21:11:11 +0000

    😽🎤 Checkpoint ./src/presenters/pages/team.jsx:97325/330 ./src/presenters/pop-overs/user-options-pop.jsx:97325/664 ./src/presenters/includes/dev-toggles.jsx:97325/201

--- src/presenters/pages/team.jsx
+++ src/presenters/pages/team.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import Helmet from 'react-helmet';
 import {CurrentUserConsumer} from '../current-user.jsx';
-import DevToggles from '../includes/dev-toggles.jsx';
 import TeamEditor from '../team-editor.jsx';
 import {getLink, getAvatarStyle, getProfileStyle} from '../../models/team';
 import {AuthDescription} from '../includes/description-field.jsx';
@@ -207,16 +206,14 @@ class TeamPage extends React.Component {
         }
 
         {/* billing info section goes here */}
-        <DevToggles>
-          {toggles => (toggles.includes('delete-teams') && this.props.currentUserIsTeamAdmin && (
-            <DeleteTeam api={() => this.props.api}
-              teamId={this.props.team.id}
-              teamName={this.props.team.name}
-              teamAdmins={this.teamAdmins()}
-              users={this.props.team.users}
-            />
-          ))}
-        </DevToggles>
+        {this.props.currentUserIsTeamAdmin && (
+          <DeleteTeam api={() => this.props.api}
+            teamId={this.props.team.id}
+            teamName={this.props.team.name}
+            teamAdmins={this.teamAdmins()}
+            users={this.props.team.users}
+          />
+        )}
 
         { !this.props.currentUserIsOnTeam &&
           <TeamMarketing />
