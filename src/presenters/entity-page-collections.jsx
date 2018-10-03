bc8f4b27c1f476d050504e23b3a74740bb4bc4ed
Glitch (humorous-spaghetti) <none>
2018-10-03 16:06:40 +0000

    😿😅 Checkpoint ./src/presenters/entity-page-collections.jsx:867163/1348

--- src/presenters/entity-page-collections.jsx
+++ src/presenters/entity-page-collections.jsx
@@ -4,7 +4,7 @@ import _ from 'lodash';
 
 import ProjectsList from './projects-list.jsx';
 import CollectionsList from './collections-list.jsx';
-import ProjectsLoader from './projects-loader.jsx';
+import {loader} from '../loader.jsx';
 
 const EntityPageCollections = ({collections, api, isAuthorized, userId}) => {
   
@@ -14,19 +14,17 @@ const EntityPageCollections = ({collections, api, isAuthorized, userId}) => {
   //     reloadProject(projectId);
   //   };
   // });
-  
-  async function loadCollections(){
-    console.log('load collections');
-    // const userId = 867163; // placeholder since can't pass it correctly yet
-    const [data] = await this.props.api.get(`collections/?userId=${userId}`);
-    console.log([data]);
-  }
+
 
   return (
     <React.Fragment>
+      
+      <loader get={() => this.props.api.get(`collections/?userId=${userId}`)}>
+        {collections => 
+          <CollectionsList title="Collections" collections={collections} api={api} isAuthorized={isAuthorized} />
+        }
+      </loader>
 
-      <CollectionsList title="Collections" collections={loadCollections} api={api} isAuthorized={isAuthorized} />
-       
       {/*
         <CollectionsList title="Collections" collections={collections.slice(0,2)} api={api} isAuthorized={isAuthorized} />
       */}
