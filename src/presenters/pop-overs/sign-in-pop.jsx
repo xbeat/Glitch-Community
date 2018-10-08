e9f98791f982dfcceb67a22d63839ffb6fdf3828
Glitch (quixotic-rocket) <none>
2018-10-08 15:36:01 +0000

    😄👡 Checkpoint ./src/presenters/pop-overs/sign-in-pop.jsx:97325/214

--- src/presenters/pop-overs/sign-in-pop.jsx
+++ src/presenters/pop-overs/sign-in-pop.jsx
@@ -4,26 +4,22 @@ import PopoverContainer from './popover-container.jsx';
 /* global GITHUB_CLIENT_ID, FACEBOOK_CLIENT_ID, APP_URL */
 
 function githubAuthLink() {
-  var clientId, redirectUri, scope;
-  clientId = GITHUB_CLIENT_ID;
-  scope = "user:email";
-  redirectUri = encodeURIComponent(`${APP_URL}/login/github`);
+  const clientId = GITHUB_CLIENT_ID;
+  const scope = "user:email";
+  const redirectUri = encodeURIComponent(`${APP_URL}/login/github`);
   return `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`;
 }
 
 function facebookAuthLink() {
-  var callbackURL, clientId, scopes;
-  clientId = FACEBOOK_CLIENT_ID;
-  scopes = "email";
-  callbackURL = encodeURIComponent(`${APP_URL}/login/facebook`);
-  return "https://www.facebook.com/v2.9/dialog/oauth?" + `client_id=${clientId}&scope=${scopes}&redirect_uri=${callbackURL}`;
+  const clientId = FACEBOOK_CLIENT_ID;
+  const scopes = "email";
+  const callbackURL = encodeURIComponent(`${APP_URL}/login/facebook`);
+  return `https://www.facebook.com/v2.9/dialog/oauth?client_id=${clientId}&scope=${scopes}&redirect_uri=${callbackURL}`;
 }
 
 const SignInPopButton = (props) => (
-  <Link className="button-link" to={props.href}>
-    <div className="button button-small">Sign in with {props.company}
-      <span className={`emoji ${props.emoji}`}></span>
-    </div>
+  <Link className="button button-small button-link" to={props.href}>
+    Sign in with {props.company} <span className={`emoji ${props.emoji}`}></span>
   </Link>
 );
 
