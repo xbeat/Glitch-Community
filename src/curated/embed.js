5d284ab229b912b1921300f9a7b149f02542c3ad
Glitch (community-staging) <none>
2018-10-11 11:53:00 +0000

    🎗👛 Checkpoint ./src/curated/embed.js:725013/567

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
 <div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/ripe-raven?path=server.js&previewSize=100" alt="ripe-raven on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+  <iframe src="https://glitch.com/embed/#!/embed/springy-particles?path=index.html&previewSize=100" alt="springy-particles on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
