c8fb4e3c69dfcf7b7cb2a86c5bf57c49bc88b129
Glitch (superb-guide) <none>
2018-10-12 16:01:53 +0000

    😇💪 Checkpoint ./src/curated/sample-analytics.js:97325/4398

--- src/curated/sample-analytics.js
+++ src/curated/sample-analytics.js
@@ -1,12 +1,110 @@
 // This is shown when a team has no projects
 // It should match the format of https://api.glitch.com/analytics/:id/team
+// The timestamps are negative because they are relative to the current time
+// See team-analytics.jsx for usage
 
 export default {
-  buckets: [{"@timestamp":1538193601000,"analytics":{"requests":47918,"remixes":2}},{"@timestamp":1538280001000,"analytics":{"requests":44497,"remixes":0}},{"@timestamp":1538366401000,"analytics":{"requests":52185,"remixes":4}},{"@timestamp":1538452801000,"analytics":{"requests":54217,"remixes":0}},{"@timestamp":1538539201000,"analytics":{"requests":56978,"remixes":4}},{"@timestamp":1538625601000,"analytics":{"requests":56922,"remixes":8}},{"@timestamp":1538712001000,"analytics":{"requests":52475,"remixes":12}},{"@timestamp":1538798401000,"analytics":{"requests":47812,"remixes":2}},{"@timestamp":1538884801000,"analytics":{"requests":45075,"remixes":4}},{"@timestamp":1538971201000,"analytics":{"requests":49986,"remixes":5}},{"@timestamp":1539057601000,"analytics":{"requests":52958,"remixes":2}},{"@timestamp":1539144001000,"analytics":{"requests":51098,"remixes":4}},{"@timestamp":1539230401000,"analytics":{"requests":51437,"remixes":2}},{"@timestamp":1539316801000,"analytics":{"requests":22920,"remixes":0}}],
+  buckets: [
+    {
+      "@timestamp": 1538280001000,
+      "analytics": {
+        "requests": 44497,
+        "remixes": 0
+      }
+    },
+    {
+      "@timestamp": 1538366401000,
+      "analytics": {
+        "requests": 52185,
+        "remixes": 4
+      }
+    },
+    {
+      "@timestamp": 1538452801000,
+      "analytics": {
+        "requests": 54217,
+        "remixes": 0
+      }
+    },
+    {
+      "@timestamp": 1538539201000,
+      "analytics": {
+        "requests": 56978,
+        "remixes": 4
+      }
+    },
+    {
+      "@timestamp": 1538625601000,
+      "analytics": {
+        "requests": 56922,
+        "remixes": 8
+      }
+    },
+    {
+      "@timestamp": 1538712001000,
+      "analytics": {
+        "requests": 52475,
+        "remixes": 12
+      }
+    },
+    {
+      "@timestamp": 1538798401000,
+      "analytics": {
+        "requests": 47812,
+        "remixes": 2
+      }
+    },
+    {
+      "@timestamp": 1538884801000,
+      "analytics": {
+        "requests": 45075,
+        "remixes": 4
+      }
+    },
+    {
+      "@timestamp": 1538971201000,
+      "analytics": {
+        "requests": 49986,
+        "remixes": 5
+      }
+    },
+    {
+      "@timestamp": 1539057601000,
+      "analytics": {
+        "requests": 52958,
+        "remixes": 2
+      }
+    },
+    {
+      "@timestamp": 1539144001000,
+      "analytics": {
+        "requests": 51098,
+        "remixes": 4
+      }
+    },
+    {
+      "@timestamp": 1539230401000,
+      "analytics": {
+        "requests": 51437,
+        "remixes": 2
+      }
+    },
+    {
+      "@timestamp": 1539316801000,
+      "analytics": {
+        "requests": 22920,
+        "remixes": 0
+      }
+    }
+  ],
   referrers: [
     {
       domain: 'glitch.com',
       requests: 265013,
+    },
+    {
+      domain: 'www.google.com',
+      requests: 8510,
     },
   ],
   remixReferrers: [
