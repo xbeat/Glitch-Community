130e1ad3a3b7132c472092beb319d8bc33bf7f86
Glitch (superb-guide) <none>
2018-10-12 15:50:55 +0000

    🐀⛄️ Checkpoint ./src/presenters/includes/team-analytics-activity.jsx:97325/26 ./src/curated/sample-analytics.js:97325/4121

--- src/curated/sample-analytics.js
+++ src/curated/sample-analytics.js
@@ -2,24 +2,7 @@
 // It should match the format of https://api.glitch.com/analytics/:id/team
 
 export default {
-  buckets: [
-    {
-      '@timestamp': 1538150401000,
-      analytics: {
-        remixes: 0,
-        requests: 2194,
-        visits: 1111,
-      },
-    },
-    {
-      '@timestamp': 1538510401000,
-      analytics: {
-        remixes: 0,
-        requests: 2649,
-        visits: 1111,
-      },
-    },
-  ],
+  buckets: [{"@timestamp":1538193601000,"analytics":{"requests":47918,"remixes":2}},{"@timestamp":1538280001000,"analytics":{"requests":44497,"remixes":0}},{"@timestamp":1538366401000,"analytics":{"requests":52185,"remixes":4}},{"@timestamp":1538452801000,"analytics":{"requests":54217,"remixes":0}},{"@timestamp":1538539201000,"analytics":{"requests":56978,"remixes":4}},{"@timestamp":1538625601000,"analytics":{"requests":56922,"remixes":8}},{"@timestamp":1538712001000,"analytics":{"requests":52475,"remixes":12}},{"@timestamp":1538798401000,"analytics":{"requests":47812,"remixes":2}},{"@timestamp":1538884801000,"analytics":{"requests":45075,"remixes":4}},{"@timestamp":1538971201000,"analytics":{"requests":49986,"remixes":5}},{"@timestamp":1539057601000,"analytics":{"requests":52958,"remixes":2}},{"@timestamp":1539144001000,"analytics":{"requests":51098,"remixes":4}},{"@timestamp":1539230401000,"analytics":{"requests":51437,"remixes":2}},{"@timestamp":1539316801000,"analytics":{"requests":22920,"remixes":0}}],
   referrers: [
     {
       domain: 'glitch.com',
