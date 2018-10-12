83230a674a9c27fe819d41333c0b50bb4ff02d3d
Glitch (superb-guide) <none>
2018-10-12 15:39:53 +0000

    👆🐡 Checkpoint ./src/curated/sample-analytics.js:97325/845

--- src/curated/sample-analytics.js
+++ src/curated/sample-analytics.js
@@ -2,8 +2,35 @@
 // It should match the format of https://api.glitch.com/analytics/:id/team
 
 export default {
-  buckets: [],
-  referrers: [],
-  remixReferrers: [],
+  buckets: [
+    {
+      '@timestamp': 1538150401000,
+      analytics: {
+        remixes: 0,
+        requests: 2194,
+        visits: 1111,
+      },
+    },
+    {
+      '@timestamp': 1538510401000,
+      analytics: {
+        remixes: 0,
+        requests: 2649,
+        visits: 1111,
+      },
+    },
+  ],
+  referrers: [
+    {
+      domain: 'glitch.com',
+      requests: 265013,
+    },
+  ],
+  remixReferrers: [
+    {
+      domain: 'glitch.com',
+      remixes: 3,
+    },
+  ],
   remixers: [],
 };
