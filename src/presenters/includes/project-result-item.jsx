6602941d5f199f65a7ddc4e14b471cd15c073f72
Glitch (longing-elf) <none>
2018-10-18 20:51:40 +0000

    🍩📚 Checkpoint ./src/presenters/includes/project-result-item.jsx:8/1148

--- src/presenters/includes/project-result-item.jsx
+++ src/presenters/includes/project-result-item.jsx
@@ -11,20 +11,26 @@ const ProjectResultItem = ({id, domain, description, users, action, isActive}) =
   }
 
   return (
-    <button className={resultClass} onClick={action} data-project-id={id}>
-      <img className="avatar" src={getAvatarUrl(id)} alt={`Project avatar for ${domain}`}/>
-      <div className="results-info">
-        <div className="result-name" title={domain}>{domain}</div>
-        { description.length > 0 && <div className="result-description">{description}</div> }
-        { users.length > 0 && <UsersList users={users} /> }
-      </div>
+    <div>
+      <button className={resultClass} onClick={action} data-project-id={id}>
+        <img className="avatar" src={getAvatarUrl(id)} alt={`Project avatar for ${domain}`}/>
+        <div className="results-info">
+          <div className="result-name" title={domain}>{domain}</div>
+          { description.length > 0 && <div className="result-description">{description}</div> }
+          { users.length > 0 && <UsersList users={users} /> }
+        </div>
+        <a href={`/~${domain}`} className="view-project-link" target="_blank">
+          <button className="view-project button-small button-docs">
+            View →
+          </button>
+        </a>
+      </button>
       <a href={`/~${domain}`} className="view-project-link" target="_blank">
         <button className="view-project button-small button-docs">
           View →
         </button>
       </a>
-        
-    </button>
+    </div>
   );
 };
 
