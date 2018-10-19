e707314cecc5f7bd8e57859acb4617e5408e8109
Glitch (longing-elf) <none>
2018-10-19 14:24:09 +0000

    🏆❣️ Checkpoint ./src/presenters/pages/category.jsx:867163/14066

--- src/presenters/pages/category.jsx
+++ src/presenters/pages/category.jsx
@@ -20,71 +20,17 @@ import AddCollectionAvatar from '../includes/add-collection-avatar.jsx';
 
 import EditCollectionColor from '../includes/edit-collection-color.jsx';
 
-import {avatars} from '../../models/collection.js'; 
+import {avatars, hexToRgbA} from '../../models/collection.js'; 
 
 import {CurrentUserConsumer} from '../current-user.jsx';
-
-// some dummy info for testing - to be eventually removed as name generation happens outside of collection page
-let adjectives = [
-  'charming',
-  'bold',
-  'brave',
-  'cool',
-  'docile',
-  'dope',
-  'faithful',
-  'fertile',
-  'fervent',
-  'forgiving',
-  'genial',
-  'genteel',
-  'grouchy',
-  'hopeful',
-  'humane',
-  'jolly',
-  'joyful',
-  'loving',
-  'magical',
-  'moral',
-  'mysterious',
-  'notorious',
-  'passionate',
-  'preposterous',
-  'quaint',
-  'quirky',
-  'scrumptious',
-  'sensitive',
-  'sober',
-  'tropical',
-  'woeful',
-  'whimsical',
-  'zealous',
-];
-
-let name = "Wondrous Collection";
-const url = "wondrous";
-const avatarUrl = "https://cdn.gomix.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Flogo-sunset.svg?1489265199230"; // to be replaced
-let color = "#FFA3BB";
-let description = "A collection of projects that does wondrous things.";
-const id = 14;
-
-const randomName = () => {
-  let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  let randomName = randomAdjective.charAt(0).toUpperCase() + randomAdjective.slice(1); // capitalize first letter
-  name = `${randomName} Collection`;
-  description = `A collection of projects that does ${randomAdjective} things`;
-};
 
 class CollectionColorWrap extends React.Component { 
   constructor(props){
     super(props);
     
-    if(this.props.collection){
-      color= this.props.collection.color;
-    }
     this.state = {
-      color: color,
-      avatar: (this.props.collection ? this.props.collection.avatarUrl : avatarUrl),
+      color: this.props.backgroundColor,
+      avatar: this.props.avatarUrl
     };
     this.setColor = this.setColor.bind(this);
     this.setAvatar = this.setAvatar.bind(this);
@@ -102,124 +48,36 @@ class CollectionColorWrap extends React.Component {
   //   return null;
   // }
   
-  setAvatar(newAvatar){
-    this.setState({
-      avatar: newAvatar
-    });
-    console.log(`newAvatar: ${newAvatar}`);
-  }
-  
-  setColor(newColor){
-    this.setState({
-      color: newColor
-    });
-    console.log(`newColor: ${newColor}`);
-  }
-  
+ 
   render(){
-    return this.props.children(this.state.color, this.setColor, this.state.avatar, this.setAvatar);
+    return this.props.children(this.state.color, this.state.avatar);
   }
 }
 
-const hexToRgbA = (hex) => {
-  var c;
-  if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-    c= hex.substring(1).split('');
-    if(c.length== 3){
-      c= [c[0], c[0], c[1], c[1], c[2], c[2]];
-    }
-    c= '0x'+c.join('');
-    return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.4)';
-  }
-  throw new Error('Bad Hex');
-};
-
-{/*
-const CollectionPageEditor = ({api, initialCollection, children}) => (
-  <CollectionEditor api={api} initialCollection={initialCollection}>
-    {(collection, funcs, ...args) =>{
-      <ProjectsLoader api={api} projects={collection.projects}>
-          {(projects, reloadProjects) => {
-          return children({...collection, projects}, {
-            ...funcs,
-          }, ...args);
-          }}
-           )}
-     </ProjectsLoader>
-    }};
-  </CollectionEditor>
-  );
-*/}
-
-const CollectionPageWrap = ({collection, api, color, setColor, avatar, setAvatar, isAuthorized, updateName, updateDescription, projectOptions, uploadAvatar, addProject, removeProject, ...props}) => (
+const CategoryPageWrap = ({category, api, color, avatar, projectOptions, uploadAvatar, ...props}) => (
   <React.Fragment>
     
     <Helmet>
-      <title>{(collection ? collection.name : name)}</title>
+      <title>{category.name}</title>
     </Helmet>
     <main className="collection-page">
       <article className="projects" style={{backgroundColor: hexToRgbA(color)}}>
         <header className="collection">
           <h1 className="collection-name">
-            {/* TO DO: actually update name */}
-            {(isAuthorized 
-              ? <AuthDescription authorized={isAuthorized}
-                description={(collection ? collection.name : name)} 
-                update={updateName => null} 
-                placeholder="Name your collection"/> 
-              : <React.Fragment>{collection.name} </React.Fragment>
-            )}
+            <React.Fragment>{category.name} </React.Fragment>
           </h1>
           <div className="collection-image-container">
             <img src={avatar} alt=""/>
           </div>
-          {/* TO DO: actually enable uploading avatar - see example of uploadAvatar in user-editor.jsx */}
-          {isAuthorized 
-            ? <div className="upload-image-buttons">
-              
-              <AddCollectionAvatar
-                api={api}
-                collectionID = {(collection ? collection.id : id)}
-                setAvatar={setAvatar}
-              />
-              
-              {/*
-                <button className="button button-small button-tertiary" onClick={uploadAvatar}>
-                  <span>Replace Avatar</span>  
-                </button>
-              */}
-              
-            </div>
-            : null
-          }
           
           <p className="description">
-            {/* TO DO: actually update description */}
-            <AuthDescription
-              authorized={isAuthorized} description={(collection ? collection.description : description)}
-              update={updateDescription => null} placeholder="Tell us about your collection"
-            />
+            {category.description}
           </p>
-          
-          
-          <EditCollectionColor
-            api={api}
-            collectionID={(collection ? collection.id : id)}
-            currentUserIsOwner={true}
-            setColor={setColor}
-          />
-          
-          {(isAuthorized
-            ? <button className={`button delete-collection button-tertiary`} >
-              Delete Collection
-            </button>
-            : null
-          )}
+
           
         </header>
         
-        {collection
-          ? <ProjectsLoader api={api} projects={collection.projects}>
+        <ProjectsLoader api={api} projects={collection.projects}>
             {projects => 
               <React.Fragment>
                 <div className="collection-contents">
@@ -250,28 +108,7 @@ const CollectionPageWrap = ({collection, api, color, setColor, avatar, setAvatar
             }
           </ProjectsLoader>
         
-          : 
-          <React.Fragment>
-            <div className="collection-project-container-header">
-              <h3>Projects</h3>
-                
-              {(isAuthorized 
-                ? <AddCollectionProject
-                  api={api}
-                  collectionProjects={null}
-                  currentUserIsOwner={true}
-                  myProjects= {[]}
-                />
-                : null
-              )}
-              <div className="empty-collection-hint" style={{backgroundColor: color}}>
-                Click <b>Add Project</b> to search for projects to add to your collection.<br/><br/>You can add any project, created by any user.
-              </div>
-                
-            </div>
-          
-          </React.Fragment>
-        }
+         
         
       </article>
       
@@ -280,7 +117,7 @@ const CollectionPageWrap = ({collection, api, color, setColor, avatar, setAvatar
   </React.Fragment>
 );
 
-CollectionPageWrap.propTypes = {
+CategoryPageWrap.propTypes = {
   collection: PropTypes.shape({
     avatarUrl: PropTypes.string.isRequired,
     backgroundColor: PropTypes.string.isRequired,
@@ -337,7 +174,7 @@ const CollectionPage = ({api, collection, user, name, addProject, removeProject,
     >
       {collection => (
         <CollectionColorWrap collection={collection}>
-          {(color, setColor, avatar, setAvatar) => <CollectionPageWrap collection={collection} setColor={setColor} color={color} setAvatar={setAvatar} avatar={avatar} api={api} isAuthorized={true} addProject={addProject} removeProject={removeProject} {...props}/>}
+          {(color, setColor, avatar, setAvatar) => <CategoryPageWrap collection={collection} setColor={setColor} color={color} setAvatar={setAvatar} avatar={avatar} api={api} isAuthorized={true} addProject={addProject} removeProject={removeProject} {...props}/>}
         </CollectionColorWrap>
       )}
     </DataLoader>
