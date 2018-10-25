97f810ad6195451340a56cbce6ca6c8216cc1096
Glitch (community) <none>
2018-10-25 11:28:41 +0000

    👙🐃 Checkpoint ./src/curated/embed.js:725013/577

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,6 +1,6 @@
 export default `
 <!-- Copy and Paste Me -->
 <div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
-  <iframe src="https://glitch.com/embed/#!/embed/noisy-circles?path=index.html&previewSize=100" alt="noisy-circles on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
+  <iframe src="https://glitch.com/embed/#!/embed/unicode-hieroglyphs?path=README.md&previewSize=100" alt="unicode-hieroglyphs on glitch" style="height: 100%; width: 100%; border: 0;"></iframe>
 </div>
 `;
