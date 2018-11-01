69a0653be54a56cfd42b34e6b8f1066060bc5ac2
Glitch (community) <none>
2018-11-01 16:22:34 +0000

    🌗🎒 Checkpoint ./src/presenters/includes/dev-toggles.jsx:8/465

--- src/presenters/includes/dev-toggles.jsx
+++ src/presenters/includes/dev-toggles.jsx
@@ -15,9 +15,9 @@ const {Provider, Consumer} = React.createContext();
 // We can only have three.
 // Users can enable them with the /secret page.
 const toggleData = [
-  {name: "add-teams", description: "This isn't used any more!"},
-  {name: "delete-teams", description: "I have served my purpose and can be removed"},
-  {name: "team-billing", description: "can you pay for teams?."},
+  {name: "Magic Beans", description: "I'm a placeholder for a future toggle"},
+  {name: "Unlimited Emojis", description: "I'm a placeholder for a future toggle"},
+  {name: "Peace of Mind", description: "I'm a placeholder for a future toggle"},
 ].splice(0,3); // <-- Yeah really, only 3.  If you need more, clean up one first.
 
 
