79d9a67eb951496a702f766f076a6e28b0f17086
Glitch (community) <none>
2018-11-05 12:30:31 +0000

    🍺🏛 Checkpoint ./src/curated/featured.js:725013/275

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,16 +13,16 @@ Example:
 
 export default [
   {
-    title: 'Your Moment of Zen',
+    title: 'Venn Diagrams Gone Wild',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F4_spontaneous-venning.png?1541419749124",
-    link: 'https://your-moment-of-zen.glitch.me/'
+    link: 'https://glitch.com/~spontaneous-venning/'
   },{
     title: 'Deploy to Firebase',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch-firebase-homepage.jpg?1540810252060",
     link: 'https://glitch.com/edit/#!/firebase-basic-webpage?path=README.md:1:0'
   }, {
-    title: 'Defend The Galaxy',
+    title: 'Trippy Generative Art Demos',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F3_inconvergent-sandbox.png?1541419748493",
-    link: 'https://invaders-from-space.glitch.me'
+    link: 'https://glitch.com/~inconvergent-sandbox'
   }  
 ];
