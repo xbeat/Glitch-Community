0ae293bfdec0836cf16084acd26830b1df782983
Glitch (longing-elf) <none>
2018-11-07 20:26:17 +0000

    🐉🌆 Checkpoint ./styles/pop-overs.styl:867163/715 ./src/presenters/pop-overs/edit-collection-color-pop.jsx:867163/4

--- src/presenters/pop-overs/edit-collection-color-pop.jsx
+++ src/presenters/pop-overs/edit-collection-color-pop.jsx
@@ -88,7 +88,7 @@ class EditCollectionColorPop extends React.Component {
                   
           <input className="color-picker" type="color" value={this.state.color} onChange={(e) => this.changeColor(e.target.value)} style={{backgroundColor: this.state.color}} id="color-picker"></input>
           
-          <input id="color-picker"
+          <input id="color-picker-hex"
             value={this.state.query} 
             onChange={this.handleChange} 
             onKeyPress={this.keyPress}
