5ad256bf99826cae6f937fba285530bac0743cad
Glitch (community) <none>
2018-11-08 12:31:13 +0000

    ⛹🍜 Checkpoint ./src/curated/featured.js:725013/211 ./src/curated/embed.js:725013/1974

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -17,12 +17,12 @@ export default [
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F2_drum-machine.png?1541419748683",
     link: 'https://glitch.com/~drum-machine/'
   },{
-    title: 'Deploy to Firebase',
+    title: 'Measure Your Team\'s Stress Level',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F1_capacimeter.png?1541419748917",
-    link: 'https://glitch.com/edit/#!/firebase-basic-webpage?path=README.md:1:0'
+    link: 'https://glitch.com/~capacimeter'
   }, {
-    title: 'Trippy Generative Art Demos',
+    title: 'Generate Your Own Quilt',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F6_quilt-generator.png?1541419749609",
-    link: 'https://glitch.com/~inconvergent-sandbox'
+    link: 'https://glitch.com/~quilt-generator'
   }  
 ];
