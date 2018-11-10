3cd47b6491ddad4454d2543d72f3713b36a99a72
Glitch (community) <none>
2018-11-10 11:17:04 +0000

    👶🚖 Checkpoint ./sh/webpack.sh:728660/72

--- sh/webpack.sh
+++ sh/webpack.sh
@@ -2,12 +2,14 @@
 
 # Webpack will occassionally crash, so run it in a loop so that it'll come back to life on failure.
 
+export TMP=/app/.tmp
+
 eslint --config server/.eslintrc.server.js webpack.config.js
 
 echo "Starting webpack watcher"
 
 while true; do
-  env NODE_OPTIONS="--max-old-space-size=256" webpack --watch --info-verbosity verbose
+  webpack --watch --info-verbosity verbose
   
   echo "Webpack crashed; Restarting webpack --watch"
 done
