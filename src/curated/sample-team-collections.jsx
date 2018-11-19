56a51b7b883fb0e5817dbc0ad2307b0a6c3f0f0d
Glitch (community) <none>
2018-11-19 18:06:02 +0000

    🌃💔 Checkpoint ./src/curated/sample-team-collections.jsx:867163/57594

---
+++ src/curated/sample-team-collections.jsx
@@ -0,0 +1,17 @@
+// this is shown when a team has no collections
+
+import CollectionAvatar from '../presenters/includes/collection-avatar.jsx';
+
+<ul className="collections-container">
+  <div className={['collection']}>
+    <div className="collection-container">
+      <div className="collection-info" style={{backgroundColor: "#EBEBEB"}}>
+        <div className="avatar-container">
+          <div className="avatar">
+            <CollectionAvatar backgroundColor={hexToR
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</ul>
