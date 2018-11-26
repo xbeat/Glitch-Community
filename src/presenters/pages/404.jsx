6412bd9be306e2f4886661a3d0fbf95bf8d8df1c
Glitch (community) <none>
2018-11-26 19:05:17 +0000

    🛫🐘 Checkpoint ./.glitch-assets:708175/406 ./src/presenters/pages/404.jsx:708175/552 ./styles/pages/404.styl:708175/80 ./styles/styles.styl:708175/25 ./src/presenters/header.jsx:8/1192 ./src/presenters/includes/logo.jsx:8/3166

--- src/presenters/pages/404.jsx
+++ src/presenters/pages/404.jsx
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import ErrorPage from './error.jsx';
 
+const telescopeImageUrl = 'https://cdn.glitch.com/7138972f-76e1-43f4-8ede-84c3cdd4b40a%2Ftelescope_404.svg?1543258683849';
+
 const NotFoundPage = () => (
   <>
-    <ErrorPage title="Page Not Found" description="Maybe a typo? Or perhaps it's moved?"/>
+    <img src={telescopeImageUrl} alt='' />
+  <div id=''>test</div>
     <Helmet>
       <title>👻 Page not found</title> {/* eslint-disable-line */}
     </Helmet>
