23930b36e51b1f4a3439506616877e51e816f364
Glitch (community) <none>
2018-11-26 19:27:16 +0000

    🎸⛄️ Checkpoint ./src/presenters/pages/404.jsx:8/2180:708175/2088 ./styles/pages/404.styl:708175/44

--- src/presenters/pages/404.jsx
+++ src/presenters/pages/404.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Helmet from 'react-helmet';
 import Layout from '../layout';
@@ -6,15 +7,24 @@ import Layout from '../layout';
 const telescopeImageUrl = 'https://cdn.glitch.com/7138972f-76e1-43f4-8ede-84c3cdd4b40a%2Ftelescope_404.svg?1543258683849';
 
 const NotFoundPage = ({api}) => (
-  <div id='page-not-found-container'>
-    <img src={telescopeImageUrl} alt='' />
-  <section id='page-not-found'>
-    test
-  </section>
+  <Layout api={api}>
     <Helmet>
       <title>👻 Page not found</title> {/* eslint-disable-line */}
     </Helmet>
-  </div>
+    
+    <main id='page-not-found-container'>
+        <img id="fallback-img" src={telescopeImageUrl} alt='' />
+        <div className="error-msg">
+          <h1>Page Not Found</h1>
+          <p>Maybe a typo, or perhaps it's moved?</p>
+          <a className="button" href="/">Back to Glitch</a>
+        </div>
+    </main>
+  </Layout>
 );
+
+NotFoundPage.propTypes = {
+  api: PropTypes.func.isRequired,
+};
 
 export default NotFoundPage;
