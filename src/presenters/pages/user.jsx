b08d14060c1850bff645188970a232d41c0e21ae
Glitch (community) <none>
2018-12-05 15:36:43 +0000

    📹⛪️ Checkpoint ./src/presenters/pages/user.jsx:867163/65 ./styles/emoji.styl:867163/195 ./.glitch-assets:867163/394

--- src/presenters/pages/user.jsx
+++ src/presenters/pages/user.jsx
@@ -109,7 +109,7 @@ const UserPage = ({
     </section>
 
     <section id="embed">
-      <h2>Featured Project</h2>
+      <h2>Featured Project<span class="emoji glowing-star emoji-in-title"></span></h2>
       <Embed domain={projects[0].domain}/>
     </section>
     
