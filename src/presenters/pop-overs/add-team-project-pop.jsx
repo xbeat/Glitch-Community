8378f37724464527346039971df39f5f9f0e9f15
Glitch (interesting-riddle) <none>
2018-12-05 18:04:26 +0000

    🙇🐜 Checkpoint ./src/presenters/pop-overs/add-team-project-pop.jsx:708175/364 ./src/presenters/pop-overs/report-abuse-pop.jsx:708175/2206

--- src/presenters/pop-overs/add-team-project-pop.jsx
+++ src/presenters/pop-overs/add-team-project-pop.jsx
@@ -12,7 +12,6 @@ export class AddTeamProjectPop extends React.Component {
       filteredProjects: [],
       source: 'my-projects', // my-projects, templates
       filterPlaceholder: 'Filter my projects',
-      filterValue: undefined,
       loadingTemplates: false,
       notifyTemplateIsRemixing: false,
     };
@@ -31,9 +30,6 @@ export class AddTeamProjectPop extends React.Component {
   }
 
   updateFilter(query) {
-    this.setState({
-        filterValue: query
-      });
     let projects = [];
     if (this.state.source === 'templates') {
       projects = this.state.templateProjects;
@@ -42,7 +38,7 @@ export class AddTeamProjectPop extends React.Component {
     }
     let filteredProjects = this.filterProjects(query, projects, this.props.teamProjects);
     this.setState({
-      filteredProjects: filteredProjects,
+      filteredProjects: filteredProjects
     });
   }
 
@@ -162,8 +158,7 @@ export class AddTeamProjectPop extends React.Component {
             ref={this.filterInput}
             onChange={(event) => {this.updateFilter(event.target.value);}}
             id="team-project-search" className="pop-over-input search-input pop-over-search"
-            value={this.state.filterValue}
-            placeholder={this.state.filterPlaceholder}
+            placeholder= {this.state.filterPlaceholder}
             autoFocus // eslint-disable-line jsx-a11y/no-autofocus
           />
         </section>
