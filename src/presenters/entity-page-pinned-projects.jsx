79e16d7910815634e98bf9022ef8d15edeee69ab
Glitch (profile-feature) <none>
2018-12-12 16:46:16 +0000

    🌏🌺 Checkpoint ./src/presenters/entity-page-pinned-projects.jsx:867163/1054 ./styles/pages/user-and-team-page.styl:867163/63

--- src/presenters/entity-page-pinned-projects.jsx
+++ src/presenters/entity-page-pinned-projects.jsx
@@ -22,48 +22,48 @@ function trackRemix(id, domain) {
 
 const FeaturedPinnedProject = ({api, isAuthorized, currentUser, featuredProjectDomain, projects, addProjectToCollection,}) => {
   return(
-    <section id="embed">      
-      
-      <h2>Pinned Projects<span className="emoji pushpin emoji-in-title"></span></h2>
-
-      {isAuthorized && <FeaturedProjectOptionsPop />}
-      <div className="glitch-embed-wrap">
-        <iframe title="embed"
-          src={`${APP_URL}/embed/#!/embed/${featuredProjectDomain}?path=README.md&previewSize=100`}
-          allow="geolocation; microphone; camera; midi; encrypted-media"
-        ></iframe>
-      </div>
-
-      {isAuthorized ?
-        <div className="buttons buttons-left">
-          <EditButton className="button-small button-edit" name={featuredProjectDomain} isMember={isAuthorized}/>
-        </div>
-        :
-        <div className="buttons buttons-left">
-          <ReportButton className="button-small" name={featuredProjectDomain} id={featuredProjectDomain}/>
+    <>
+      <h2 style={{marginTop: 2+"em"}}>Pinned Projects<span className="emoji pushpin emoji-in-title"></span></h2>
+      <section id="embed" style={{marginTop: 0}}>      
+        {isAuthorized && <FeaturedProjectOptionsPop />}
+        <div className="glitch-embed-wrap">
+          <iframe title="embed"
+            src={`${APP_URL}/embed/#!/embed/${featuredProjectDomain}?path=README.md&previewSize=100`}
+            allow="geolocation; microphone; camera; midi; encrypted-media"
+          ></iframe>
         </div>
 
-      }
+        {isAuthorized ?
+          <div className="buttons buttons-left">
+            <EditButton className="button-small button-edit" name={featuredProjectDomain} isMember={isAuthorized}/>
+          </div>
+          :
+          <div className="buttons buttons-left">
+            <ReportButton className="button-small" name={featuredProjectDomain} id={featuredProjectDomain}/>
+          </div>
 
-      {/* Might want to allow users to hide this message once they've seen it once
-        <button className="button-small button-tertiary" style={{marginLeft:5+"px"}}>hide</button> 
-      */}
-      {isAuthorized && 
-        <p className="hint">Tweak the way this embed looks by editing the project and going to <b>Share > Embed Project</b></p>
-      }
+        }
 
-      <div className="buttons buttons-right">
-
-        {currentUser.login && <AddProjectToCollection className="button-small" api={api} currentUser={currentUser} project={projects[0]} fromProject={true} addProjectToCollection={addProjectToCollection}/>}
-
-        {/* Can add to track remix later 
-          onClick={() => trackRemix(projects[0].id, projects[0].domain)}
+        {/* Might want to allow users to hide this message once they've seen it once
+          <button className="button-small button-tertiary" style={{marginLeft:5+"px"}}>hide</button> 
         */}
-        <RemixButton className="button-small"
-          name={projects[0].domain} isMember={isAuthorized}
-        />
-      </div>
-    </section>
+        {isAuthorized && 
+          <p className="hint">Tweak the way this embed looks by editing the project and going to <b>Share > Embed Project</b></p>
+        }
+
+        <div className="buttons buttons-right">
+
+          {currentUser.login && <AddProjectToCollection className="button-small" api={api} currentUser={currentUser} project={projects[0]} fromProject={true} addProjectToCollection={addProjectToCollection}/>}
+
+          {/* Can add to track remix later 
+            onClick={() => trackRemix(projects[0].id, projects[0].domain)}
+          */}
+          <RemixButton className="button-small"
+            name={projects[0].domain} isMember={isAuthorized}
+          />
+        </div>
+      </section>
+    </>
     )
 };
 
