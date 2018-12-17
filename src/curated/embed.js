e573ab254533f7d184d12f0e8d48c1a54c0aad8d
Glitch (community) <none>
2018-12-17 14:03:55 +0000

    🚙🍗 Checkpoint ./src/curated/featured.js:725013/611 ./src/curated/embed.js:725013/720

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -3,8 +3,8 @@ export default `
 <div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
   <iframe
     allow="geolocation; microphone; camera; midi; encrypted-media"
-    src="https://glitch.com/embed/#!/embed/four-venom?path=server.js&previewSize=100"
-    alt="four-venom on Glitch"
+    src="https://glitch.com/embed/#!/embed/falling-snow?path=README.md&previewSize=100"
+    alt="falling-snow on Glitch"
     style="height: 100%; width: 100%; border: 0;">
   </iframe>
 </div>
