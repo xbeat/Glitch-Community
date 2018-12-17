41e817bbdbea57d501a958a414151118fe86baad
Glitch (community) <none>
2018-12-17 19:08:10 +0000

    🍜👋 Checkpoint ./.glitch-assets:867163/763 ./src/presenters/includes/not-found.jsx:867163/424 ./styles/animations.styl:867163/112

--- src/presenters/includes/not-found.jsx
+++ src/presenters/includes/not-found.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const compass = "https://cdn.glitch.com/bc50f686-4c0a-4e20-852f-3999b29e8092%2Fcompass.svg?1545073264846";
+const needle = "https://cdn.glitch.com/bc50f686-4c0a-4e20-852f-3999b29e8092%2Fneedle.svg?1545073265096";
+
 const NotFound = ({name}) => (
   <section>
     <p>we didn't find {name}</p>
+    <div className="error-image">
+      <img className="compass" src={compass}/>
+      <img className="needle" src={needle}/>
+    </div>      
   </section>
 );
 
