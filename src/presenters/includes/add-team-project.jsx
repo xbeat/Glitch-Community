35d81f228779239913edacd445349f5a999fab5f
Glitch (interesting-riddle) <none>
2018-12-18 14:59:41 +0000

    🍣🗽 Checkpoint ./src/presenters/includes/add-team-project.jsx:708175/1595 ./src/presenters/pop-overs/add-collection-project-pop.jsx:708175/12 ./src/presenters/includes/add-collection-project.jsx:708175/976

--- src/presenters/includes/add-team-project.jsx
+++ src/presenters/includes/add-team-project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import AddTeamProjectPop from "../pop-overs/add-team-project-pop.jsx";
-import PopoverContainer from "../pop-overs/popover-container.jsx";
+import PopoverWithButton from "../pop-overs/popover-with-button";
 
 const AddTeamProject = ({ currentUserIsOnTeam, ...props }) => {
   if (!currentUserIsOnTeam) {
@@ -11,30 +11,14 @@ const AddTeamProject = ({ currentUserIsOnTeam, ...props }) => {
 
   return (
     <section className="add-project-container">
-      {/* Add disabled={props.projectLimitIsReached} once billing is ready */}
+      {/* Add disabled={props.projectLimitIsReached} to button once billing is ready */}
       <PopoverWithButton
         buttonClass={`button add-project has-emoji opens-pop-over ${props.extraButtonClass}`}
-        buttonText={<div className="down-arrow" aria-label="options" />}
+        buttonText={<>Add Project <span className="emoji bento-box" role="img" aria-label="" /></>}
         passToggleToPop
-      />
-      
-      <PopoverContainer>
-        {({ visible, togglePopover }) => (
-          <div className="button-wrap">
-            
-            <button
-              className=
-              onClick={togglePopover}
-            >
-              Add Project{" "}
-              <span className="emoji bento-box" role="img" aria-label="" />
-            </button>
-            {visible && (
-              <AddTeamProjectPop {...props} togglePopover={togglePopover} />
-            )}
-          </div>
-        )}
-      </PopoverContainer>
+      >
+        <AddTeamProjectPop {...props} />
+      </PopoverWithButton>
     </section>
   );
 };
