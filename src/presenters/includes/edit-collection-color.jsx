60039ef0a57c05690a1c361634d822063477da8f
Glitch (interesting-riddle) <none>
2018-12-18 02:02:22 +0000

    edit collection color done

--- src/presenters/includes/edit-collection-color.jsx
+++ src/presenters/includes/edit-collection-color.jsx
@@ -1,42 +1,22 @@
-
 import React from "react";
 import PropTypes from "prop-types";
 
 import EditCollectionColorPop from "../pop-overs/edit-collection-color-pop.jsx";
-import PopoverWithButton from '../pop-overs/popover-with-button';
+import PopoverWithButton from "../pop-overs/popover-with-button";
 
-class EditCollectionColor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false
-    };
-
-    this.togglePopover = this.togglePopover.bind(this);
-  }
-
-  togglePopover() {
-    this.setState({ visible: !this.state.visible });
-  }
-
-  render() {
-    const { update, initialColor, ...props } = this.props;
-    return (
-      // wrapper class edit-collection-color-btn
-      <PopoverWithButton
-        buttonText="Color"
-        buttonClass="edit-collection-color-btn"
-        passToggleToPop
-      >
-        <EditCollectionColorPop
-          {...props}
-          updateColor={update}
-          initialColor={initialColor}
-        />
-      </PopoverWithButton>
-    );
-  }
-}
+const EditCollectionColor = ({ update, initialColor, ...props }) => (
+  <PopoverWithButton
+    buttonText="Color"
+    buttonClass="edit-collection-color-btn"
+    passToggleToPop
+  >
+    <EditCollectionColorPop
+      {...props}
+      updateColor={update}
+      initialColor={initialColor}
+    />
+  </PopoverWithButton>
+);
 
 export default EditCollectionColor;
 
