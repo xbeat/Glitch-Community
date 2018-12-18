c03e59b0aec4469ddab6833205091586be4577c7
Glitch (interesting-riddle) <none>
2018-12-18 18:12:33 +0000

    ☠️🏠 Checkpoint ./src/presenters/pop-overs/report-abuse-pop.jsx:708175/33

--- src/presenters/pop-overs/report-abuse-pop.jsx
+++ src/presenters/pop-overs/report-abuse-pop.jsx
@@ -255,7 +255,7 @@ ${secondHalf}`;
       <>
       <p className="pop-description">But you can still send us your message! Just copy the details below and email us at <b>support@glitch.com</b> and we'll respond right away.
         Here's some text to include:</p>
-      <textarea value={this.formatRaw()} /></>
+      <textarea className="content-editable" value={this.formatRaw()} /></>
     );
   }
 
