badf074b951a4a5ea49bc54871e2fb14f55c5ce1
Glitch (interesting-riddle) <none>
2018-12-18 14:49:27 +0000

    😺🌋 Checkpoint ./src/presenters/includes/add-team-project.jsx:708175/15902

--- src/presenters/includes/add-team-project.jsx
+++ src/presenters/includes/add-team-project.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import AddTeamProjectPop from '../pop-overs/add-team-project-pop.jsx';
-import PopoverContainer from '../pop-overs/popover-container.jsx';
+import AddTeamProjectPop from "../pop-overs/add-team-project-pop.jsx";
+import PopoverContainer from "../pop-overs/popover-container.jsx";
 
-const AddTeamProject = ({currentUserIsOnTeam, ...props}) => {
-  if(!currentUserIsOnTeam) {
+const AddTeamProject = ({ currentUserIsOnTeam, ...props }) => {
+  if (!currentUserIsOnTeam) {
     return null;
   }
-  
+
   return (
     <section className="add-project-container">
+      {/* Add disabled={props.projectLimitIsReached} once billing is ready */}
+      <PopoverWithButton
+        buttonClass={`button add-project has-emoji opens-pop-over ${props.extraButtonClass}`}
+        buttonText={<div className="down-arrow" aria-label="options" />}
+        passToggleToPop
+      />
+      
       <PopoverContainer>
-        {({visible, togglePopover}) => (
+        {({ visible, togglePopover }) => (
           <div className="button-wrap">
-            {/* Add disabled={props.projectLimitIsReached} once billing is ready */}
-            <button className={`button add-project has-emoji opens-pop-over ${props.extraButtonClass}`} onClick={togglePopover}>
-              Add Project <span className="emoji bento-box" role="img" aria-label="" />
+            
+            <button
+              className=
+              onClick={togglePopover}
+            >
+              Add Project{" "}
+              <span className="emoji bento-box" role="img" aria-label="" />
             </button>
-            { visible && <AddTeamProjectPop {...props} togglePopover={togglePopover} /> }
+            {visible && (
+              <AddTeamProjectPop {...props} togglePopover={togglePopover} />
+            )}
           </div>
         )}
       </PopoverContainer>
@@ -31,7 +44,7 @@ AddTeamProject.propTypes = {
   addProject: PropTypes.func.isRequired,
   teamProjects: PropTypes.array.isRequired,
   extraButtonClass: PropTypes.string,
-  api: PropTypes.func.isRequired,
+  api: PropTypes.func.isRequired
 };
 
 export default AddTeamProject;
