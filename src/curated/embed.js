204a2883db913376e4b5588db7df14bcf9065b33
Glitch (community) <none>
2018-12-24 18:52:36 +0000

    🐑🎣 Checkpoint ./src/curated/embed.js:11/727

--- src/curated/embed.js
+++ src/curated/embed.js
@@ -1,10 +1,10 @@
 export default `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
+<div class="glitch-embed-wrap" style="height: 400px; width: 100%;">
   <iframe
     allow="geolocation; microphone; camera; midi; encrypted-media"
-    src="https://glitch.com/embed/#!/embed/spiral-square?path=README.md&previewSize=100"
-    alt="spiral-square on Glitch"
+    src="https://glitch.com/embed/#!/embed/snowy-island?path=index.html&previewSize=100"
+    alt="snowy-island on Glitch"
     style="height: 100%; width: 100%; border: 0;">
   </iframe>
 </div>
