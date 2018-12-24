27fc55c5052869781fd0cbce2e85f61391f66939
Glitch (community) <none>
2018-12-24 18:41:36 +0000

    ☎️🖖 Checkpoint ./src/curated/featured.js:11/587

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -17,12 +17,12 @@ export default [
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffood-hype.jpg?1540206366423",
     link: 'https://glitch.com/~food-hype'
   },{
-    title: 'Colorful Flower Spirals',
+    title: 'Bling-bling!',
     img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fcrystal-generator.jpg?1538392448859",
     link: 'https://generative-crystals.glitch.me'
   },{
-    title: 'Do You Want To Build A Snowman?',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F6_2.jpg?1545054764300",
-    link: 'https://glitch.com/~aframe-supercraft-christmas'
+    title: 'Cool Quilts to Stay Warm',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F6_quilt-generator.png?1541419749609",
+    link: 'https://quilt-generator.glitch.me/'
   }  
 ];
