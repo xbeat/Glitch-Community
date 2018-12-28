98072ca8b1c1f8fea84089853081fcfe93a605c8
Glitch (hot-pearl) <none>
2018-12-28 21:27:24 +0000

    ✂️🐇 Checkpoint ./src/presenters/analytics.jsx:334152/54

--- src/presenters/analytics.jsx
+++ src/presenters/analytics.jsx
@@ -18,6 +18,7 @@ const resolveProperties = (properties={}, inheritedProperties) => {
 // stick this in the tree to add a property value to any tracking calls within it
 export const AnalyticsContext = ({children, properties, context}) => (
   <Context.Consumer>
+    {console.log('analyticscontext', context)}
     {inherited => (
       <Context.Provider value={{
         properties: resolveProperties(properties, inherited.properties),
