30c1f0ae102e7d3b19538dd83957d4fbd434cfbe
Glitch (community) <none>
2018-12-31 12:20:03 +0000

    🚕🕌 Checkpoint ./src/curated/featured.js:725013/798

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,16 +13,16 @@ Example:
 
 export default [
   {
-    title: 'Let me take a selfie',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F5_coherent-closet.png?1541419749445",
-    link: 'https://glitch.com/~facebox'
+    title: 'Play Piano Like a Virtuoso',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F1_2.jpg?1545054763363",
+    link: 'https://glitch.com/~piano-genie'
   },{
-    title: 'The Wikipedia Game',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fwiki-game.jpg?1537183217930",
-    link: 'https://wikipedia-game.glitch.me/'
+    title: 'Spot The Fake Headline',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FLivingwhileblack_1.png?1537791215940",
+    link: 'https://livingwhileblack.glitch.me/'
   },{
-    title: 'Like Shazam for your face',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FRecordPlayer.jpg?1540832912802",
-    link: 'https://record-player.glitch.me'
+    title: 'Make Your Own Magic Eye Drawings',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fmagic-eye_GLITCH.png?1543234498314",
+    link: 'https://magic-eye.glitch.me'
   }  
 ];
