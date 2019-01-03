7b8fbccf06b587af1c594165cd270d5dd4c16151
Glitch (community) <none>
2019-01-03 22:44:08 +0000

    🌖🍧 Checkpoint ./styles/includes/featured.styl:867163/271 ./src/presenters/featured-embed.jsx:867163/239 ./src/curated/featured-embed.js:867163/8

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -13,12 +13,12 @@ Example:
 
 var embed = `
 <!-- Copy and Paste Me -->
-<div class="glitch-embed-wrap" style="height: 420px; width: 50%;">
+<div class="glitch-embed-wrap" style="height: 420px; width: 100%;">
   <iframe
     allow="geolocation; microphone; camera; midi; encrypted-media"
     src="https://glitch.com/embed/#!/embed/egg-drop?path=README.md&previewSize=100"
     alt="egg-drop on Glitch"
-    style="height: 100%; width: 50%; border: 0;">
+    style="height: 100%; width: 100%; border: 0;">
   </iframe>
 </div>
 `;
--- src/presenters/featured-embed.jsx
+++ src/presenters/featured-embed.jsx
@@ -11,12 +11,16 @@ const FeaturedEmbed = ({feature}) => (
       <div className="mask-container">
         <img className='mask mask-4' src={feature.feature_image} alt=""/>
       </div>
-      <div className="body">
-        <Link to={`culture${feature.url}`}>
-          <h1>{feature.title}</h1>
-        </Link>
-        <p>{feature.body}</p>
-        <span dangerouslySetInnerHTML={{__html: feature.embed}}/>
+      <div className="content">
+        <div className="description">
+          <Link to={`culture${feature.url}`}>
+            <h1>{feature.title}</h1>
+          </Link>
+          <p>{feature.body}</p>
+        </div>
+        <div className="embed">
+          <span dangerouslySetInnerHTML={{__html: feature.embed}}/>
+        </div>
       </div>
   </div>
   );
