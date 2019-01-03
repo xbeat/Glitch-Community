64f893e2314de09717252816c68da6ec4b6dbd6b
Glitch (community) <none>
2019-01-03 12:34:50 +0000

    👴📮 Checkpoint ./src/curated/featured.js:725013/869

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -13,16 +13,16 @@ Example:
 
 export default [
   {
-    title: 'Play Piano Like a Virtuoso',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F1_2.jpg?1545054763363",
-    link: 'https://glitch.com/~piano-genie'
+    title: 'Venn Diagram Simulation',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2F4_spontaneous-venning.png?1541419749124",
+    link: 'https://spontaneous-venning.glitch.me/'
   },{
-    title: 'Spot The Fake Headline',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FLivingwhileblack_1.png?1537791215940",
-    link: 'https://livingwhileblack.glitch.me/'
+    title: 'This is Your Moment of Zen',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FYourMomentofZen.jpg?1541072010180",
+    link: 'https://your-moment-of-zen.glitch.me/'
   },{
     title: 'Make Your Own Magic Eye',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fmagic-eye_GLITCH.png?1543234498314",
-    link: 'https://magic-eye.glitch.me'
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fpiano-scribe.jpg?1538999010225",
+    link: 'https://piano-scribe.glitch.me/'
   }  
 ];
