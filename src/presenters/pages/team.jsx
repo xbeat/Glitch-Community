25e7452c600bb120631a11acba0d5f9d2a21bb28
Glitch (superb-guide) <none>
2019-01-08 22:57:28 +0000

    🍃💀 Checkpoint ./src/presenters/pages/team.jsx:97325/50

--- src/presenters/pages/team.jsx
+++ src/presenters/pages/team.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import Helmet from 'react-helmet';
 import {AnalyticsContext} from '../analytics';
-import DevToggles from '../includes/dev-toggles';
 import {CurrentUserConsumer} from '../current-user';
 import {DataLoader} from '../includes/loader';
 import TeamEditor from '../team-editor.jsx';
