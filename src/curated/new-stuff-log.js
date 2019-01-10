46f967efccfe73e908573e17e0c26fb6cd9373c1
Glitch (collection-pupdate) <none>
2019-01-10 17:03:35 +0000

    👕🏦 Checkpoint ./src/curated/new-stuff-log.js:867163/478

--- src/curated/new-stuff-log.js
+++ src/curated/new-stuff-log.js
@@ -5,21 +5,12 @@
 
 export default [
   {
-    id: 2,
+    id: 1,
     title: "Get organized with Collections",
     body: `
 Add any project to a collection and show off your favorite things. Head over to your profile or team page to create and view your collections.
 
 ![](https://cdn.glitch.com/e9551d04-9c48-4dbb-ab1b-04eca25b9652%2Fimage.png?1547136745894)
 `,
-  },
-  {
-    id: 1,
-    title: "Glitch for Teams",
-    body: `
-Soon, we’ll provide Glitch to teams at work, so developers, designers and anyone with an idea can create real, full-power apps incredibly easily.
-
-But we’re counting on you to help us define what that offering will include. [Sign up](https://glitch.com/teams) and let us know what you need from Glitch in the workplace, be the first to hear what’s coming to Glitch for Teams, and help inform its development.
-`,
-  },
+  }
 ];
