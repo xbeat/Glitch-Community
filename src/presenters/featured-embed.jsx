18f282f90fdfb32003d28192b76110db28aa8671
Glitch (community-staging) <none>
2019-01-10 15:20:54 +0000

    💦🎤 Checkpoint ./src/presenters/featured-embed.jsx:867163/280

--- src/presenters/featured-embed.jsx
+++ src/presenters/featured-embed.jsx
@@ -3,38 +3,42 @@ import PropTypes from 'prop-types';
 
 import Link from './includes/link.jsx';
 
-const FeaturedEmbed = ({feature}) => (
+const FeaturedEmbed = ({
+  feature: {url, image, title, body, embed, mask, color}
+}) => (
   <div className="featured-embed">
     <div className="mask-container">
-      <Link to={`culture${feature.url}`}>
-        <img className={'mask ' + feature.mask} src={feature.image} alt=""/>
+      <Link to={`culture${url}`}>
+        <img className={'mask ' + mask} src={image} alt=""/>
       </Link>
     </div>
       
-    <div className="content" style={{backgroundColor: feature.color}}>
+    <div className="content" style={{backgroundColor: color}}>
       <div className="description">
-        <Link to={`culture${feature.url}`}>
-          <h1>{feature.title}</h1>
+        <Link to={`culture${url}`}>
+          <h1>{title}</h1>
         </Link>
-        <p dangerouslySetInnerHTML={{__html: feature.body}}/>
-        <Link to={`culture${feature.url}`} className="learn-more">
+        <p dangerouslySetInnerHTML={{__html: body}}/>
+        <Link to={`culture${url}`} className="learn-more">
           <button className="button-small">Learn More →</button>
         </Link>
       </div>
       <div className="embed">
-        <span dangerouslySetInnerHTML={{__html: feature.embed}}/>
+        <span dangerouslySetInnerHTML={{__html: embed}}/>
       </div>
     </div>
   </div>
 );
 
 FeaturedEmbed.propTypes = {
-  feature: PropTypes.object({
+  feature: PropTypes.shape({
     url: PropTypes.string.isRequired,
-    featured_image: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
     embed: PropTypes.string.isRequired,
+    mask: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
   }).isRequired,
 };
 
