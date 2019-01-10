74d89dfe2d0e3cd160621a01651d7ce3b75bf41b
Glitch (collection-pupdate) <none>
2019-01-10 16:30:36 +0000

    🤘🐇 Checkpoint ./src/curated/new-stuff-log.js:867163/869

--- src/curated/new-stuff-log.js
+++ src/curated/new-stuff-log.js
@@ -58,24 +58,4 @@ Soon, we’ll provide Glitch to teams at work, so developers, designers and anyo
 But we’re counting on you to help us define what that offering will include. [Sign up](https://glitch.com/teams) and let us know what you need from Glitch in the workplace, be the first to hear what’s coming to Glitch for Teams, and help inform its development.
 `,
   },
-  {
-    id: 1,
-    title: "Deleted, but not forgotten",
-    body: `
-You can now delete _and undelete_ projects. 🎉
-
-Tidy up your profile page without fear of regret.
-There's a new "Delete This" button on your project tiles that'll let you clean up 
-your old experiments and help you present a healthier, more active looking profile page. 🥦
-
-![Delete using the project's dropdown list](https://cdn.glitch.com/03736932-82dc-40e8-8dc7-93330c933143%2Fnew-stuff-delete.png?1518549490386)
-
-All of your deleted projects are listed for you at the bottom of your profile page,
-and you can restore them to life with just one click.
-
-![Undelete projects using the new controls at the bottom of your profile page](https://cdn.glitch.com/03736932-82dc-40e8-8dc7-93330c933143%2Fnew-stuff-undelete.png?1518552423035)
-
-No forever-💔 here, every project has a second chance.
-`,
-  },
 ];
