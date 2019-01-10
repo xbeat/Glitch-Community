1fbda93ee07ca049070ce0fde79a6d43b18ed757
Glitch (community-staging) <none>
2019-01-10 15:31:16 +0000

    🏔🕌 Checkpoint ./src/presenters/featured-embed.jsx:867163/577 ./src/presenters/featured.jsx:867163/61

--- src/presenters/featured-embed.jsx
+++ src/presenters/featured-embed.jsx
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 
 import Link from './includes/link.jsx';
 
-const FeaturedEmbed = ({
-  feature: {url, image, title, body, embed, mask, color}
-}) => (
+const FeaturedEmbed = ({image, mask, title, url, embed, body, color}) => (
   <div className="featured-embed">
     <div className="mask-container">
       <Link to={`culture${url}`}>
@@ -30,16 +28,14 @@ const FeaturedEmbed = ({
   </div>
 );
 
-FeaturedEmbed.propTypes = {
-  feature: PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired,
-    embed: PropTypes.string.isRequired,
-    mask: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-  }).isRequired,
+FeaturedEmbed.propTypes = {  
+  image: PropTypes.string.isRequired,
+  mask: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  embed: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
 };
 
 export default FeaturedEmbed;
