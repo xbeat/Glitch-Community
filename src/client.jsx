0ed9f92f0e0f13e5435a0406c4ac75092027dd14
Glitch (community) <none>
2019-01-17 18:47:35 +0000

    👕👋 Checkpoint ./src/client.jsx:2833158/24

--- src/client.jsx
+++ src/client.jsx
@@ -18,14 +18,14 @@ import App from './app.jsx';
 // Here's a bunch of browser support tests
 // If any of them don't work we can't run in this browser
 /* eslint-disable no-unused-vars */
-let x = {a: 1, b: 2}; // Can we use let?
-const y = [1, 2, 3]; // Can we use const?
-const {a, ...aRest} = x; // Can we use object destructuring?
-const [b, ...bRest] = y; // Can we use array destructuring?
-const str = `${b}23`; // Can we use formatted strings?
-const func = (f, ...args) => f(...args); // Can we define arrow functions?
-func(async arg => await arg, Promise.resolve()); // Can we do async/await?
-new URLSearchParams(); // Do we have URLSearchParams? 
+// let x = {a: 1, b: 2}; // Can we use let?
+// const y = [1, 2, 3]; // Can we use const?
+// const {a, ...aRest} = x; // Can we use object destructuring?
+// const [b, ...bRest] = y; // Can we use array destructuring?
+// const str = `${b}23`; // Can we use formatted strings?
+// const func = (f, ...args) => f(...args); // Can we define arrow functions?
+// func(async arg => await arg, Promise.resolve()); // Can we do async/await?
+// new URLSearchParams(); // Do we have URLSearchParams? 
 /* eslint-enable no-unused-vars */
 
 
