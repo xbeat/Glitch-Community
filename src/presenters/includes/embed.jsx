6ebfde08a886a8208cc888f013084c30296b06c9
Glitch (mountainous-pixie) <none>
2019-01-18 17:06:30 +0000

    🍧⛽️ Checkpoint ./src/presenters/includes/embed.jsx:252452/235

--- src/presenters/includes/embed.jsx
+++ src/presenters/includes/embed.jsx
@@ -3,18 +3,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Loader = () => (
-  <div className="loader">
-    <div className="moon"></div>
-    <div className="earth"></div>
-    <div className="asteroid"></div>    
-    <div className="asteroid-dust"></div>    
-  </div>    
-);
-
 export class Embed extends React.Component {
   render() {
-    <div className="glitch-embed-wrap">
+    return <div className="glitch-embed-wrap">
       <iframe title="embed"
         src={`${APP_URL}/embed/#!/embed/${this.props.domain}?path=README.md&previewSize=100`}
         allow="geolocation; microphone; camera; midi; encrypted-media"
