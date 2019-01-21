26cd7aee6f2fa981fe57c3ea493e36db4e49028c
Glitch (superb-guide) <none>
2019-01-21 19:03:47 +0000

    🎾🚵 Checkpoint ./src/curated/collections.js:97325/749

--- src/curated/collections.js
+++ src/curated/collections.js
@@ -1,5 +1,11 @@
+/// Collections shown on the homepage and in the more ideas area
 
+// What collections to show in the expanded area on the homepage
+export const featuredCollections = [
+  { user: 'scientiffic', url: 'microbit' },
+  { team: 'gregs-test-team-1', url: 'zany-playlist' },
+];
 
+// More ideas is populated from this user/team
+// Pull from here if there are less than 3 featured collections
 export const moreIdeasTeam = 'glitch';
-
-export const 
