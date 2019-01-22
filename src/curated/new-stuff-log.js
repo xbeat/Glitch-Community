832fd898d7570f0cd101072c91e2cb6751e37dfa
Glitch (community) <none>
2019-01-22 21:28:55 +0000

    🍟👳 Checkpoint ./src/curated/new-stuff-log.js:9/419 ./.glitch-assets:9/505

--- src/curated/new-stuff-log.js
+++ src/curated/new-stuff-log.js
@@ -8,11 +8,11 @@ export default [
     id: 1,
     title: "Use your email to get Glitchy",
     body: `
-Don't have Facebook or GitHub, but still want to use Glitch? 
+Don't have Facebook or GitHub, but still want to use Glitch? Want to code at the library without remembering all your logins?
 
-Head over to your profile or team page to create and view your collections.
+Now you can sign into Glitch with a one-time code sent to your email!
 
-![](https://cdn.glitch.com/e9551d04-9c48-4dbb-ab1b-04eca25b9652%2Fimage.png?1547136745894)
+![](https://cdn.glitch.com/69bf2d27-a029-4632-86a4-a722974778ae%2FScreen%20Shot%202019-01-22%20at%204.26.50%20PM.png?1548192423838)
 `,
   },
   {
