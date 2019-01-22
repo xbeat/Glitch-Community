b52b748416d8bbcdfaa8a2982659a876eba5d108
Glitch (community) <none>
2019-01-22 20:55:55 +0000

    😺🏞 Checkpoint ./src/curated/new-stuff-log.js:9/447

--- src/curated/new-stuff-log.js
+++ src/curated/new-stuff-log.js
@@ -4,8 +4,19 @@
 // prepend new updates
 
 export default [
-  {
+    {
     id: 1,
+    title: "Use your email to get Glitchy",
+    body: `
+Add any project to a collection and show off your favorite things, group all your related projects, collect precious memories, etc. 
+
+Head over to your profile or team page to create and view your collections.
+
+![](https://cdn.glitch.com/e9551d04-9c48-4dbb-ab1b-04eca25b9652%2Fimage.png?1547136745894)
+`,
+  },
+  {
+    id: 2,
     title: "Get organized with Collections",
     body: `
 Add any project to a collection and show off your favorite things, group all your related projects, collect precious memories, etc. 
