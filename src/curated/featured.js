18a9efa5772d68d945487c94fa62edce2aaa48ef
Glitch (community-staging) <none>
2019-01-24 12:08:32 +0000

    🛬⭐️ Checkpoint ./src/curated/featured.js:725013/49

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -21,7 +21,7 @@ export default [
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch05_lmo-feeder.png?1548074746382",
     link: 'https://glitch.com/~lmo-feeder'
   },{
-    title: 'How Would You Stack Your Oreo?',
+    title: 'Pictionary, Anyone?',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch06_technology-pictionary.png?1548074745528",
     link: 'https://technology-pictionary.glitch.me/'
   }  
