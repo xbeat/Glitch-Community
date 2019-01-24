8bbe4ee61d7b1ad0b4e7116645120dc2219c8886
Glitch (community-staging) <none>
2019-01-24 14:22:42 +0000

    💋🚦 Checkpoint ./.glitch-assets:11/436 ./src/curated/featured.js:11/1321

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -17,12 +17,12 @@ export default [
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch04_zealous-mammal.png?1548074746239",
     link: 'https://glitch.com/~zealous-mammal'
   },{
+    title: '',
+    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FtwitchLiveStream.png?1548339656179",
+    link: 'https://www.twitch.tv/events/dppl6CecTFmB67JTc8sfSg'
+  },{
     title: 'Make Your Own RSS Reader',
     img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch05_lmo-feeder.png?1548074746382",
     link: 'https://glitch.com/~lmo-feeder'
-  },{
-    title: 'Pictionary, Anyone?',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch06_technology-pictionary.png?1548074745528",
-    link: 'https://technology-pictionary.glitch.me/'
-  }  
+  } 
 ];
