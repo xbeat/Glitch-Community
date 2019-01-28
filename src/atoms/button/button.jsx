a95b59235807cf4f6ef2b8812c97e9de73ca0ba5
Glitch (splendid-carol) <none>
2019-01-28 18:39:04 +0000

    🙄🌎 Checkpoint ./src/atoms/button/button.jsx:252452/256 ./package.json:252452/63

--- src/atoms/button/button.jsx
+++ src/atoms/button/button.jsx
@@ -5,20 +5,22 @@ import './button.css'
 const TYPES = ["TERTIARY", "CTA", "REGULAR"]
 const SIZES = ["SMALL", "REGULAR"]
 
-Button.defaultProps({
-  type: "REGULAR",
-  size: "REGULAR",
-  emoji: "none"
-});
-
 /**
  * Button Component
  */
-const Button = ({ onClick, disabled, type, size, emoji }) => {
+const Button = ({ onClick, disabled, type, size, emoji, children }) => {
   return (
-    <button onClick={onClick} disabled={disabled} type={type} size={size} emoji={emoji} />
+    <button onClick={onClick} disabled={disabled} type={type} size={size} emoji={emoji} children={children}>
+      {children}
+    </button>
   )
 }
+
+Button.defaultProps = {
+  type: "REGULAR",
+  size: "REGULAR",
+  emoji: "none"
+};
 
 Button.propTypes = {
   /** callback when button clicked */
