9894f963d1f1bf190f33bc2ac9df5604ce87363b
Glitch (superb-guide) <none>
2019-01-28 22:26:10 +0000

    🌷🏁 Checkpoint ./src/presenters/featured-collections.jsx:97325/2839 ./src/curated/collections.js:97325/1

--- src/presenters/featured-collections.jsx
+++ src/presenters/featured-collections.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {sampleSize} from 'lodash';
-import {captureException} from '../utils/sentry';
 
 import {featuredCollections} from '../curated/collections';
 import {getContrastTextColor} from '../models/collection';
@@ -55,24 +54,24 @@ CollectionWide.propTypes = {
   api: PropTypes.any.isRequired,
 };
 
-const loadCollection = async info => {
+const loadCollection = async (api, info) => {
   let collections = [];
   if (info.team) {
-    const {data: teamId} = await this.props.api.get(`teamid/byUrl/${info.team}`);
+    const {data: teamId} = await api.get(`teamid/byUrl/${info.team}`);
     if (teamId !== 'NOT FOUND') {
-      const {data} = await this.props.api.get(`collections?teamId=${teamId}`);
+      const {data} = await api.get(`collections?teamId=${teamId}`);
       collections = data;
     }
   } else if (info.user) {
-    const {data: userId} = await this.props.api.get(`userid/byLogin/${info.user}`);
+    const {data: userId} = await api.get(`userid/byLogin/${info.user}`);
     if (userId !== 'NOT FOUND') {
-      const {data} = await this.props.api.get(`collections?userId=${userId}`);
+      const {data} = await api.get(`collections?userId=${userId}`);
       collections = data;
     }
   }
   const collection = collections.find(c => c.url === info.url);
   if (collection) {
-    const {data} = await this.props.api.get(`collections/${collection.id}`);
+    const {data} = await api.get(`collections/${collection.id}`);
     data.projectCount = data.projects.length;
     data.projects = sampleSize(data.projects, 3).map(p => ({...p, users: p.users||[]}));
     return data;
@@ -80,73 +79,18 @@ const loadCollection = async info => {
   return null;
 };
 
-export const FeaturedCollections = ({api}) => (
-  <>
-    {featuredCollections.map(info => (
-     <DataLoader get={() => loadCollection(info)}>
-       {collection => !!collection && (
-         
-       )}
-     </DataLoader>
-    ))}
-  </>
-);
+const loadAllCollections = async (api, infos) => {
+  const promises = infos.map(info => loadCollection(api, info));
+  return await Promise.all(promises);
+};
 
-class FeaturedCollections extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: {},
-      done: false,
-      error: false,
-    };
-  }
-  
-  async loadCollection(n, info) {
-    try {
-      let collections = [];
-      if (info.team) {
-        const {data: teamId} = await this.props.api.get(`teamid/byUrl/${info.team}`);
-        if (teamId !== 'NOT FOUND') {
-          const {data} = await this.props.api.get(`collections?teamId=${teamId}`);
-          collections = data;
-        }
-      } else if (info.user) {
-        const {data: userId} = await this.props.api.get(`userid/byLogin/${info.user}`);
-        if (userId !== 'NOT FOUND') {
-          const {data} = await this.props.api.get(`collections?userId=${userId}`);
-          collections = data;
-        }
-      }
-      const collection = collections.find(c => c.url === info.url);
-      if (collection) {
-        const {data} = await this.props.api.get(`collections/${collection.id}`);
-        data.projectCount = data.projects.length;
-        data.projects = sampleSize(data.projects, 3).map(p => ({...p, users: p.users||[]}));
-        this.setState(({collections}) => ({collections: {...collections, [n]: data}}));
-      }
-    } catch (error) {
-      console.log(error);
-      captureException(error);
-      throw error;
-    }
-  }
-  
-  componentDidMount() {
-    const loaders = featuredCollections.map((info, n) => this.loadCollection(n, info));
-    Promise.all(loaders).then(() => {
-      
-    }, () => {
-    });
-  }
-  
-  render() {
-    const collections = Array.from(featuredCollections.keys()).map(n => this.state.collections[n]);
-    return collections.filter(c => !!c).map(collection => (
-      <CollectionWide key={collection.id} collection={collection} api={this.props.api}/>
-    ));
-  }
-}
+export const FeaturedCollections = ({api}) => (
+  <DataLoader get={() => loadAllCollections(api, featuredCollections)}>
+    {collections => collections.filter(c => !!c).map(collection => (
+      <CollectionWide collection={collection} api={api} key={collection.id}/>
+    ))}
+  </DataLoader>
+);
 
 FeaturedCollections.propTypes = {
   api: PropTypes.any.isRequired,
