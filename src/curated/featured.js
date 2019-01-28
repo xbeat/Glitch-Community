25c182a607275b7bf7b4950191efd22ee4739c37
Glitch (community) <none>
2019-01-28 13:31:12 +0000

    👻🏡 Checkpoint ./.glitch-assets:725013/2478 ./src/curated/featured.js:725013/382

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -14,11 +14,11 @@ Example:
 export default [
   {
     title: 'Wave Function Maps',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch04_zealous-mammal.png?1548074746239",
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fbells.jpg?1548682192588",
     link: 'https://glitch.com/~zealous-mammal'
   },{
     title: 'What Am I Drawing?',
-    img: "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fglitch06_technology-pictionary.png?1548074745528",
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fopml.jpg?1548682192922",
     link: 'https://technology-pictionary.glitch.me'
   },{
     title: 'Make Your Own RSS Reader',
