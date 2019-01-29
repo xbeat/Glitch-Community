c7db52a6b3889b0763736a9ffba3c2aa5b38e957
Glitch (earthy-submarine) <none>
2019-01-29 21:59:36 +0000

    😭🛥 Checkpoint ./src/curated/new-stuff-log.js:867163/293

--- src/curated/new-stuff-log.js
+++ src/curated/new-stuff-log.js
@@ -9,7 +9,7 @@ export default [
     title: "Featured projects now live on your profile",
     body: `Have a project you're particularly proud of?  Feature it to embed the app directly at the top of your profile.
 
-You can only choose one featured project at a time, so pick wisely...🎬
+You can feature one project at a time – and change or remove your featured project as often as you'd like.
 
 ![](https://cdn.glitch.com/1b4c205a-e6b6-4ad9-ae8a-1e511e611105%2Ffeature.png?1548796979652)
 `,
