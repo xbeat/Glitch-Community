9e79518eb43c5d9182d3ffeaf89c97a09849f087
Glitch (community) <none>
2019-01-30 21:04:58 +0000

    🍈💴 Checkpoint ./src/curated/featured-embed.js:725013/345

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -1,13 +1,13 @@
 var body = `<p>Follow along with MythBusters Jr. by remixing your own Glitch apps inspired by each episode.</p>
-<p><b>Episode 5:</b></p>
-<p>How long of a line can a standard no. 2 pencil make?</p>`;
+<p><b>Episode 6:</b></p>
+<p>Is it really impossible to make mayonnaise in a lightning storm?</p>`;
 
 export default {
-  "image": "http://culture-zine.glitch.me/culture/content/images/2019/01/episode-5-header.jpg",
+  "image": "http://culture-zine.glitch.me/culture/content/images/2019/01/episode-6-electric-mayo.jpg",
   "mask": "mask-4",
   "title": "MythBusters Jr.",
-  "appDomain": "pencil-distance",
-  "blogUrl": "/gravity-busters/",
+  "appDomain": "mayo-hero",
+  "blogUrl": "/paper-shred-ka-boom/",
   "body": body,
   "color": '#C9BFF4',
 };
