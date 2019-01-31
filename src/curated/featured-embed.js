81ac019434cc6bff4a7bbf3de4a988431c93d6e8
Glitch (nickel-dill) <none>
2019-01-31 03:41:08 +0000

    👟👢 Checkpoint ./src/curated/featured-embed.js:252452/14

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -1,6 +1,6 @@
 var body = `<p>Follow along with MythBusters Jr. by remixing your own Glitch apps inspired by each episode.</p>
 <p><b>Episode 6:</b></p>
-<p>Is it really impossible to make mayo in a lightning storm?</p>`;
+<p>Is it really impossible to make mayonnaise in a lightning storm?</p>`;
 
 export default {
   "image": "http://culture-zine.glitch.me/culture/content/images/2019/01/episode-6-electric-mayo.jpg",
