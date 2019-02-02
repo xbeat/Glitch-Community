6365355affe6fe27cf063c6ac5067ade3bd0733e
Glitch (splendid-carol) <none>
2019-02-02 14:23:41 +0000

    🚇🛫 Checkpoint ./src/atoms/button/button.jsx:252452/34

--- src/atoms/button/button.jsx
+++ src/atoms/button/button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import styles from '../../../styles/atoms/button/button.styl';
+import styles from './button.css';
 
 let cx = classNames.bind(styles);
 
