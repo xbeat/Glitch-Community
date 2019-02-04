b76fdb95e6aa40c990a377e9e72e33bfb65d5116
Glitch (community) <none>
2019-02-04 13:39:02 +0000

    ❤️🎪 Checkpoint ./src/curated/featured-embed.js:725013/372

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -1,13 +1,13 @@
 var body = `<p>Follow along with MythBusters Jr. by remixing your own Glitch apps inspired by each episode.</p>
-<p><b>Episode 6:</b></p>
-<p>Is it really impossible to make mayonnaise in a lightning storm?</p>`;
+<p><b>Episode 7:</b></p>
+<p>How much reliable energy can be harnessed from a human fart?</p>`;
 
 export default {
-  "image": "http://culture-zine.glitch.me/culture/content/images/2019/01/episode-6-electric-mayo.jpg",
+  "image": "http://culture-zine.glitch.me/culture/content/images/2019/01/episode-8-flatus.jpg",
   "mask": "mask-4",
   "title": "MythBusters Jr.",
-  "appDomain": "mayo-hero",
-  "blogUrl": "/paper-shred-ka-boom/",
+  "appDomain": "whoopie-cushion",
+  "blogUrl": "/flatus-special/",
   "body": body,
-  "color": '#C9BFF4',
+  "color": '#F87D93',
 };
