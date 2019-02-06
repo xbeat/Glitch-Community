440ecbd650e1c068bf3d5a027c1b4f458ee91d7c
Glitch (splendid-carol) <none>
2019-02-06 21:59:46 +0000

    🐽🌛 Checkpoint ./sh/watchers.sh:252452/9

--- sh/watchers.sh
+++ sh/watchers.sh
@@ -2,6 +2,6 @@
 set -e
 
 # ESLint the server files:
-nodemon --max_old_space_size= --watch server --watch shared --exec "eslint --config server/.eslintrc.server.js server shared webpack.config.js" &
+nodemon --max-old-space-size=500 --watch server --watch shared --exec "eslint --config server/.eslintrc.server.js server shared webpack.config.js" &
   
 wait
