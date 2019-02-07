48752ba775db3be02d236941d256effc1d9331fd
Glitch (superb-guide) <none>
2019-02-07 17:12:53 +0000

    📒🏙 Checkpoint ./src/presenters/includes/add-collection-project.jsx:97325/1026

--- src/presenters/includes/add-collection-project.jsx
+++ src/presenters/includes/add-collection-project.jsx
@@ -9,33 +9,23 @@ class AddCollectionProject extends React.Component {
     super(props);
     this.state = {projects: []};
   }
-  async loadProjects() {
-    this.setState({projects: []});
-    const {api, collection, currentUser} = this.props;
+  async componentDidMount() {
+    const {api, collection} = this.props;
     if (collection.teamId > 0) {
       const {data: team} = await api.get(`teams/${collection.teamId}`);
       this.setState({projects: team.projects});
-    } else if (collection.userId === currentUser.id) {
-      this.setState({projects: currentUser.projects});
-    } else if (collection.userId > 0) {
-      const {data: user} = await api.get(`users/${collection.userId}`);
-      this.setState({projects: user.projects});
-    }
-  }
-  componentDidMount() {
-    this.loadProjects();
-  }
-  componentDidUpdate(prevProps) {
-    const collection = this.props.collection;
-    const prevCollection = prevProps.collection;
-    if (collection.teamId !== prevCollection.teamId || collection.userId !== prevCollection.userId) {
-      this.loadProjects();
     }
   }
   render() {
+    let initialProjects = [];
+    if (this.props.collection.teamId > 0) {
+      initialProjects = this.state.projects;
+    } else if (this.props.currentUser) {
+      initialProjects = this.props.currentUser.projects;
+    }
     return (
-      <PopoverWithButton buttonClass="button add-project opens-pop-over" buttonText="Add Project" passToggleToPop>
-        <AddCollectionProjectPop initialProjects={this.state.projects.slice(0,20)} {...this.props} />
+      <PopoverWithButton buttonClass="button add-project" buttonText="Add Project" passToggleToPop>
+        <AddCollectionProjectPop initialProjects={initialProjects.slice(0,20)} {...this.props} />
       </PopoverWithButton>
     );
   }
