d882afa21b156a7c9163a0039719c0d349c2a537
Glitch (community) <none>
2019-02-11 10:16:08 +0000

    💁🏵 Checkpoint ./.glitch-assets:11/2628 ./src/curated/featured.js:11/1219

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -14,16 +14,16 @@ Example:
 // make sure image urls use https
 export default [
   {
-    title: 'What\'s the Right Tea for You?',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fteaforme_final.jpg?1549538703848",
-    link: 'https://theteaforme.glitch.me/'
+    title: 'Make Inkle Patterns',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Ftracery-inkle.gif?1549879572651",
+    link: 'https://tracery-inkle.glitch.me/'
   },{
-    title: 'A Mahjong Platformer',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Ftilejumper_final.jpg?1549538704021",
-    link: 'https://tile-jumper.glitch.me/'
+    title: 'Hello, World!',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fhello-around-the-world.gif?1549879571293",
+    link: 'https://hello-around-the-world.glitch.me/'
   },{
-    title: 'Absurd Karaoke Lyrics',
-    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fsongbot_final.jpg?1549538704210",
-    link: 'https://songbot.glitch.me/'
+    title: 'Springy Particles',
+    img: "https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fimages-to-particles.gif?1549879572018",
+    link: 'https://image-to-particles.glitch.me/'
   } 
 ];
