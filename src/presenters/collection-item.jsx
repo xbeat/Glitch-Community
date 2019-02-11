80a041c35de63e87c628475858eda39a7d50ec71
Glitch (omniscient-crafter) <none>
2019-02-11 15:27:31 +0000

    🚅🙏 Checkpoint ./src/presenters/collection-item.jsx:301412/21

--- src/presenters/collection-item.jsx
+++ src/presenters/collection-item.jsx
@@ -54,7 +54,7 @@ class CollectionItem extends React.Component{
           <div className={"collection" + (isAuthorized ? " authorized" : "")} id={"collection-" + collection.id}>
             <div className="collection-container">
               <CollectionLink collection={collection} className="collection-info button-area" style={{backgroundColor: collection.coverColor}}>
-                <div className="avatar-container">
+                <div className="avatar-container" aria-hidden="true">
                   <div className="avatar">
                     <CollectionAvatar backgroundColor={hexToRgbA(collection.coverColor)} collectionId={collection.id}/>
                   </div>
