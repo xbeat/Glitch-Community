657b41dd90bd1a5bbf8e4693515a0c06f508d55f
Glitch (add-to-collection-pop) <none>
2019-02-11 15:17:40 +0000

    👧🚪 Checkpoint ./src/presenters/includes/avatar.jsx:867163/33920

--- src/presenters/includes/avatar.jsx
+++ src/presenters/includes/avatar.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import {DEFAULT_TEAM_AVATAR, getAvatarUrl as getTeamAvatarUrl} from '../../models/team';
-import {ANON_AVATAR_URL, getAvatarThumbnailUrl, getDisplayName} from '../../models/user';
+import {
+  DEFAULT_TEAM_AVATAR,
+  getAvatarUrl as getTeamAvatarUrl
+} from "../../models/team";
+import {
+  ANON_AVATAR_URL,
+  getAvatarThumbnailUrl,
+  getDisplayName
+} from "../../models/user";
 
 // UserAvatar
 
-export const Avatar = ({name, src, color, srcFallback, type, isStatic}) => {
-  var contents = (
-    <img width="32px" height="32px" src={src} alt={name}
-      style={color ? {backgroundColor: color} : null}
-      onError={srcFallback ? (event => event.target.src = srcFallback) : null}
+export const Avatar = ({ name, src, color, srcFallback, type, isStatic }) => {
+  const contents = (
+    <img
+      width="32px"
+      height="32px"
+      src={src}
+      alt={name}
+      style={color ? { backgroundColor: color } : null}
+      onError={srcFallback ? event => (event.target.src = srcFallback) : null}
       className={type + "-avatar"}
     />
   );
-  
-  if(!isStatic){
+
+  if (!isStatic) {
     <div data-tooltip={name} data-tooltip-left="true">
       {contents}
     </div>;
@@ -31,19 +42,31 @@ Avatar.propTypes = {
   isStatic: PropTypes.bool
 };
 
-export const TeamAvatar = ({team}) => (
-  <Avatar name={team.name} src={getTeamAvatarUrl({...team, size:'small'})} srcFallback={DEFAULT_TEAM_AVATAR} type="team"/>
+export const TeamAvatar = ({ team }) => (
+  <Avatar
+    name={team.name}
+    src={getTeamAvatarUrl({ ...team, size: "small" })}
+    srcFallback={DEFAULT_TEAM_AVATAR}
+    type="team"
+  />
 );
 TeamAvatar.propTypes = {
   team: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    hasAvatarImage: PropTypes.bool.isRequired,
-  }).isRequired,
+    hasAvatarImage: PropTypes.bool.isRequired
+  }).isRequired
 };
 
-export const UserAvatar = ({user, suffix='', isStatic}) => (
-  <Avatar name={getDisplayName(user) + suffix} src={getAvatarThumbnailUrl(user)} color={user.color} srcFallback={ANON_AVATAR_URL} type="user" isStatic={isStatic}/>
+export const UserAvatar = ({ user, suffix = "", isStatic }) => (
+  <Avatar
+    name={getDisplayName(user) + suffix}
+    src={getAvatarThumbnailUrl(user)}
+    color={user.color}
+    srcFallback={ANON_AVATAR_URL}
+    type="user"
+    isStatic={isStatic}
+  />
 );
 UserAvatar.propTypes = {
   user: PropTypes.shape({
@@ -51,8 +74,8 @@ UserAvatar.propTypes = {
     login: PropTypes.string,
     name: PropTypes.string,
     avatarThumbnailUrl: PropTypes.string,
-    color: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired
   }).isRequired,
   suffix: PropTypes.string,
-  isStatic: PropTypes.bool,
+  isStatic: PropTypes.bool
 };
