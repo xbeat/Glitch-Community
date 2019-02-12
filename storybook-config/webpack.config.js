85077b708b561620b5059d2bfa6113d9212485ba
Glitch (splendid-carol) <none>
2019-02-12 15:44:21 +0000

    🌒🌮 Checkpoint ./storybook-config/webpack.config.js:252452/2291

--- storybook-config/webpack.config.js
+++ storybook-config/webpack.config.js
@@ -1,35 +1,25 @@
 const path = require("path");
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const AutoprefixerStylus = require("autoprefixer-stylus");
-
-module.exports = {
-  module: {
-    rules: [
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.push({
+    test: /\.styl/,
+    include: path.resolve(__dirname, "../src/components"),
+    use: [
+      'style-loader',
       {
-        test: /\.styl/,
-        include: path.resolve(__dirname, "../src/components"),
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader?modules',
-            options: {
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            },
-          },
-          {
-            loader: 'stylus-loader',
-            options: {
-              use: [AutoprefixerStylus()],
-            },
-          },
-        ]
+        loader: "css-loader?modules",
+        options: {
+          sourceMap: true,
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        },
       },
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({filename: '[name].css?[chunkhash]'}),
-  ],
+      {
+        loader: 'stylus-loader',
+      }
+    ],
+  });
+  defaultConfig.resolve.extensions.push(".styl");
+  
+  return defaultConfig;
 };
