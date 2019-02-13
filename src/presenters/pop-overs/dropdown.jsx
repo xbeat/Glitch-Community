305b7916333ee398707ab5fa7ddb2dfab95f046c
Glitch (add-to-collection-pop) <none>
2019-02-13 18:52:43 +0000

    👶😰 Checkpoint ./src/presenters/pop-overs/dropdown.jsx:867163/7012

--- src/presenters/pop-overs/dropdown.jsx
+++ src/presenters/pop-overs/dropdown.jsx
@@ -2,75 +2,97 @@ import React from "react";
 import PropTypes from "prop-types";
 import PopoverWithButton from "./popover-with-button";
 
-const DropdownMenu = ({contents, selected, updateSelected, togglePopover}) => {
-    
-  return(
+const DropdownMenu = ({
+  contents,
+  handleKeyPress,
+  selected,
+  togglePopover,
+  updateSelected,
+}) => {
+  return (
     /** Note - should have a unique identifier here, in the case that there are multiple dropdowns on a single page*/
     <ul className="pop-over mini-pop" tabIndex="0">
-      { contents.map((item, index) => (
-        <li className={"mini-pop-action" + (index === selected ? " selected" : "")} key={index} 
-          aria-selected={index==selected}
+      {contents.map((item, index) => (
+        <li
+          className={
+            "mini-pop-action" + (index === selected ? " selected" : "")
+          }
+          key={index}
+          aria-selected={index == selected}
           onClick={() => {
             updateSelected(index);
             togglePopover();
           }}
-          onKeyPress={() => {
-            console.log('key press');
-            updateSelected(index);
-            togglePopover();
-          }}
+          onKeyPress={() => {handleKeyPress()}}
           tabIndex="-1"
         >
           {item}
         </li>
       ))}
     </ul>
-  );};
+  );
+};
 
 DropdownMenu.propTypes = {
   contents: PropTypes.node.isRequired,
+  handleKeyPress: PropTypes.func.isRequired,
   selected: PropTypes.number.isRequired, // the index of the selected item
-  updateSelected: PropTypes.func.isRequired,
   togglePopover: PropTypes.func, // added dynamically from PopoverWithButton
+  updateSelected: PropTypes.func.isRequired,
 };
-
 
 class Dropdown extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      selected: 0, 
+    this.state = {
+      selected: 0,
       buttonContents: this.props.buttonContents
     };
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.updateSelected = this.updateSelected.bind(this);
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     // set default menu item here
     // TO DO - set default menu item based on whether we're on a user or team page
   }
 
-  
-  updateSelected(itemIndex){
+  handleKeyPress(e) {
+    const { selected } = this.state;
+    const { menuContents } = this.props.menuContents;
+
+    if (e.keyCode === 38 && selected > 0) {
+      console.log("pressed key down");
+    } else if (e.keyCode === 40 && selected < menuContents.length - 1) {
+      console.log("pressed key up");
+    }
+  }
+
+  updateSelected(itemIndex) {
     this.setState({
       selected: itemIndex,
-      buttonContents: this.props.menuContents[itemIndex],
+      buttonContents: this.props.menuContents[itemIndex]
     });
     // pass selected button back to onUpdate
     this.props.onUpdate(this.props.menuContents[itemIndex]);
   }
-  
-  render(){
-    return(
+
+  render() {
+    return (
       <PopoverWithButton
         buttonClass="button-small dropdown-btn user-or-team-toggle has-emoji"
         buttonText={this.state.buttonContents}
         containerClass="dropdown"
         dropdown={true}
         passToggleToPop
-      > 
-        <DropdownMenu contents={this.props.menuContents} selected={this.state.selected} updateSelected={this.updateSelected}/>
+        onKeyDown={this.handleKeyPress}
+      >
+        <DropdownMenu
+          contents={this.props.menuContents}
+          selected={this.state.selected}
+          updateSelected={this.updateSelected}
+          handleKeyPress={this.handleKeyPress}
+        />
       </PopoverWithButton>
     );
   }
@@ -79,7 +101,7 @@ class Dropdown extends React.Component {
 Dropdown.propTypes = {
   buttonContents: PropTypes.node.isRequired,
   menuContents: PropTypes.node.isRequired,
-  onUpdate: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired
 };
 
 export default Dropdown;
