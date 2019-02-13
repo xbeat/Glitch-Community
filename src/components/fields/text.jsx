603e4bcd9958f6eb9f46a3ff7d92ac46d3a5d0d3
Glitch (profuse-tea) <none>
2019-02-13 20:50:44 +0000

    🎟😿 Checkpoint ./src/components/fields/text.jsx:97325/642 ./src/components/fields/text.styl:97325/398

--- src/components/fields/text.jsx
+++ src/components/fields/text.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './text.styl';
+
 const cx = classNames.bind(styles);
 
-const TextField = ({onChange, placeholder, value}) => {
+const TextField = ({opaque, postfix, prefix, ...props}) => {
+  const className = cx({
+    'text-input': true,
+    'opaque': opaque,
+  });
+  return (
+    <input className={className}/>
+  );
 };
+
+TextField.propTypes = {
+  disabled: PropTypes.bool,
+  onChange: PropTypes.function,
+  opaque: PropTypes.bool,
+  placeholder: PropTypes.string,
+  postfix: PropTypes.node,
+  prefix: PropTypes.node,
+  value: PropTypes.string,
+};
+
+export default TextField;
