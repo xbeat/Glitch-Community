ea41f652a2173281100d4a5a4357d08078229548
Glitch (superb-guide) <none>
2019-02-13 16:25:37 +0000

    🏔🌮 Checkpoint ./webpack.config.js:97325/52

--- webpack.config.js
+++ webpack.config.js
@@ -107,6 +107,7 @@ module.exports = {
     new MiniCssExtractPlugin({filename: '[name].css?[chunkhash]'}),
     new StatsPlugin('stats.json', {all: false, entrypoints: true, hash: true, publicPath: true}),
   ],
+  stats: 'minimal',
   watchOptions: {
     ignored: /node_modules/,
   },
