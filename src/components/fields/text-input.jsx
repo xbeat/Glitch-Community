d6891461d145fd43a72e11375333ca564db201ac
Glitch (profuse-tea) <none>
2019-02-15 21:38:43 +0000

    🛍🍙 Checkpoint ./src/components/fields/text-input.jsx:97325/300

--- src/components/fields/text-input.jsx
+++ src/components/fields/text-input.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
 
 import InputErrorMessage from './input-error-message';
 import InputErrorIcon from './input-error-icon';
 import useUniqueId from './hook-unique-id';
 
 import styles from './text-input.styl';
-const cx = classNames.bind(styles);
 
 const TYPES = [
   'email',
@@ -18,20 +17,17 @@ const TYPES = [
 
 const TextInput = ({className, error, onChange, opaque, postfix, prefix, search, ...props}) => {
   const uniqueId = useUniqueId();
-  const outerClassName = cx('outer', className);
-  const borderClassName = cx({
-    'input-flex': true,
-    underline: !opaque,
-    opaque: opaque,
+  const outerClassName = classNames(styles.outer, className);
+  const borderClassName = classNames(styles.inputBorder, {
+    [styles.underline]: !opaque,
+    [styles.opaque]: opaque,
   });
-  const inputClassName = cx({
-    'input-part': true,
-    input: true,
-    search: search,
+  const inputClassName = classNames(styles.inputPart, styles.input, {
+    [styles.search]: search,
   });
   return (
     <label className={outerClassName} htmlFor={uniqueId}>
-      <div className={flexClassName}>
+      <div className={borderClassName}>
         {!!prefix && <span className={styles.inputPart}>{prefix}</span>}
         <input id={uniqueId} className={inputClassName} onChange={evt => onChange(evt.target.value)} {...props}/>
         {!!error && <InputErrorIcon/>}
