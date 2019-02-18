897902937a3fe29107c5b284c582a9e27cbcb6f0
Glitch (profuse-tea) <none>
2019-02-18 20:25:04 +0000

    👓🍿 Checkpoint ./webpack.config.js:97325/9

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
         include: SRC,
         loader: "eslint-loader",
         options: {
-          fix: mode === 'development', // Only change source files in development
+          fix: false, //mode === 'development', // Only change source files in development
           cache: false, // Keep this off, it can use a lot of space.  Let Webpack --watch do the heavy lifting for us.
           emitError: false,
           emitWarning: true,
