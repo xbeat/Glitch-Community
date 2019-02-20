e48e07d5a0041de77b74342d9da6b2be86e5e6b2
Glitch (lumpy-organ) <none>
2019-02-20 11:58:04 +0000

    🛀🍨 Checkpoint ./server/server.js:252452/128

--- server/server.js
+++ server/server.js
@@ -50,11 +50,11 @@ redirects(app);
 const proxy = require('./proxy');
 const proxied = proxy(app);
 
-const router = require('./routes');
-app.use('/', router(['/edit', ...proxied]));
-
 const logger = require('./logger');
 app.use(logger)
+
+const router = require('./routes');
+app.use('/', router(['/edit', ...proxied]));
 
 // Add an explicit no-cache to 404 responses
 // Since this is the last handler it will only be hit when all other handlers miss
