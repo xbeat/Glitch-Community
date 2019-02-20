dab27804340a82f9d9c749a4e7435788f6da466c
Glitch (lumpy-organ) <none>
2019-02-20 12:09:04 +0000

    🏐🏘 Checkpoint ./server/logger.js:252452/103

--- server/logger.js
+++ server/logger.js
@@ -36,7 +36,8 @@ Response-Time: ${res.responseTime}ms /
 User-Agent: ${req.headers["user-agent"]} / 
 Cache-Control: ${req.headers["cache-control"]}`;
     },
-    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).,
+    ignoredRoute: (req) => { return req.url.match(/\.js|\.css/) }
   }),
 );
 
