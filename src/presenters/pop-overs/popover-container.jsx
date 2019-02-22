fb587ee1da75c36729d27b5807b657ccd64cf436
Glitch (quixotic-rocket) <none>
2019-02-22 21:10:27 +0000

    💈💼 Checkpoint ./src/presenters/pop-overs/popover-container.jsx:97325/4839

--- src/presenters/pop-overs/popover-container.jsx
+++ src/presenters/pop-overs/popover-container.jsx
@@ -16,88 +16,48 @@ Wrapper.propTypes = {
   children: PropTypes.element,
 };
 
-class MonitoredComponent extends React.Component {
+class UnmonitoredComponent extends React.Component {
   handleClickOutside(event) {
     // On keyup events, only hide the popup if it was the Escape key
     if (event.type === 'keyup' && !['Escape', 'Esc'].includes(event.key)) {
       return;
     }
+    this.props.onClickOutside();
+  }
+  render() {
+    return this.props.children;
   }
 }
 
-export default class PopoverContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { visible: props.startOpen };
+const MonitoredComponent = onClickOutside(UnmonitoredComponent, { excludeScrollbar: true });
 
-    this.set = this.set.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-
-    // We need to set up and instantiate an onClickOutside wrapper
-    // It's important to instantiate it once and pass though its children,
-    // otherwise the diff algorithm won't be able to figure out our hijinks.
-    // https://github.com/Pomax/react-onclickoutside
-
-    // We do extra work with disableOnClickOutside and handleClickOutside
-    // to prevent event bindings from being created until the popover is opened.
-    const handleClickOutside = this.handleClickOutside.bind(this);
-    const clickOutsideConfig = {
-      handleClickOutside: () => handleClickOutside,
-      excludeScrollbar: true,
-    };
-    this.MonitoredComponent = onClickOutside(Wrapper, clickOutsideConfig);
-  }
-
-  handleClickOutside(event) {
-    // On keyup events, only hide the popup if it was the Escape key
-    if (event.type === 'keyup' && !['Escape', 'Esc'].includes(event.key)) {
-      return;
-    }
-
-    this.setState({ visible: false });
-  }
-
-  set(visible) {
-    this.setState({ visible });
-  }
-
-  toggle() {
-    this.setState(prevState => ({ visible: !prevState.visible }));
-  }
-
-  // show() {
-  //   this.setState(() => {
-  //     return {visible: true};
-  //   });
-  // }
-
-  render() {
-    const props = {
-      visible: this.state.visible,
-      togglePopover: this.toggle,
-      setVisible: this.set,
-    };
-    const inner = this.props.children(props);
-    if (isFragment(inner)) {
-      console.error(
-        'PopoverContainer does not support Fragment as the top level item. Please use a different element.',
-      );
-    }
-    const outer = this.props.outer ? this.props.outer(props) : null;
-    return (
-      <>
-        {outer}
-        <this.MonitoredComponent
-          disableOnClickOutside={!this.state.visible}
-          eventTypes={['mousedown', 'touchstart', 'keyup']}
-        >
-          {inner}
-        </this.MonitoredComponent>
-      </>
+const PopoverContainer = ({ children, startOpen }) => {
+  const [visible, setVisible] = React.useState(startOpen);
+  const togglePopover = () => setVisible(!visible);
+  
+  const props = { visible, setVisible, togglePopover };
+  
+  const inner = this.props.children(props);
+  if (isFragment(inner)) {
+    console.error(
+      'PopoverContainer does not support Fragment as the top level item. Please use a different element.',
     );
   }
-}
+  const outer = this.props.outer ? this.props.outer(props) : null;
+  
+  return (
+    <>
+      {outer}
+      <MonitoredComponent
+        disableOnClickOutside={!visible}
+        eventTypes={['mousedown', 'touchstart', 'keyup']}
+        onClickOutside={() => setVisible(false)}
+      >
+        {inner}
+      </MonitoredComponent>
+    </>
+  );
+};
 
 PopoverContainer.propTypes = {
   children: PropTypes.func.isRequired,
