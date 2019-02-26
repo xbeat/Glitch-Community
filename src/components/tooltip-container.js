7fb01e6570026ae844e00561558e4625a7b1be21
Glitch (community-staging) <none>
2019-02-26 22:36:21 +0000

    🍶🙇 Checkpoint ./styles/styles.styl:301412/20 ./src/components/tooltip.styl:301412/1662 ./src/components/tooltip-container.js:301412/2794 ./src/presenters/pages/project.js:301412/17570

---
+++ src/components/tooltip-container.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+import styles from "./tooltip.styl";
+
+const cx = classNames.bind(styles);
+
+export const TYPES = ["action", "information"];
+export const ALIGNMENTS = ["left", "right", "center", "top"];
+
+function TooltipContainer({ id, type, tooltip, target, align, persistent }) {
+  const [tooltipIsActive, setTooltipIsActive] = useState(false);
+
+  const tooltipContainerClassName = cx({
+    'tooltip-container': true
+  });
+
+  const tooltipClassName = cx({
+    tooltip: true,
+    top: align.includes("top"),
+    left: align.includes("left"),
+    right: align.includes("right"),
+    persistent
+  });
+
+  let role;
+  let extendedTarget;
+  if (type === "action") {
+    // action tooltips are visible on hover and focus, click triggers a separate action
+    // they should always be populated with their content, even when they are "hidden"
+
+    role = "tooltip";
+    extendedTarget = React.cloneElement(target, {
+      "aria-labelledby": id,
+      onMouseEnter: () => setTooltipIsActive(true),
+      onMouseLeave: () => setTooltipIsActive(false),
+      onFocus: () => setTooltipIsActive(true),
+      onBlur: () => setTooltipIsActive(false)
+    });
+  } else if (type === "information") {
+    // information tooltips are visible on hover and foctus, they provide supplementary info
+    // they should be empty when not "visible", and populated when they are
+
+    role = "status";
+    extendedTarget = React.cloneElement(target, {
+      "aria-describedby": id,
+      onMouseEnter: () => setTooltipIsActive(true),
+      onMouseLeave: () => setTooltipIsActive(false),
+      onFocus: () => setTooltipIsActive(true),
+      onBlur: () => setTooltipIsActive(false)
+    });
+  }
+
+  const shouldShowTooltip =
+    tooltip && (type === "information" || tooltipIsActive || persistent);
+  
+  return (
+    <div className={tooltipContainerClassName}>
+      {extendedTarget}
+      <div
+        role={role}
+        id={id}
+        className={tooltipClassName}
+        style={{ opacity: shouldShowTooltip ? 1 : 0 }}
+      >
+        {shouldShowTooltip ? tooltip : null}
+      </div>
+    </div>
+  );
+}
+
+// TODO multiple duplicate ids
+
+TooltipContainer.propTypes = {
+  /* the id of the tooltip */
+  id: PropTypes.string.isRequired,
+  /* the type of tooltip */
+  type: PropTypes.oneOf(TYPES).isRequired,
+  /* tooltip text */
+  toolip: PropTypes.string,
+  /* the focus/hover target of the tooltip */
+  target: PropTypes.node.isRequired,
+  /* how to align the tooltip */
+  align: PropTypes.arrayOf(PropTypes.oneOf(ALIGNMENTS)),
+  /* whether to persistently show the tooltip */
+  persistent: PropTypes.bool
+};
+
+TooltipContainer.defaultProps = {
+  align: ["center"]
+};
+
+export default TooltipContainer;
--- src/presenters/pages/project.js
+++ src/presenters/pages/project.js
@@ -1,42 +1,43 @@
 /* global analytics */
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Helmet from 'react-helmet';
-import { getAvatarUrl } from '../../models/project';
+import Helmet from "react-helmet";
+import { getAvatarUrl } from "../../models/project";
 
-import { AnalyticsContext } from '../analytics';
-import { DataLoader } from '../includes/loader';
-import NotFound from '../includes/not-found';
-import Markdown from '../includes/markdown';
-import ProjectEditor from '../project-editor';
-import Expander from '../includes/expander';
-import EditableField from '../includes/editable-field';
-import Embed from '../includes/embed';
-import { AuthDescription } from '../includes/description-field';
-import { InfoContainer, ProjectInfoContainer } from '../includes/profile';
+import { AnalyticsContext } from "../analytics";
+import TooltipContainer from "../../components/tooltip-container";
+import { DataLoader } from "../includes/loader";
+import NotFound from "../includes/not-found";
+import Markdown from "../includes/markdown";
+import ProjectEditor from "../project-editor";
+import Expander from "../includes/expander";
+import EditableField from "../includes/editable-field";
+import Embed from "../includes/embed";
+import { AuthDescription } from "../includes/description-field";
+import { InfoContainer, ProjectInfoContainer } from "../includes/profile";
 import {
   ShowButton,
   EditButton,
-  RemixButton,
-} from '../includes/project-actions';
-import ReportButton from '../pop-overs/report-abuse-pop';
-import AddProjectToCollection from '../includes/add-project-to-collection';
-import TeamsList from '../teams-list';
-import UsersList from '../users-list';
-import RelatedProjects from '../includes/related-projects';
-import { addBreadcrumb } from '../../utils/sentry';
+  RemixButton
+} from "../includes/project-actions";
+import ReportButton from "../pop-overs/report-abuse-pop";
+import AddProjectToCollection from "../includes/add-project-to-collection";
+import TeamsList from "../teams-list";
+import UsersList from "../users-list";
+import RelatedProjects from "../includes/related-projects";
+import { addBreadcrumb } from "../../utils/sentry";
 
-import { CurrentUserConsumer } from '../current-user';
+import { CurrentUserConsumer } from "../current-user";
 
-import Layout from '../layout';
+import Layout from "../layout";
 
 function trackRemix(id, domain) {
-  analytics.track('Click Remix', {
-    origin: 'project page',
+  analytics.track("Click Remix", {
+    origin: "project page",
     baseProjectId: id,
-    baseDomain: domain,
+    baseDomain: domain
   });
 }
 
@@ -44,48 +45,56 @@ function syncPageToDomain(domain) {
   history.replaceState(null, null, `/~${domain}`);
 }
 
-const PrivateTooltip = 'Only members can view code';
-const PublicTooltip = 'Visible to everyone';
+const PrivateTooltip = "Only members can view code";
+const PublicTooltip = "Visible to everyone";
 
-const PrivateBadge = () => (
-  <span
-    className="project-badge private-project-badge"
-    aria-label={PrivateTooltip}
-    data-tooltip={PrivateTooltip}
+const PrivateBadge = () => {
+  console.log('privatetooltip', PrivateTooltip)
+  return(
+  <TooltipContainer
+    type="information"
+    id="private-project-badge-tooltip"
+    tooltip={PrivateTooltip}
+    target={<span className="project-badge private-project-badge" />}
   />
-);
+);}
 
 const PrivateToggle = ({ isPrivate, setPrivate }) => {
   const tooltip = isPrivate ? PrivateTooltip : PublicTooltip;
-  const classBase = 'button-tertiary button-on-secondary-background project-badge';
+  const classBase =
+    "button-tertiary button-on-secondary-background project-badge";
   const className = isPrivate
-    ? 'private-project-badge'
-    : 'public-project-badge';
+    ? "private-project-badge"
+    : "public-project-badge";
+
   return (
-    <span data-tooltip={tooltip}>
-      <button
-        aria-label={tooltip}
-        onClick={() => setPrivate(!isPrivate)}
-        className={`${classBase} ${className}`}
-        type="button"
-      />
-    </span>
+    <TooltipContainer
+      type="action"
+      id="toggle-private-button-tooltip"
+      target={
+        <button
+          onClick={() => setPrivate(!isPrivate)}
+          className={`${classBase} ${className}`}
+          type="button"
+        />
+      }
+      tooltip={tooltip}
+    />
   );
 };
 PrivateToggle.propTypes = {
   isPrivate: PropTypes.bool.isRequired,
-  setPrivate: PropTypes.func.isRequired,
+  setPrivate: PropTypes.func.isRequired
 };
 
-const ReadmeError = error => (error && error.response && error.response.status === 404 ? (
-  <>
-      This project would be even better with a
-    {' '}
-    <code>README.md</code>
-  </>
-) : (
-  <>We couldn't load the readme. Try refreshing?</>
-));
+const ReadmeError = error =>
+  error && error.response && error.response.status === 404 ? (
+    <>
+      This project would be even better with a <code>README.md</code>
+    </>
+  ) : (
+    <>We couldn't load the readme. Try refreshing?</>
+  );
 const ReadmeLoader = ({ api, domain }) => (
   <DataLoader
     get={() => api.get(`projects/${domain}/readme`)}
@@ -93,19 +102,17 @@ const ReadmeLoader = ({ api, domain }) => (
   >
     {({ data }) => (
       <Expander height={250}>
-        <Markdown>
-          {data.toString()}
-        </Markdown>
+        <Markdown>{data.toString()}</Markdown>
       </Expander>
     )}
   </DataLoader>
 );
 ReadmeLoader.propTypes = {
   api: PropTypes.any,
-  domain: PropTypes.string.isRequired,
+  domain: PropTypes.string.isRequired
 };
 ReadmeLoader.defaultProps = {
-  api: null,
+  api: null
 };
 
 const ProjectPage = ({
@@ -116,7 +123,7 @@ const ProjectPage = ({
   isAuthorized,
   updateDomain,
   updateDescription,
-  updatePrivate,
+  updatePrivate
 }) => {
   const { domain, users, teams } = project;
   return (
@@ -131,21 +138,22 @@ const ProjectPage = ({
                 <EditableField
                   value={domain}
                   placeholder="Name your project"
-                  update={newDomain => updateDomain(newDomain).then(() => syncPageToDomain(newDomain))
+                  update={newDomain =>
+                    updateDomain(newDomain).then(() =>
+                      syncPageToDomain(newDomain)
+                    )
                   }
-                  suffix={(
+                  suffix={
                     <PrivateToggle
                       isPrivate={project.private}
                       isMember={isAuthorized}
                       setPrivate={updatePrivate}
                     />
-                  )}
+                  }
                 />
               ) : (
                 <>
-                  {domain}
-                  {' '}
-                  {project.private && <PrivateBadge />}
+                  {domain} {project.private && <PrivateBadge />}
                 </>
               )}
             </h1>
@@ -206,68 +214,65 @@ ProjectPage.propTypes = {
   api: PropTypes.any,
   currentUser: PropTypes.object.isRequired,
   isAuthorized: PropTypes.bool.isRequired,
-  project: PropTypes.object.isRequired,
+  project: PropTypes.object.isRequired
 };
 
 ProjectPage.defaultProps = {
-  api: null,
+  api: null
 };
 
 async function getProject(api, domain) {
   const { data } = await api.get(`projects/${domain}`);
   addBreadcrumb({
-    level: 'info',
-    message: `project: ${JSON.stringify(data)}`,
+    level: "info",
+    message: `project: ${JSON.stringify(data)}`
   });
   return data;
 }
 
-const ProjectPageLoader = ({
-  domain, api, currentUser, ...props
-}) => (
+const ProjectPageLoader = ({ domain, api, currentUser, ...props }) => (
   <DataLoader
     get={() => getProject(api, domain)}
     renderError={() => <NotFound name={domain} />}
   >
-    {project => (project ? (
-      <ProjectEditor api={api} initialProject={project}>
-        {(currentProject, funcs, userIsMember) => (
-          <>
-            <Helmet>
-              <title>
-                {currentProject.domain}
-              </title>
-            </Helmet>
-            <ProjectPage
-              api={api}
-              project={currentProject}
-              {...funcs}
-              isAuthorized={userIsMember}
-              currentUser={currentUser}
-              {...props}
-            />
-          </>
-        )}
-      </ProjectEditor>
-    ) : (
-      <NotFound name={domain} />
-    ))
+    {project =>
+      project ? (
+        <ProjectEditor api={api} initialProject={project}>
+          {(currentProject, funcs, userIsMember) => (
+            <>
+              <Helmet>
+                <title>{currentProject.domain}</title>
+              </Helmet>
+              <ProjectPage
+                api={api}
+                project={currentProject}
+                {...funcs}
+                isAuthorized={userIsMember}
+                currentUser={currentUser}
+                {...props}
+              />
+            </>
+          )}
+        </ProjectEditor>
+      ) : (
+        <NotFound name={domain} />
+      )
     }
   </DataLoader>
 );
 ProjectPageLoader.propTypes = {
   api: PropTypes.func,
   domain: PropTypes.string.isRequired,
-  currentUser: PropTypes.object.isRequired,
+  currentUser: PropTypes.object.isRequired
 };
 
 ProjectPageLoader.defaultProps = {
-  api: null,
+  api: null
 };
 
 const ProjectPageContainer = ({ api, name }) => (
   <Layout api={api}>
-    <AnalyticsContext properties={{ origin: 'project' }}>
+    <AnalyticsContext properties={{ origin: "project" }}>
       <CurrentUserConsumer>
         {currentUser => (
           <ProjectPageLoader
