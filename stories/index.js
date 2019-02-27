5439c073414f728caf3ee70d471bffc83411f6e2
Glitch (community-staging) <none>
2019-02-27 00:08:34 +0000

    😜💣 Checkpoint ./stories/index.js:301412/7715

--- stories/index.js
+++ stories/index.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Button from '../src/components/buttons/button';
-import TooltipContainer from '../src/components/tooltip-container';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "../src/components/buttons/button";
+import TooltipContainer from "../src/components/tooltip-container";
 
-storiesOf('Button', module)
-  .add('regular', () => (
-    <Button>Hello Button</Button>
+storiesOf("Button", module)
+  .add("regular", () => <Button>Hello Button</Button>)
+  .add("cta", () => <Button type="cta">CTA Button</Button>)
+  .add("small", () => <Button size="small">Small Button</Button>)
+  .add("tertiary", () => (
+    <Button type="tertiary" size="small">
+      Tertiary (Small) Button
+    </Button>
   ))
-  .add('cta', () => (
-    <Button type="cta">CTA Button</Button>
-  ))
-  .add('small', () => (
-    <Button size="small">Small Button</Button>
-  ))
-.add('tertiary', () => (
-    <Button type="tertiary" size="small">Tertiary (Small) Button</Button>
-  ))
-.add('danger zone', () => (
-    <Button type="dangerZone" size="small">Destructive Action</Button>
+  .add("danger zone", () => (
+    <Button type="dangerZone" size="small">
+      Destructive Action
+    </Button>
   ));
 
-storiesOf('TooltipContainer', module)
-  .add('base', () => (
-    <TooltipContainer type="action" id="a-unique-id" target={<Button>Hover or focus me</Button>}> tooltip="I'm a tooltip"></TooltipContainer>
+storiesOf("TooltipContainer", module)
+  .add("action", () => (
+    <TooltipContainer
+      type="action"
+      id="a-unique-id"
+      target={<Button>Hover or focus me</Button>}
+      tooltip="I'm an action tooltip"
+    />
   ))
+  .add("information", () => (
+    <TooltipContainer
+      type="information"
+      id="a-unique-id"
+      target={
+        <img src="https://favicon-fetcher.glitch.me/img/glitch.com" />
+      }
+      tooltip="I'm an information tooltip"
+    />
+  ))
+  .add("persistent", () => (
+    <TooltipContainer
+      type="information"
+      id="a-unique-id"
+      target={
+        <img src="https://favicon-fetcher.glitch.me/img/glitch.com" />
+      }
+      tooltip="I'm a persistent tooltip"
+      persistent
+    />
+  ))
+  .add("left and top aligned", () => (
+    <TooltipContainer
+      type="action"
+      id="a-unique-id"
+      target={<Button>Hover or focus me</Button>}
+      tooltip="I'm a tooltip"
+      align={["top", "left"]}
+    />
+  ))
+  .add("with children", () => (
+    <TooltipContainer
+      type="action"
+      id="a-unique-id"
+      target={<Button>Hover or focus me</Button>}
+      tooltip="I'm a tooltip"
+    >
+      <p>I'm the <code>props.children</code></p>
+  </TooltipContainer>
+  ));
