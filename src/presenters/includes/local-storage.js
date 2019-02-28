a75cc12a980bc7eb281fd6cdcda90024bc93a646
Glitch (superb-guide) <none>
2019-02-28 20:24:04 +0000

    🏦🐟 Checkpoint ./src/presenters/includes/local-storage.js:97325/1088

--- src/presenters/includes/local-storage.js
+++ src/presenters/includes/local-storage.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import { captureException } from '../../utils/sentry';
 
-const readFromStorage = (storage, name) => {
+const getStorage = () => {
+  try {
+    const storage = window.localStorage;
+    storage.setItem('test', 'test');
+    storage.getItem('test');
+    storage.removeItem('test');
+    return storage;
+  } catch (error) {
+    console.warn('Local storage not available, using memory store');
+  }
+  const data = new Map();
+  const getItem = (key) => data.get(key);
+  const setItem = (key, value) => data.set(key, value);
+  const removeItem = (key) => data.delete(key);
+  return { getItem, setItem, removeItem };
+};
+const storage = getStorage();
+
+const readFromStorage = (name) => {
   try {
     const raw = storage.getItem(name);
     if (raw !== null) {
       return JSON.parse(raw);
     }
   } catch (error) {
-    console.warn('Failed to read from localStorage!', error);
+    captureException(error);
   }
   return undefined;
 };
 
-const writeToStorage = (storage, name, value) => {
+const writeToStorage = (name, value) => {
   try {
     if (value !== undefined) {
       storage.setItem(name, JSON.stringify(value));
@@ -25,26 +44,24 @@ const writeToStorage = (storage, name, value) => {
 };
 
 const useLocalStorage = (name, defaultValue) => {
-  const storage = window.localStorage;
-
-  const [rawValue, setValueInMemory] = React.useState(() => readFromStorage(storage, name));
+  const [rawValue, setValueInMemory] = React.useState(() => readFromStorage(name));
 
   React.useEffect(() => {
     const reload = (event) => {
       if (event.storageArea === storage && event.key === name) {
-        setValueInMemory(readFromStorage(storage, name));
+        setValueInMemory(readFromStorage(name));
       }
     };
     window.addEventListener('storage', reload, { passive: true });
     return () => {
       window.removeEventListener('storage', reload, { passive: true });
     };
-  }, [storage, name]);
+  }, [name]);
 
   const value = rawValue !== undefined ? rawValue : defaultValue;
   const setValue = (newValue) => {
     setValueInMemory(newValue);
-    writeToStorage(storage, name, newValue);
+    writeToStorage(name, newValue);
   };
 
   return [value, setValue];
