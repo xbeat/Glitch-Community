78a6f112a0dabfe33177241770076604f9f7732f
Glitch (community-staging) <none>
2019-03-01 19:14:09 +0000

    💅💉 Checkpoint ./src/presenters/includes/avatar.js:301412/57 ./src/components/tooltip.styl:301412/360

--- src/presenters/includes/avatar.js
+++ src/presenters/includes/avatar.js
@@ -43,7 +43,7 @@ export const Avatar = ({
         target={contents}
         type="action"
         id={"avatar-tooltip-" + name}
-        align={["top", "left"]}
+        align={["left"]}
       />
     );
   }
