be00dc8d03f310737345df01251d461bf4ac563c
Tara Vancil <tara@Taras-MacBook-Pro.local>
2019-03-04 17:06:58 -0500

    Fix merge conflict

--- stories/index.js
+++ stories/index.js
@@ -1,38 +1,33 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Button from "../src/components/buttons/button";
-import TooltipContainer from "../src/components/tooltips/tooltip-container";
-import TextInput from "../src/components/fields/text-input";
-import TextArea from "../src/components/fields/text-area";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Button from '../src/components/buttons/button';
+import TooltipContainer from '../src/components/tooltips/tooltip-container';
+import TextInput from '../src/components/fields/text-input';
+import TextArea from '../src/components/fields/text-area';
 
-storiesOf("Button", module)
-  .add("regular", () => <Button>Hello Button</Button>)
-  .add("cta", () => <Button type="cta">CTA Button</Button>)
-  .add("small", () => <Button size="small">Small Button</Button>)
-  .add("tertiary", () => (
+storiesOf('Button', module)
+  .add('regular', () => <Button>Hello Button</Button>)
+  .add('cta', () => <Button type="cta">CTA Button</Button>)
+  .add('small', () => <Button size="small">Small Button</Button>)
+  .add('tertiary', () => (
     <Button type="tertiary" size="small">
       Tertiary (Small) Button
     </Button>
   ))
-  .add("danger zone", () => (
+  .add('danger zone', () => (
     <Button type="dangerZone" size="small">
       Destructive Action
     </Button>
   ));
 
-storiesOf("TooltipContainer", module)
-  .add("action", () => (
-    <div style={{margin: '70px'}}>
-      <TooltipContainer
-        type="action"
-        id="a-unique-id"
-        target={<Button>Hover or focus me</Button>}
-        tooltip="I'm an action tooltip"
-      />
+storiesOf('TooltipContainer', module)
+  .add('action', () => (
+    <div style={{ margin: '70px' }}>
+      <TooltipContainer type="action" id="a-unique-id" target={<Button>Hover or focus me</Button>} tooltip="I'm an action tooltip" />
     </div>
   ))
-  .add("info", () => (
-    <div style={{margin: '70px'}}>
+  .add('info', () => (
+    <div style={{ margin: '70px' }}>
       <TooltipContainer
         type="info"
         id="a-unique-id"
@@ -41,8 +36,8 @@ storiesOf("TooltipContainer", module)
       />
     </div>
   ))
-  .add("persistent", () => (
-    <div style={{margin: '70px'}}>
+  .add('persistent', () => (
+    <div style={{ margin: '70px' }}>
       <TooltipContainer
         type="info"
         id="a-unique-id"
@@ -52,15 +47,9 @@ storiesOf("TooltipContainer", module)
       />
     </div>
   ))
-  .add("left and top aligned", () => (
-    <div style={{margin: '70px'}}>
-      <TooltipContainer
-        type="action"
-        id="a-unique-id"
-        target={<Button>Hover or focus me</Button>}
-        tooltip="I'm a tooltip"
-        align={["top", "left"]}
-      />
+  .add('left and top aligned', () => (
+    <div style={{ margin: '70px' }}>
+      <TooltipContainer type="action" id="a-unique-id" target={<Button>Hover or focus me</Button>} tooltip="I'm a tooltip" align={['top', 'left']} />
     </div>
   ));
 
