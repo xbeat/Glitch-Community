f3d8bae3191d8aa8397e7b1340372ed1e14c3c19
Glitch (lumpy-organ) <none>
2019-03-04 22:20:36 +0000

    🌇🚘 Checkpoint ./src/presenters/includes/team-elements.js:252452/9866

--- src/presenters/includes/team-elements.js
+++ src/presenters/includes/team-elements.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { debounce } from 'lodash';
-import { Link } from './link';
+import { debounce } from "lodash";
+import { Link } from "./link";
 
 export const TeamMarketing = () => {
-  const forPlatformsIcon = 'https://cdn.glitch.com/be1ad2d2-68ab-404a-82f4-6d8e98d28d93%2Ffor-platforms-icon.svg?1506442305188';
+  const forPlatformsIcon = "https://cdn.glitch.com/be1ad2d2-68ab-404a-82f4-6d8e98d28d93%2Ffor-platforms-icon.svg?1506442305188";
   return (
     <section className="team-marketing">
       <p>
-        <img
-          className="for-platforms-icon"
-          src={forPlatformsIcon}
-          alt="fishing emoji"
-        />
+        <img className="for-platforms-icon" src={forPlatformsIcon} alt="fishing emoji" />
         Want your own team page, complete with detailed app analytics?
       </p>
       <Link to="/teams" className="button button-link has-emoji">
-        About Teams
-        {' '}
-        <span className="emoji fishing_pole" role="img" aria-label="emoji" />
+        About Teams <span className="emoji fishing_pole" role="img" aria-label="emoji" />
       </Link>
     </section>
   );
 };
 
 export const VerifiedBadge = () => {
-  const image = 'https://cdn.glitch.com/55f8497b-3334-43ca-851e-6c9780082244%2Fverified.svg?1501783108220';
-  const tooltip = 'Verified to be supportive, helpful people';
+  const image = "https://cdn.glitch.com/55f8497b-3334-43ca-851e-6c9780082244%2Fverified.svg?1501783108220";
+  const tooltip = "Verified to be supportive, helpful people";
   return (
     <span data-tooltip={tooltip}>
       <img className="verified" src={image} alt="✓" />
@@ -67,14 +61,7 @@ export class WhitelistedDomainIcon extends React.Component {
   render() {
     const { domain } = this.props;
     if (this.state.src) {
-      return (
-        <img
-          className="whitelisted-domain"
-          alt={domain}
-          src={this.state.src}
-          onError={() => this.setState({ src: null })}
-        />
-      );
+      return <img className="whitelisted-domain" alt={domain} src={this.state.src} onError={() => this.setState({ src: null })} />;
     }
     return (
       <div className="whitelisted-domain" aria-label={domain}>
