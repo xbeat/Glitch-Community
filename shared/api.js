e629289e1261e5171071b32ff9612d059f0960b5
Glitch (superb-guide) <none>
2019-03-06 16:52:23 +0000

    🛋🌪 Checkpoint ./shared/api.js:97325/596

--- shared/api.js
+++ shared/api.js
@@ -1,15 +1,26 @@
 /// Api helper functions
 
-async function getFromApi(api, url) {
+const getFromApi = async (api, url) => {
   const { data } = await api.get(url);
   return data;
+}
+
+const getSingleItem = async (api, url, key) => {
+  // The api is case insensitive when getting by url/login
+  // but it'll return an object keyed using the correct case
+  // for now do an icky hack so we can get the team page
+  const data = getFromApi(api, url);
+  if (data[key]) {
+    return data[key];
+  }
+  const realKey = 
 }
 
 const getAllPages = async (api, url) => {
   let hasMore = true;
   let results = [];
   while (hasMore) {
-    const { data } = await api.get(url);
+    const data = await getFromApi(api, url);
     results.push(...data.items);
     hasMore = data.hasMore;
     url = data.nextPage;
