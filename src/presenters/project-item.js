126b3e5f6b05547ab45ba34d045f5d223ad9cc10
Glitch (iced-flag) <none>
2019-03-11 17:34:34 +0000

    🚜🌦 Checkpoint ./src/presenters/project-item.js:433767/1508

--- src/presenters/project-item.js
+++ src/presenters/project-item.js
@@ -18,7 +18,31 @@ const ProjectItem = ({
       project={project}
       update={note => props.projectOptions.updateOrAddNote({ note, projectId: project.id })}
     />
-    <Note currentUser={{ id: 433767, login: 'sarahzinger' }} update={something => console.log(something)} project={{ isAddingANewNote: true }} />
+    <Note currentUser={{ 
+        avatarThumbnailUrl: "https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/user-avatar/3864a79d-937d-4f61-83e6-fec22f484269-small.png"
+          avatarUrl: "https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/user-avatar/3864a79d-937d-4f61-83e6-fec22f484269-large.png"
+color: "#9cf989"
+
+facebookId: null
+facebookToken: null
+featuredProjectId: null
+features: []
+gitAccessToken: "62cc115a-de7f-4444-a6e1-49e856721fe7"
+githubId: 6620164
+githubToken: "f44cb020335657f56deaf00bf507abb0951b303e"
+hasCoverImage: false
+id: 433767
+lastActiveDay: "2019-03-11"
+location: "Brooklyn, NY"
+login: "sarahzinger"
+name: "Sarah Zinger"
+persistentToken: "319d4e97-f385-4fc3-8607-0934251477d5"
+pins: [{…}]
+projects: (39) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+teams: (2) [{…}, {…}]
+thanksCount: 0
+updatedAt: "2019-03-11T13:58:14.577Z"
+utcOffset: -240 }} update={something => console.log(something)} project={{ isAddingANewNote: true }} />
 
     <UsersList
       glitchTeam={project.showAsGlitchTeam}
