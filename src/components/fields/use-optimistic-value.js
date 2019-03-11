53abd4525221e021302c802ab8f04eab6df3dc3b
Glitch (superb-guide) <none>
2019-03-11 19:27:44 +0000

    👳🌅 Checkpoint ./src/components/fields/use-optimistic-value.js:97325/575

--- src/components/fields/use-optimistic-value.js
+++ src/components/fields/use-optimistic-value.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { debounce } from 'lodash';
 
 const useOptimisticValue = (realValue, onChangeAsync) => {
+  const [stateValue, setStateValue] = React.useState(undefined);
   
+  // use a ref so we can get the current value in async functions
+  const valueRef = React.useRef(stateValue);
+  React.useEffect(() => {
+    valueRef.current = stateValue;
+  }, [stateValue]);
+  
+  const onChange = async (newValue) => {
+    setStateValue(newValue);
+    try {
+      await onChangeAsync(newValue);
+    } catch (error) {
+    }
+  };
 };
+
+export default useOptimisticValue;
