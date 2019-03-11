a8649c655e015342718761fec04ac2a50ff42714
Glitch (superb-guide) <none>
2019-03-11 21:23:14 +0000

    🎇🌲 Checkpoint ./src/components/fields/use-optimistic-value.js:97325/1122

--- src/components/fields/use-optimistic-value.js
+++ src/components/fields/use-optimistic-value.js
@@ -4,34 +4,28 @@ import { debounce } from 'lodash';
 const useOptimisticValue = (realValue, setValueAsync) => {
   const [stateValue, setStateValue] = React.useState({ value: undefined, error: null });
   
-  // use a ref so we can get the current value in async functions
-  const valueRef = React.useRef(stateValue);
-  React.useEffect(() => {
-    valueRef.current = stateValue;
-  }, [stateValue]);
+  const setStateIfMatches = (newState, valueToMatch) => {
+    setStateValue(prevState => {
+      if (prevState.value === valueToMatch) {
+        return newState;
+      }
+      return prevState;
+    });
+  };
   
   const setValue = async (newValue) => {
     try {
       await setValueAsync(newValue);
-      setStateValue(prevState => {
-        if (prevState.value === newValue) {
-          return { value: undefined, error: null };
-        }
-        return prevState;
-      });
+      setStateIfMatches({ value: undefined, error: null }, newValue);
     } catch (error) {
-      setStateValue(prevState => {
-        if (prevState.value === newValue) {
-          return { value: newValue, error };
-        }
-        return prevState;
-      });
+      setStateIfMatches({ value: newValue, error }, newValue);
     }
   };
   
   const optimisticValue = stateValue === undefined ? realValue : stateValue;
   const setOptimisticValue = (newValue) => {
     setStateValue({ value: newValue, error: null });
+    
   };
   
   return [optimisticValue, stateValue.error, setOptimisticValue];
