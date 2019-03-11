1f2e1a6cb5f10337b0cace147a703de89b85f5b8
Glitch (superb-guide) <none>
2019-03-11 22:06:24 +0000

    📻😼 Checkpoint ./src/components/fields/use-optimistic-value.js:97325/820

--- src/components/fields/use-optimistic-value.js
+++ src/components/fields/use-optimistic-value.js
@@ -3,16 +3,20 @@ import React from 'react';
 import useDebouncedValue from './use-debounced-value';
 
 const useOptimisticValue = (realValue, setValueAsync) => {
-  // store 
+  // store what is being typed in, along with an error message
+  // value undefined means that the field is unchanged from the 'real' value
   const [state, setState] = React.useState({ value: undefined, error: null });
   
-  // debounce our stored state and send the async updates when it changes
+  // debounce our stored value and send the async updates when it is not undefined
   const debouncedValue = useDebouncedValue(state.value, 500);
   React.useEffect(() => {
     if (debouncedValue !== undefined) {
+      // they might have typed something else during the async action
+      // if the value changed then we'll get a new debounced effect in time
       const setStateIfMatches = (newState) => {
         setState(prevState => prevState.value === debouncedValue ? newState : prevState);
       };
+      // this scope can't be async/await because it's an effect
       setValueAsync(debouncedValue).then(
         () => setStateIfMatches({ value: undefined, error: null }),
         error => setStateIfMatches({ value: debouncedValue, error }),
@@ -20,7 +24,7 @@ const useOptimisticValue = (realValue, setValueAsync) => {
     }
   }, [debouncedValue]);
   
-  // parse out the display value and how to change it, and send them along
+  // replace undefined with the real value
   const optimisticValue = state.value !== undefined ? state.value : realValue;
   const setOptimisticValue = (newValue) => {
     setState(prevState => ({ ...prevState, value: newValue }));
