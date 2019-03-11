2cfdbf0656229e1a33603243fcfeceb5dcd0457b
Glitch (community) <none>
2019-03-11 13:38:59 +0000

    add pinned projects

--- src/presenters/pages/user.js
+++ src/presenters/pages/user.js
@@ -93,9 +93,10 @@ const UserPage = ({
   setDeletedProjects,
   addProjectToCollection,
 }) => {
-  const pinnedSet = new Set(user.pins.map(({ projectId }) => projectId));
+  const pinnedSet = new Set(user.pins.map(({ id }) => id));
   // filter featuredProject out of both pinned & recent projects
   const [pinnedProjects, recentProjects] = partition(user.projects.filter(({ id }) => id !== featuredProjectId), ({ id }) => pinnedSet.has(id));
+  
   const featuredProject = user.projects.find(({ id }) => id === featuredProjectId);
 
   return (
