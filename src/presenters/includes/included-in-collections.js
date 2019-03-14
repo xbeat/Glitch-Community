3845c58034b90649cf9e0204c91fc4548143c723
Glitch (community) <none>
2019-03-14 16:42:36 +0000

    🍐🎄 Checkpoint ./src/presenters/pages/project.js:271885/600 ./src/presenters/includes/related-projects.js:271885/236 ./src/curated/collections.js:271885/1 ./src/presenters/includes/included-in-collections.js:271885/647

---
+++ src/presenters/includes/included-in-collections.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IncludedInCollections = () => {
+  console.log('included');
+  return (
+    <>
+      <h2>Included in Collections</h2>
+    </>
+  );
+};
+
+export default IncludedInCollections;
