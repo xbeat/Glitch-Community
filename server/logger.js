9632bbcb3427c1a4d9cdbdd5fb87b322a8a9ec3a
Glitch (lumpy-organ) <none>
2019-03-14 19:01:34 +0000

    🛣🐸 Checkpoint ./server/logger.js:252452/2849

--- server/logger.js
+++ server/logger.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const winston = require("winston");
-const expressWinston = require("express-winston");
+const express = require('express');
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 const app = express.Router();
 
@@ -16,22 +16,22 @@ app.use(
   expressWinston.logger({
     transports: [
       new winston.transports.File({
-        name: "requests",
-        filename: ".log/requests.log",
-        maxsize: 100 * 1024, // max size of each log file in bytes
+        name: 'requests',
+        filename: '.data/log/requests.log',
+        maxsize: 1000 * 1024, // max size of each log file in bytes
         maxFiles: 1, // overflow will go into requests1.log once the max size of requests.log is reached
         tailable: true, // the oldest data will get removed from requests.log so that you can always tail it for the latest logs
       }),
     ],
-    format: winston.format.combine(
-      winston.format.json(),
-    ),
+    format: winston.format.combine(winston.format.json()),
     meta: false, // logs meta data about the request if true
     msg: (req, res) => {
-      return `${req.requestTime}: HTTP ${req.method} ${req.url} ${res.statusCode} / Response-Time: ${res.responseTime}ms / User-Agent: ${req.headers["user-agent"]} / Cache-Control: ${req.headers["cache-control"]}`;
+      return `${req.requestTime}: HTTP ${req.method} ${req.url} ${res.statusCode} / Response-Time: ${res.responseTime}ms`;
     },
     colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).,
-    ignoreRoute: (req) => { return req.url.match(/\.js|\.css/) !== null }
+    ignoreRoute: (req) => {
+      return req.url.match(/\.js|\.css/) !== null;
+    },
   }),
 );
 
