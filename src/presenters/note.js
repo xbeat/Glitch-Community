aebaa95700ba215ad067514aa647d3147071bff5
Glitch (iced-flag) <none>
2019-03-14 20:39:03 +0000

    🌷🏄 Checkpoint ./src/presenters/includes/team-analytics-activity.js:433767/6063 ./src/presenters/includes/team-analytics.js:433767/13794 ./styles/projects.styl:433767/262 ./src/presenters/note.js:433767/1629 ./src/components/fields/note.styl:433767/94

---
+++ src/presenters/note.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+// TODO: let's move these into components
+import { EditableDescription } from '../../presenters/includes/description-field';
+import { UserTile } from '../../presenters/users-list';
+
+import { isDarkColor } from '../../models/collection';
+
+/**
+ * Note Component
+ */
+const Note = ({
+  collectionCoverColor, currentUser, project, update,
+}) => {
+  if (!project.isAddingANewNote && !project.note) {
+    return null;
+  }
+
+  const className = classNames({
+    descriptionContainer: true,
+    dark: isDarkColor(collectionCoverColor),
+  });
+
+  return (
+    <div>
+      <div className={className} style={{ backgroundColor: collectionCoverColor, borderColor: collectionCoverColor }}>
+        <EditableDescription
+          description={project.note || ''}
+          placeholder="Share why you love this app."
+          update={update}
+          maxLength={75}
+        />
+      </div>
+      <div className="user">
+        <UserTile user={currentUser} />
+      </div>
+    </div>
+  );
+};
+
+
+Note.propTypes = {
+  collectionCoverColor: PropTypes.string.isRequired,
+  currentUser: PropTypes.object,
+  project: PropTypes.shape({
+    note: PropTypes.string,
+    isAddingANewNote: PropTypes.bool,
+    collectionCoverColor: PropTypes.string,
+  }).isRequired,
+  update: PropTypes.func,
+};
+
+Note.defaultProps = {
+  currentUser: null,
+  update: null,
+};
+
+export default Note;
