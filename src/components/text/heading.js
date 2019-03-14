bf34fc04beadf5176fc11f5bdbd8b60913eb2d8b
Glitch (kiwi-poinsettia) <none>
2019-03-14 08:26:22 +0000

    🍖💺 Checkpoint ./src/components/text/heading.styl:355867/113 ./src/components/text/heading.js:355867/965

--- src/components/text/heading.js
+++ src/components/text/heading.js
@@ -1,53 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
 import styles from './heading.styl';
-
-const cx = classNames.bind(styles);
-
-const TAGS_AND_SIZES = {
-  h1: 'xlarge',
-  h2: 'large',
-  h3: 'medium',
-  h4: 'small',
-};
 
 /**
  * Heading Component
  */
-const Heading = ({ children, tagName: TagName, className }) => {
-  // In the future we might want tag names and sizes to be different
-
-  // For now, we're keeping them 1-1,
-  // and also specifically setting 'h1' and 'h2' headings to be bold
-  // which should match the current styles on production for switching to this
-
-  const size = TAGS_AND_SIZES[TagName];
-  const bold = TagName === 'h1' || TagName === 'h2';
-  return (
-    <TagName
-      className={cx({
-        [size]: true,
-        bold,
-        [className]: className !== undefined,
-      })}
-    >
-      {children}
-    </TagName>
-  );
-};
+const Heading = ({ children, tagName: TagName }) => <TagName>{children}</TagName>;
 
 Heading.propTypes = {
   /** element(s) to display in the button */
   children: PropTypes.node.isRequired,
   /** heading tag to be rendered [h1, h2...] */
-  tagName: PropTypes.oneOf(Object.keys(TAGS_AND_SIZES)).isRequired,
-  /** additional className to add to the heading */
-  className: PropTypes.string,
-};
-
-Heading.defaultProps = {
-  className: undefined,
+  tagName: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4']).isRequired,
 };
 
 export default Heading;
