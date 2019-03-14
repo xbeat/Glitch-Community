ab45ce240a8a6ce3c3e7c538122260bb3b1af5dd
Glitch (community) <none>
2019-03-14 19:10:37 +0000

    📕🚣 Checkpoint ./src/presenters/includes/included-in-collections.js:271885/6402 ./src/presenters/collection-item.js:271885/17901

--- src/presenters/includes/included-in-collections.js
+++ src/presenters/includes/included-in-collections.js
@@ -1,27 +1,28 @@
-import React, { createRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getSingleItem, getAllPages, allByKeys } from '../../../shared/api';
 
 import CollectionItem from '../collection-item';
 
 const getIncludedCollections = async (api, projectId) => {
   const collections = await getAllPages(api, `/v1/projects/by/id/collections?id=${projectId}&limit=100&orderKey=createdAt&orderDirection=DESC`);
-  return await Promise.all(
+  return Promise.all(
     collections.map(async (collection) => {
-      const { projects, user } = await allByKeys({
+      const { projects, user, team } = await allByKeys({
         projects: getAllPages(api, `/v1/collections/by/id/projects?id=${collection.id}&limit=100&orderKey=createdAt&orderDirection=DESC`),
         user: collection.user && getSingleItem(api, `v1/users/by/id?id=${collection.user.id}`, collection.user.id),
+        team: collection.team && getSingleItem(api, `v1/teams/by/id?id=${collection.team.id}`, collection.team.id),
       });
-      return { ...collection, projects, user };
+      return { ...collection, projects, user, team };
     }),
   );
 };
 
 const useAsync = (cb, ...args) => {
-  const [result, setResult] = useState(null)
+  const [result, setResult] = useState(null);
   useEffect(() => {
     cb(...args).then(setResult);
   }, args);
-  return result
+  return result;
 };
 
 const IncludedInCollections = ({ api, projectId }) => {
@@ -33,11 +34,8 @@ const IncludedInCollections = ({ api, projectId }) => {
     <div className="collections">
       <h2>Included in Collections</h2>
       <ul className="collections-container">
-        {collections.map(collection => (
-          <CollectionItem
-            key={collection.id}
-            collection={collection}
-          />
+        {collections.map((collection) => (
+          <CollectionItem showUser key={collection.id} collection={collection} />
         ))}
       </ul>
     </div>
