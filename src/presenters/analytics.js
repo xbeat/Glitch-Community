73a74b65c13142b811779199b1a9885d71a7c6f3
Glitch (profuse-tea) <none>
2019-03-14 20:17:37 +0000

    🏛🍔 Checkpoint ./src/presenters/analytics.js:97325/4370

--- src/presenters/analytics.js
+++ src/presenters/analytics.js
@@ -42,97 +42,83 @@ AnalyticsContext.defaultProps = {
 };
 
 // this gives you a generic track function that pulls in inherited properties
-const useAnalytics = () => {
+const useAnalyticsTracker = () => {
   const inherited = React.useContext(Context);
   return (name, properties, context) => {
-        try {
-          analytics.track(name, resolveProperties(properties, inherited.properties), resolveProperties(context, inherited.context));
-        } catch (error) {
-          /* From Segment: "We currently return a 200 response for all API requests so debugging should be done in the Segment Debugger.
-          The only exception is if the request is too large / json is invalid it will respond with a 400."
-          If it was not a 400, it wasn't our fault so don't track it.
-        */
-          if (error && error.response && error.response.status === 400) {
-            captureException(error);
-          }
-        }
-      })
+    try {
+      analytics.track(name, resolveProperties(properties, inherited.properties), resolveProperties(context, inherited.context));
+    } catch (error) {
+      /* From Segment: "We currently return a 200 response for all API requests so debugging should be done in the Segment Debugger.
+      The only exception is if the request is too large / json is invalid it will respond with a 400."
+      If it was not a 400, it wasn't our fault so don't track it.
+      */
+      if (error && error.response && error.response.status === 400) {
+        captureException(error);
+      }
     }
-  </Context.Consumer>
-);
+  };
+};
 
 // this is the equivalent of doing <AnalyticsTracker>{track => <asdf onClick={() => track('asdf')}/></AnalyticsTracker>
 // this won't work for links that do a full page load, because the request will get cancelled by the nav
 // use the TrackedExternalLink for that, because it will stall the page for a moment and let the request finish
-export const TrackClick = ({ children, name, properties, context }) => (
-  <AnalyticsTracker>
-    {(track) =>
-      React.Children.map(children, (child) => {
-        function onClick(...args) {
-          track(name, properties, context);
-          if (child.props.onClick) {
-            return child.props.onClick(...args);
-          }
-          return null;
-        }
-        return React.cloneElement(child, { onClick });
-      })
+export const TrackClick = ({ children, name, properties, context }) => {
+  const track = useAnalyticsTracker();
+  return React.Children.map(children, (child) => {
+    function onClick(...args) {
+      track(name, properties, context);
+      if (child.props.onClick) {
+        return child.props.onClick(...args);
+      }
+      return null;
     }
-  </AnalyticsTracker>
-);
+    return React.cloneElement(child, { onClick });
+  });
+};
 TrackClick.propTypes = {
   children: PropTypes.node.isRequired,
   name: PropTypes.string.isRequired,
   properties: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   context: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
-
 TrackClick.defaultProps = {
   properties: {},
   context: {},
 };
 
-// this pulls in segment's trackLink, which stalls the page load until the analytics request is done
+// this uses segment's trackLink, which stalls the page load until the analytics request is done
 // it forces a full page load at the end, so don't use it for links within the community site
-class TrackedExternalLinkWithoutContext extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ref = React.createRef();
-  }
+export const TrackedExternalLink = ({ children, name, properties, to, ...props }) => {
+  const inherited = React.useContext(Context);
 
-  componentDidMount() {
+  const nameRef = React.useRef(name);
+  const propertiesRef = React.useRef({});
+  React.useEffect(() => {
+    nameRef.current = name;
+    propertiesRef.current = resolveProperties(properties, inherited.properties);
+  });
+
+  const ref = React.useRef(null);
+  React.useEffect(() => {
     try {
-      analytics.trackLink(this.ref.current, () => this.props.name, () => this.props.properties);
+      // we only call this on first render, use refs to keep the name/properties up to date
+      analytics.trackLink(ref.current, () => nameRef.current, () => propertiesRef.current);
     } catch (error) {
       captureException(error);
     }
-  }
-
-  render() {
-    const { children, to, ...props } = this.props;
-    return (
-      <a href={to} {...omit(props, ['name', 'properties'])} ref={this.ref}>
-        {children}
-      </a>
-    );
-  }
-}
-export const TrackedExternalLink = ({ children, name, properties, to, ...props }) => (
-  <Context.Consumer>
-    {(inherited) => (
-      <TrackedExternalLinkWithoutContext to={to} name={name} properties={resolveProperties(properties, inherited.properties)} {...props}>
-        {children}
-      </TrackedExternalLinkWithoutContext>
-    )}
-  </Context.Consumer>
-);
+  }, []);
+  return (
+    <a href={to} {...omit(props, ['name', 'properties', 'to'])} ref={ref}>
+      {children}
+    </a>
+  );
+};
 TrackedExternalLink.propTypes = {
   children: PropTypes.node.isRequired,
   name: PropTypes.string.isRequired,
   properties: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   to: PropTypes.string.isRequired,
 };
-
 TrackedExternalLink.defaultProps = {
   properties: {},
 };
