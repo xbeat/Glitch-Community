cd5b793198cb4d41359ea565de856912c164f204
Glitch (community-staging) <none>
2019-03-14 17:34:37 +0000

    ⭐️🦃 Checkpoint ./server/constants.js:301412/3895

--- server/constants.js
+++ server/constants.js
@@ -42,9 +42,9 @@ const sources = {
     'fast.wistia.com',
   ],
   styles: ['*.webtype.com', 'cdn.glitch.com', 'cdn.gomix.com'],
-  images: ['*.glitch.com', '*.gomix.com', 'cdn.hyperdev.com', 's3.amazonaws.com', '*.webtype.com', 'culture-zine.glitch.me'],
+  images: ['*.glitch.com', '*.gomix.com', 'cdn.hyperdev.com', 's3.amazonaws.com', '*.webtype.com', 'fast.wistia.com', 'culture-zine.glitch.me'],
   fonts: ['*.webtype.com'],
-  connect: ['api.glitch.com', 'api.segment.io', '*.wistia.com', '*.litix.io'],
+  connect: ['api.glitch.com', 'api.segment.io', '*.wistia.com', '*.litix.io', '*.akamaihd.net'],
   frames: ['glitch.com', '*.glitch.me', 'fast.wistia.com'],
 };
 
