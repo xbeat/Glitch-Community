c87e1fb0f5be47a659d13676dd0daae3c21d764c
Glitch (community) <none>
2019-03-14 17:04:33 +0000

    🏠😹 Checkpoint ./src/presenters/includes/included-in-collections.js:271885/1339

--- src/presenters/includes/included-in-collections.js
+++ src/presenters/includes/included-in-collections.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { createRef, useEffect } from 'react';
 import { getSingleItem, getAllPages, allByKeys } from '../../../shared/api';
 
 const getIncludedCollections = async (api, projectId) => {
-  
+  // TODO: are we only getting 3 collections here?
   const collections = await getAllPages(api,`/v1/projects/by/id/collections?id=${projectId}&limit=100&orderKey=createdAt&orderDirection=DESC`);
-  const withProjectsAndUsers = await Promise.all(collections.map(async (collection) => {
-    const { projects, users } = await allByKeys({
-      // TODO: are we only getting 3 items here?
-      projects: getAllPages(api,`/v1/projects/by/id/collections?id=${projectId}&limit=100&orderKey=createdAt&orderDirection=DESC`),
-      users: getAll
+  return await Promise.all(collections.map(async (collection) => {
+    const { projects, user } = await allByKeys({
+      // TODO: are we only getting 3 projects here?
+      projects: getAllPages(api,`/v1/collections/by/id/projects?id=${collection.id}&limit=100&orderKey=createdAt&orderDirection=DESC`),
+      user: getSingleItem(api, `v1/users/by/id?id=${collection.user.id}`, collection.user.id),
     })
-    return { ...collection, projects, users }
+    return { ...collection, projects, user }
   }));
 };
 
+const useAPI = (cb, ...args) => {
+  const ref = createRef()
+  useEffect(async () => {
+    ref.current = await cb(...args)
+  }, args)
+  return ref.current
+}
+
 const IncludedInCollections = ({ api, projectId }) => {
-  console.log('included');
+  const data = useAPI(getIncludedCollections, api, projectId)
   return (
     <>
       <h2>Included in Collections</h2>
