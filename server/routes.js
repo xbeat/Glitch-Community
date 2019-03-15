9acf3f49ca845194e3d5e06e572b1a0cbc855d68
Glitch (community-staging) <none>
2019-03-15 13:41:50 +0000

    🙅📅 Checkpoint ./server/routes.js:301412/21

--- server/routes.js
+++ server/routes.js
@@ -96,7 +96,6 @@ module.exports = function(external) {
   app.use(helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'none'"],
-      /*
       scriptSrc: [
         "'self'",
         ...sources.scripts,
@@ -104,14 +103,13 @@ module.exports = function(external) {
       ],
       // style-src unsafe-inline is required for our SVGs
       // for context and link to bug, see https://pokeinthe.io/2016/04/09/black-icons-with-svg-and-csp/
-      styleSrc: ["'self'", "'unsafe-inline'", ...sources.styles],
+      // styleSrc: ["'self'", "'unsafe-inline'", ...sources.styles],
       imgSrc: ["'self'", ...sources.images],
       fontSrc: ["'self'", ...sources.fonts],
       connectSrc: ["'self'", ...sources.connect],
       frameSrc: ["'self'", ...sources.frames],
       mediaSrc: ["'self'", ...sources.media],
       baseUri: ["'self'"],
-      */
       reportUri: 'https://csp-reporting-server.glitch.me/report',
     }
   }));
