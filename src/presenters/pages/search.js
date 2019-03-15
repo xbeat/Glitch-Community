5b12cddb72a5f5d64491732c253362e0df5b35f7
Glitch (pentagonal-streetcar) <none>
2019-03-15 18:55:20 +0000

    💇🛣 Checkpoint ./src/presenters/pages/search.js:867163/399 ./styles/buttons.styl:867163/102

--- src/presenters/pages/search.js
+++ src/presenters/pages/search.js
@@ -38,7 +38,7 @@ const FilterContainer = ({ filters, activeFilter, setFilter, query, loaded }) =>
         {filters.map(
           (filter) =>
             (filter.hits === null || filter.hits > 0) && (
-              <Button key={filter.name} size="small" type="tertiary" onClick={() => setFilter(filter.name)}>
+              <Button key={filter.name} size="small" type="tertiary" onClick={evt => setFilter(filter.name, evt)}>
                 {capitalize(filter.name)}
                 {filter.hits > 0 && <div className="status-badge">{filter.hits}</div>}
               </Button>
@@ -134,7 +134,10 @@ class SearchResults extends React.Component {
     this.searchProjects().catch(handleError);
   }
 
-  setFilter(filter) {
+  setFilter(filter, btnEvt) {
+    console.log(btnEvt);
+    document.querySelectorAll('.segmented-buttons button').forEach((btn) => btn.classList.remove('active'));
+    btnEvt.target.classList.add('active');
     this.setState({ activeFilter: filter });
   }
 
