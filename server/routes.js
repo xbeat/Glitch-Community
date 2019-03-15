4b4ab53be565eab81f8fd74510eca65ba649e649
Glitch (community-staging) <none>
2019-03-15 15:55:06 +0000

    😥💩 Checkpoint ./server/routes.js:301412/195

--- server/routes.js
+++ server/routes.js
@@ -96,17 +96,17 @@ module.exports = function(external) {
   app.use(helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'none'"],
-      /*scriptSrc: [
+      scriptSrc: [
         "'self'",
         ...sources.scripts,
         (req, res) => res.locals.nonces.map(n => `'nonce-${n}'`).join(' ')
-      ],*/
+      ],
       // style-src unsafe-inline is required for our SVGs
       // for context and link to bug, see https://pokeinthe.io/2016/04/09/black-icons-with-svg-and-csp/
-      // styleSrc: ["'self'", "'unsafe-inline'", ...sources.styles],
-      // imgSrc: ["'self'", ...sources.images],
+      styleSrc: ["'self'", "'unsafe-inline'", ...sources.styles],
+      imgSrc: ["'self'", ...sources.images],
       fontSrc: ["'self'", ...sources.fonts],
-      // connectSrc: ["'self'", ...sources.connect],
+      connectSrc: ["'self'", ...sources.connect],
       frameSrc: ["'self'", ...sources.frames],
       mediaSrc: ["'self'", ...sources.media],
       baseUri: ["'self'"],
