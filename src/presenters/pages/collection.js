31b2b6ea948348eca3e3678f2c2d95b5d2c86673
Glitch (iced-flag) <none>
2019-03-15 14:53:48 +0000

    Clean up

--- src/presenters/pages/collection.js
+++ src/presenters/pages/collection.js
@@ -86,7 +86,7 @@ const CollectionPageContents = ({
 }) => {
   const collectionHasProjects = !!collection && !!collection.projects;
   const userIsLoggedIn = currentUser && currentUser.login;
-  console.log("true?", !userIsAuthor && !userIsLoggedIn)
+
   return (
     <>
       <Helmet>
--- src/presenters/projects-list.js
+++ src/presenters/projects-list.js
@@ -72,15 +72,13 @@ ExpandyProjects.defaultProps = {
   maxCollapsedProjects: 12,
 };
 
-export const ProjectsUL = ({ ...props }) => {
-  console.log("sup these are the props", props)
-  return (
+export const ProjectsUL = ({ ...props }) => (
   <ul className="projects-container">
     {props.projects.map((project) => (
       <ProjectItem key={project.id} {...{ project }} {...props} />
     ))}
   </ul>
-)};
+);
 
 ProjectsUL.propTypes = {
   projects: PropTypes.array.isRequired,
