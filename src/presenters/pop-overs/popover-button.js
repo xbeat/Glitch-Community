2ce23f22eeac4b81a8e6563fa8b8ee45166d4fe7
Glitch (wax-snowstorm) <none>
2019-03-16 21:31:56 +0000

    🏆🎨 Checkpoint ./src/presenters/pop-overs/popover-button.js:252452/1824 ./server/.eslintrc.server.js:252452/98 ./server/webpack.js:252452/2

--- src/presenters/pop-overs/popover-button.js
+++ src/presenters/pop-overs/popover-button.js
@@ -10,23 +10,27 @@ const PopoverButton = ({ onClick, text, emoji, link }) => (
 );
 
 PopoverButton.propTypes = {
-  onClick: PropTypes.func,
+  /** text to display in the button */
   text: PropTypes.node.isRequired,
+  /** emoji to display in the button */
   emoji: PropTypes.string.isRequired,
-  link: PropTypes.string,
-};
-
-PopoverButton.defaultPropTypes = {
-  onClick: function(props, propName, componentName) {
+  /** onClick function activated when button is clicked */
+  onClick(props, propName) {
     if (props.link === false && (props[propName] === null || typeof props[propName] !== 'function')) {
       return new Error('Please provide a link or an onClick function');
     }
   },
-  link: function(props, propName, componentName) {
+  /** link followed when button is clicked */
+  link(props, propName) {
     if (props.onClick === false && (props[propName] === null || typeof props[propName] !== 'string')) {
       return new Error('Please provide a link or an onClick function');
     }
   },
+};
+
+PopoverButton.defaultProps = {
+  onClick: null,
+  link: null,
 };
 
 export default PopoverButton;
