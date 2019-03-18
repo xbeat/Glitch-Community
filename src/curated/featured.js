ef9cccc0920ba4dcf4097db38aabd7efc880b8fa
Glitch (community-staging) <none>
2019-03-18 14:20:12 +0000

    🐿😘 Checkpoint ./src/curated/featured.js:11/1191

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -14,18 +14,18 @@ Example:
 // make sure image urls use https
 export default [
   {
-    title: 'Shout out your peoples',
-    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fglitch_shoutout.jpg?1552302001451',
-    link: 'https://glitch.com/~shout-out',
+    title: 'Feel less lonely',
+    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fimg_2.jpg?1552917252788',
+    link: 'https://glitch.com/~handholding-simulator',
   },
   {
-    title: 'A dating app for biscuit lovers',
-    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fglitch_biscuit.jpg?1552302002356',
-    link: 'https://biscuittinder.glitch.me/',
+    title: 'Alternatives to "you guys"',
+    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fimg_1.jpg?1552917246402',
+    link: 'https://glitch.com/~vosotros',
   },
   {
-    title: 'Remember the metaverse? D\'oh!',
-    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fglitch_homer.jpg?1552302001952',
-    link: 'https://homer-metaverse.glitch.me/',  
+    title: 'Fine art in 100 rectangles',
+    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fimg_3.jpg?1552917253176',
+    link: 'https://glitch.com/~thorn-larkspur',  
   }
 ];
