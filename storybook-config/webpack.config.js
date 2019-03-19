af10ee104ebf69670584ec5fb6228f6d2edfe3fd
Glitch (wax-snowstorm) <none>
2019-03-19 15:28:27 +0000

    👷💋 Checkpoint ./storybook-config/webpack.config.js:252452/3145

--- storybook-config/webpack.config.js
+++ storybook-config/webpack.config.js
@@ -1,26 +1,29 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  config.module.rules.push({
+    test: /\.styl$/,
+    use: [
+      'style-loader',
       {
-        test: /\.styl$/,
-        use: [
-          'style-loader',
-          {
-            loader: "css-loader?modules",
-            options: {
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            },
-          },
-          {
-            loader: 'stylus-loader',
-          }
-        ],
-        include: path.resolve(__dirname, "../")
-      }
-    ]
-  }
+        loader: 'css-loader?modules',
+        options: {
+          sourceMap: true,
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+      {
+        loader: 'stylus-loader',
+      },
+    ],
+    include: path.resolve(__dirname, '../'),
+  });
+  config.resolve.modules.push(path.resolve('./'));
+
+  return config;
 };
