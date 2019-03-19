8a8c579c3873c76f065cdb97fba980adb213e52a
Glitch (community) <none>
2019-03-19 20:58:10 +0000

    🐋🐿 Checkpoint ./src/presenters/more-collections.js:433767/1736 ./src/models/collection.js:433767/2

--- src/presenters/more-collections.js
+++ src/presenters/more-collections.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import { loadAllCollections } from './featured-collections';
 import { DataLoader } from './includes/loader';
 import { CoverContainer } from './includes/profile';
-import { getProfileStyle } from '../models/user';
+import { UserLink } from './includes/link';
+import { getProfileStyle, getDisplayName } from '../models/user';
 
+// move to components
 //I wonder if this needs a new name or should be combined with the existing collectionItem in some way
-const CollectionItem = ({ title, description, projects }) => {
+const CollectionItem = ({ name, description, projects }) => {
   // should this whole thing be a button, an anchor tag, or a div? end result is to make it all clickable
   return (
     <div>
-      <button>{title}</button>
+      <button>{name}</button>
       <div>{description}</div>
       <div>projectslength:{projects.length}</div>
     </div>
@@ -28,8 +30,12 @@ class MoreCollections extends React.Component {
     const { api, currentUser } = this.props;
     const collectionsToLoad = currentUser.collections.map((c) => ({ owner: currentUser.login, name: c.url }));
     const coverStyle = getProfileStyle({ ...currentUser, cache: currentUser._cacheCover })
+    // is there a way to know if a collection was created by a user vs a team ? 
     return (
       <div>
+        <h2>
+          <UserLink user={currentUser}>More by {getDisplayName(currentUser)} →</UserLink>
+        </h2>
         <DataLoader get={() => loadAllCollections(api, collectionsToLoad)}>
         {(collections) => {
           return (
