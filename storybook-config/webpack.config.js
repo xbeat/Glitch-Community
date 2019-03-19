9762876aee828bc4a68897c8adfdf4bd186ba091
Glitch (wax-snowstorm) <none>
2019-03-19 15:38:45 +0000

    🏈📈 Checkpoint ./storybook-config/webpack.config.js:252452/8596

--- storybook-config/webpack.config.js
+++ storybook-config/webpack.config.js
@@ -1,29 +1,32 @@
 const path = require('path');
 
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const appConfig = require('../webpack.config.js');
 
-module.exports = (baseConfig, env) => {
-  const config = genDefaultConfig(baseConfig, env);
-
-  config.module.rules.push({
-    test: /\.styl$/,
-    use: [
-      'style-loader',
+module.exports = {
+  module: {
+    rules: [
       {
-        loader: 'css-loader?modules',
-        options: {
-          sourceMap: true,
-          modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]',
-        },
-      },
-      {
-        loader: 'stylus-loader',
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader?modules',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'stylus-loader',
+          },
+        ],
+        include: path.resolve(__dirname, '../src/components'),
       },
     ],
-    include: path.resolve(__dirname, '../'),
-  });
-  config.resolve.modules.push(path.resolve('./'));
-
-  return config;
+  },
+  resolve: {
+    modules: [path.resolve('./')],
+    alias: appConfig.resolve.alias,
+  },
 };
