493004f7329cdcd49601e8c53fead7311fdec323
Glitch (iced-flag) <none>
2019-03-20 15:43:59 +0000

    ✈️🐚 Checkpoint ./src/presenters/pages/collection.js:433767/50 ./src/presenters/includes/description-field.js:433767/34

--- src/presenters/pages/collection.js
+++ src/presenters/pages/collection.js
@@ -250,7 +250,7 @@ async function loadCollection(api, ownerName, collectionName) {
       const { data: projects } = await api.get(`projects/byIds?ids=${collection.projects.map(({ id }) => id).join(',')}`);
       collection.projects = projects.map((project) => {
         const collectionProject = _.find(collection.collectionProjects, (p) => p.projectId === project.id);
-        if (collectionProject && collectionProject.annotation) {
+        if (collectionProject && collectionProject.annotation && ) {
           project.note = _.trim(collectionProject.annotation);
           project.isAddingANewNote = true;
         }
