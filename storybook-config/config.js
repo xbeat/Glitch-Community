ac236a0d2b847d501b5281b235c8bfb0a0aa73dd
Glitch (lumpy-organ) <none>
2019-03-20 16:14:02 +0000

    ⛷🎑 Checkpoint ./src/presenters/header.js:252452/90 ./package.json:252452/6122 ./stories/index.js:252452/50 ./storybook-config/config.js:252452/301

--- storybook-config/config.js
+++ storybook-config/config.js
@@ -1,7 +1,15 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+addDecorator(
+  withInfo({
+    header: false, // Global configuration for the info addon across all of your stories.
+  })
+);
 
 function loadStories() {
   require('../stories/index.js');
+  
   // You can require as many stories as you need.
 }
 
