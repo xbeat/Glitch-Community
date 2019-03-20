6ccad890667ec51a9429b4d7f19245cf06d7e076
Glitch (community) <none>
2019-03-20 18:34:12 +0000

    🛍🚥 Checkpoint ./src/presenters/pages/collection.js:433767/157 ./src/presenters/note.js:433767/24

--- src/presenters/pages/collection.js
+++ src/presenters/pages/collection.js
@@ -165,7 +165,6 @@ const CollectionPageContents = ({
                       <ProjectsUL
                         {...props}
                         projects={collection.projects}
-                        author={collection.user}
                         api={api}
                         collectionCoverColor={collection.coverColor}
                         hideNote={hideNote}
