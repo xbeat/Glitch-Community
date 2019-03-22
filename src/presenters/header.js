2f75b0c70791da95d04082d5f5d47261ea2e418c
Glitch (community-staging) <none>
2019-03-22 20:31:25 +0000

    lint

--- src/presenters/header.js
+++ src/presenters/header.js
@@ -20,29 +20,21 @@ const ResumeCoding = () => (
   </TrackedExternalLink>
 );
 
-function SearchForm ({ defaultValue }) {
-  const [value, onChange] = useState(defaultValue)
-  const [submitted, setSubmitted] = useState(false)
+function SearchForm({ defaultValue }) {
+  const [value, onChange] = useState(defaultValue);
+  const [submitted, setSubmitted] = useState(false);
   const onSubmit = (event) => {
     event.preventDefault();
     if (!value) return;
-    setSubmitted(true)
-  }
-  
+    setSubmitted(true);
+  };
+
   return (
     <form action="/search" method="get" role="search" onSubmit={onSubmit}>
-      <TextInput
-        className="header-search"
-        name="q"
-        onChange={onChange}
-        opaque
-        placeholder="bots, apps, users"
-        type="search"
-        value={value}
-      />
+      <TextInput className="header-search" name="q" onChange={onChange} opaque placeholder="bots, apps, users" type="search" value={value} />
       {submitted && <Redirect to={`/search?q=${value}`} push />}
     </form>
-    );
+  );
 }
 
 SearchForm.propTypes = {
