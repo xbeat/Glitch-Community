6c7fea96d93658285328e32e4664a8138976f0ba
Glitch (community-staging) <none>
2019-03-25 11:45:06 +0000

    🚴🏃 Checkpoint ./src/curated/featured-embed.js:11/580

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -1,13 +1,13 @@
 const body =
-  '<p>Get started with Virtual Reality.</p><p>A free, 5-part video course with interactive code examples that will teach you the fundamentals of WebVR using A-Frame.</p>';
+  '<p>How today’s technologies from artificial intelligence to generative scripts are changing the way art is made.</p>';
 
 // make sure image urls use https
 export default {
-  image: 'https://culture-zine.glitch.me/culture/content/images/2019/02/WebVR-Starter-Kit.-Part-1_-Intro-to-WebVR-1.png',
+  image: 'https://culture-zine.glitch.me/culture/content/images/2019/03/artCodeBanner-1.png',
   mask: 'mask-4',
-  title: 'WebVR Starter Kit',
-  appDomain: 'starter-aframe-animated',
-  blogUrl: '/an-intro-to-webvr/',
+  title: 'When Art Meets Algorithm',
+  appDomain: 'orbit',
+  blogUrl: '/art-meets-algorithm/',
   body,
-  color: '#F39B4D',
+  color: '#f5f5f5'
 };
