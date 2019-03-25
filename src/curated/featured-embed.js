2ab5060a4c1096bf3b01d1639e15837316d95c5c
Glitch (community-staging) <none>
2019-03-25 11:55:09 +0000

    🌹📮 Checkpoint ./src/curated/featured-embed.js:11/259

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -1,9 +1,9 @@
 const body =
-  '<p>How today’s technologies from artificial intelligence to generative scripts are changing the way art is made.</p>';
+  '<p>How today’s tech from artificial intelligence to virtual reality is changing the way art is made.</p>';
 
 // make sure image urls use https
 export default {
-  image: 'https://culture-zine.glitch.me/culture/content/images/2019/03/artCodeBanner-1.png',
+  image: 'https://cdn.glitch.com/3cef6b25-69ba-4fa9-aa32-cff0fedce195%2FGlitch_Illo_TechArt-Article-Inline1-v1%20(1).jpg?1553514484331',
   mask: 'mask-4',
   title: 'When Art Meets Algorithm',
   appDomain: 'orbit',
