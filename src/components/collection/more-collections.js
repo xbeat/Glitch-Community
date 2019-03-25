5a3a7fb4841ba4c3009503d3c605742f385d9436
Glitch (community) <none>
2019-03-25 15:47:54 +0000

    😺🐥 Checkpoint ./src/components/collection/more-collections.js:433767/2124 ./styles/more-collections.styl:433767/120

--- src/components/collection/more-collections.js
+++ src/components/collection/more-collections.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 import { getProfileStyle as getUserStyle, getDisplayName } from '../../models/user';
 import { getProfileStyle as getTeamStyle } from '../../models/team';
-import { getLink } from '../../models/collection';
+// import { getLink } from '../../models/collection';
 
 import { CoverContainer } from '../../presenters/includes/profile';
 import { UserLink, TeamLink } from '../../presenters/includes/link';
+import CollectionItem from '../../presenters/collection-item';
 
-import Markdown from '../text/markdown';
-import Button from '../buttons/button';
+// import Markdown from '../text/markdown';
+// import Button from '../buttons/button';
 
 // import styles from './more-collections.styl';
 // TODO:
@@ -17,6 +18,12 @@ import Button from '../buttons/button';
 // - componentize UserLink and TeamLink (this is in-flight by Sheridan, probs will just be one generic link)
 // bring in styles from here and delete styles/more-collections.styl
 
+
+// <a key={id} href={getLink({ user, team, url })} className="more-collections-item" style={{ backgroundColor: coverColor }}>
+//   <Button>{name}</Button>
+//   <Markdown>{description}</Markdown>
+//   <div className="projects-count">{projectsCount}</div>
+// </a>
 const MoreCollections = ({ currentCollection, collections, currentUser }) => {
   const isUserCollection = currentCollection.teamId === -1;
   const coverStyle = isUserCollection
@@ -35,16 +42,7 @@ const MoreCollections = ({ currentCollection, collections, currentUser }) => {
       <CoverContainer style={coverStyle} className="collections">
         <div className="more-collections">
           {
-            collections.map(({ name, description, projects, coverColor, user, team, url, id }) => {
-              const projectsCount = `${projects.length} project${projects.length === 1 ? '' : 's'}`;
-              return (
-                <a key={id} href={getLink({ user, team, url })} className="more-collections-item" style={{ backgroundColor: coverColor }}>
-                  <Button>{name}</Button>
-                  <Markdown>{description}</Markdown>
-                  <div className="projects-count">{projectsCount}</div>
-                </a>
-              );
-            })
+            collections.map((collection) => <CollectionItem showCurator={false} key={collection.id} collection={collection} />)
           }
         </div>
       </CoverContainer>
