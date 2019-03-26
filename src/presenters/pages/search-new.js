1771713df8bd8020a0375e0630e539de438c293d
Glitch (community-staging) <none>
2019-03-26 21:02:48 +0000

    🙌🔦 Checkpoint ./src/state/search.js:271885/990 ./src/presenters/pages/search-new.js:271885/4041

--- src/presenters/pages/search-new.js
+++ src/presenters/pages/search-new.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { capitalize, sum } from 'lodash';
@@ -6,6 +6,7 @@ import algoliasearch from 'algoliasearch';
 
 import { useAPI } from '../../state/api';
 import { useCurrentUser } from '../../state/current-user';
+import { useSearch } from '../../state/search';
 import useErrorHandlers from '../error-handlers';
 
 import Layout from '../layout';
@@ -16,16 +17,35 @@ import ProjectsList from '../projects-list';
 import TeamItem from '../team-item';
 import UserItem from '../user-item';
 
-const SearchResults = () => null;
+
+function SearchResults ({ query }) {
+  const [activeFilter, setActiveFilter] = useState('all')
+  const { hits } = useSearch(query)
+  const noResults = hits.length === 0
+  
+  const filters = [
+    { name: 'all' },
+    { name: 'teams' },
+    { name: 'users' },
+    { name: 'projects' },
+  ];
+  
+  console.log(hits)
+  
+  return (
+      <main className="search-results">
+        <FilterContainer filters={filters} setFilter={setActiveFilter} activeFilter={activeFilter} query={query} loaded={loaded} />
+        {noResults && <NotFound name="any results" />}
+      </main>
+    );
+}
+
 
 const SearchPage = ({ query }) => {
-  const api = useAPI();
-  const { currentUser } = useCurrentUser();
-  const errorFuncs = useErrorHandlers();
   return (
     <Layout searchQuery={query}>
       {!!query && <Helmet title={`Search for ${query}`} />}
-      {query ? <SearchResults {...errorFuncs} api={api} query={query} currentUser={currentUser} /> : <NotFound name="anything" />}
+      {query ? <SearchResults query={query}/> : <NotFound name="anything" />}
       <MoreIdeas />
     </Layout>
   );
--- src/state/search.js
+++ src/state/search.js
@@ -1,18 +1,14 @@
 import algoliasearch from 'algoliasearch/lite';
 import { useState, useEffect } from 'react';
 
-const searchClient = algoliasearch(
-  'LAS7VGSQIQ',
-  '27938e7e8e998224b9e1c3f61dd19160'
-);
+const searchClient = algoliasearch('LAS7VGSQIQ', '27938e7e8e998224b9e1c3f61dd19160');
 
-const searchIndex = searchClient.initIndex('search')
+const searchIndex = searchClient.initIndex('search');
 
-export function useSearch (query) {
-  const [results, setResults] = useState({ hits: [], nbHits: 0 })
+export function useSearch(query) {
+  const [results, setResults] = useState({ hits: [], nbHits: 0 });
   useEffect(() => {
-    searchIndex.search({ query })
-      .then((res) => setResults(res))
-  }, [query])
-  return results
+    searchIndex.search({ query, hitsPerPage: 100 }).then((res) => setResults(res));
+  }, [query]);
+  return results;
 }
