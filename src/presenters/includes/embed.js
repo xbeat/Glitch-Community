f19e9278a77ceaff4f5e0a289436886533313421
Glitch (community) <none>
2019-03-26 18:38:11 +0000

    remove state from embed

--- src/presenters/includes/embed.js
+++ src/presenters/includes/embed.js
@@ -8,13 +8,17 @@ const telescopeImageUrl = 'https://cdn.glitch.com/7138972f-76e1-43f4-8ede-84c3cd
 // Babel does not transpile URLSearchParams, so using this as a compatibility check for showing embeds now.
 // TODO(sheridan) make this more robust in future
 const browserSatisfiesRequirements = (() => {
-  if ('URLSearchParams' in window && window.URLSearchParams.name === 'URLSearchParams') {
-    return true
-  } 
-  console.log(
-    "Sorry, you don't have the necessary JavaScript features to run Glitch code editors. Try applying your latest system updates, or try again with a different web browser.",
-    error,
-  );
+  try {
+    if (URLSearchParams.name !== 'URLSearchParams') {
+      throw new Error('URLSearchParams is minified, so it must have been polyfilled');
+    }
+    return true;
+  } catch (error) {
+    console.log(
+      "Sorry, you don't have the necessary JavaScript features to run Glitch code editors. Try applying your latest system updates, or try again with a different web browser.",
+      error,
+    );
+  }
   return false;
 })();
 
