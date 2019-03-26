7d9313f0f533d4495fae51bd9753963d76e14cb0
Glitch (community) <none>
2019-03-26 15:09:06 +0000

    🎹🎺 Checkpoint ./storybook-config/webpack.config.js:252452/1778 ./stories/index.js:252452/56

--- storybook-config/webpack.config.js
+++ storybook-config/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = async ({ config, mode }) => {
   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
@@ -6,26 +6,26 @@ module.exports = async ({ config, mode }) => {
   // 'PRODUCTION' is used when building the static version of storybook.
 
   // Make whatever fine-grained changes you need
-  config.mode = 'development'
-  
+  config.devtool = 'source-map';
+
   config.module.rules.push({
-        test: /\.styl$/,
-        use: [
-          'style-loader',
-          {
-            loader: "css-loader?modules",
-            options: {
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            },
-          },
-          {
-            loader: 'stylus-loader',
-          },
-        ],
-        include: path.resolve(__dirname, "../src/components")
-      });
+    test: /\.styl$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader?modules',
+        options: {
+          sourceMap: true,
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+      {
+        loader: 'stylus-loader',
+      },
+    ],
+    include: path.resolve(__dirname, '../src/components'),
+  });
 
   // Return the altered config
   return config;
