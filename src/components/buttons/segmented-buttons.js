9f3d6da5673de6602c72c6a762be6a8cd0cfcbc5
Glitch (community) <none>
2019-03-27 15:24:26 +0000

    i really botched that merge i guess

--- src/components/buttons/segmented-buttons.js
+++ src/components/buttons/segmented-buttons.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import styles from './segmented-buttons.styl';
 import Button from './button';
 
-const SegmentedButtons = ({ value, buttons, onClick }) => (
+const SegmentedButtons = ({ value, buttons, onChange }) => (
   <div className={styles.segmentedButtons}>
     {buttons.map((button) => (
-      <Button key={button.name} size="small" type="tertiary" active={button.name === value} onClick={() => onClick(button.name)}>
+      <Button key={button.name} size="small" type="tertiary" active={button.name === value} onClick={() => onChange(button.name)}>
         {button.contents}
       </Button>
     ))}
@@ -16,13 +16,13 @@ const SegmentedButtons = ({ value, buttons, onClick }) => (
 SegmentedButtons.propTypes = {
   buttons: PropTypes.arrayOf(
     PropTypes.shape({
-      /** name: the filter name, passed back to onClick */
+      /** name: the filter name, passed back to onChange */
       name: PropTypes.string.isRequired,
       contents: PropTypes.node.isRequired,
     }),
   ).isRequired,
   value: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default SegmentedButtons;
