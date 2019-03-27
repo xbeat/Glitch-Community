b9a7d7cb50c3803f2af577865656a782b3303f43
Glitch (community) <none>
2019-03-27 16:21:45 +0000

    🌎🙎 Checkpoint ./src/curated/collections.js:355867/550

--- src/curated/collections.js
+++ src/curated/collections.js
@@ -2,9 +2,11 @@
 
 // What collections to show in the expanded area on the homepage
 export const featuredCollections = [
-  { owner: 'glitch', name: 'probot-your-bff-on-github' },
   { owner: 'glitch', name: 'make-me-multilingual' },
   { owner: 'glitch', name: 'code-with-comics' },
+  { owner: 'glitch', name: 'sketch-with-p-5' },
+  { owner: 'clever-assembly-test', name: 'forest-melange' },
+  { owner: 'empty-team', name: 'markdown-overflow-testing' }
 ];
 
 // More ideas is populated from this team
