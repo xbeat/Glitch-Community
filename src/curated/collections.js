ebf1310c78a544c810969d2098ca3edb2e9237a1
Glitch (pickle-meteoroid) <none>
2019-03-27 19:37:51 +0000

    🛰🤑 Checkpoint ./src/curated/collections.js:252452/235

--- src/curated/collections.js
+++ src/curated/collections.js
@@ -2,15 +2,9 @@
 
 // What collections to show in the expanded area on the homepage
 export const featuredCollections = [
-<<<<<<< HEAD
-  { owner: 'glitch', name: 'discover-modern-art' },
-  { owner: 'glitch', name: 'this-week-on-glitch-mar-26-2019' },
-  { owner: 'glitch', name: 'code-with-comics' },
-=======
   { owner: 'glitch', name: 'draw-with-music' },
   { owner: 'glitch', name: 'this-week-on-glitch-mar-27-2019' },
   { owner: 'glitch', name: 'glitch-flix' },
->>>>>>> d47043a2e2596c2352f2ab9455c87921c7efd556
 ];
 
 // More ideas is populated from this team
