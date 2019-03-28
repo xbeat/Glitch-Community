7bb54e9e0390413f42f91c1d89a6a32ba663076f
Glitch (wax-snowstorm) <none>
2019-03-28 22:00:44 +0000

    👟😂 Checkpoint ./.eslintrc.js:252452/79

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 // Add new aliases in webpack.config.js. This file will automatically pull them here so Babel doesn't complain about them.
+// More details at https://www.npmjs.com/package/eslint-import-resolver-alias
+
 const aliases = require('./webpack.config.js').resolve.alias;
 
 const mappings = Object.keys(aliases).map((alias) => {
