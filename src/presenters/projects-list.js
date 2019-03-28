5dd6395801060adde264612bcd35c7cf18c65390
Glitch (community-staging) <none>
2019-03-28 13:57:00 +0000

    👷💎 Checkpoint ./src/presenters/projects-list.js:301412/26397

--- src/presenters/projects-list.js
+++ src/presenters/projects-list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ProjectItem from './project-item';
 import ExpanderContainer from '../components/containers/expander';
@@ -6,6 +6,19 @@ import Badge from '../components/badges/badge';
 
 import Heading from '../components/text/heading';
 
+function ProjectsList({ title, placeholder, extraClasses, ...props }) {
+  return (
+    <article className={`projects ${extraClasses}`}>
+      <Heading tagName="h2">{title}</Heading>
+
+      {!!(placeholder && !props.projects.length) && <div className="placeholder">{placeholder}</div>}
+
+      <ExpandyProjects {...props} />
+    </article>
+  );
+}
+
+/*
 const ProjectsList = ({ title, placeholder, extraClasses, ...props }) => (
   <article className={`projects ${extraClasses}`}>
     <Heading tagName="h2">{title}</Heading>
@@ -15,6 +28,7 @@ const ProjectsList = ({ title, placeholder, extraClasses, ...props }) => (
     <ExpandyProjects {...props} />
   </article>
 );
+*/
 
 ProjectsList.propTypes = {
   projects: PropTypes.array.isRequired,
@@ -28,6 +42,44 @@ ProjectsList.defaultProps = {
   extraClasses: '',
 };
 
+function ExpandyProjects(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  function handleClick() {
+    setExpanded(true);
+  }
+
+  const maxProjects = props.maxCollapsedProjects;
+  const totalProjects = props.projects.length;
+  const hiddenProjects = totalProjects - maxProjects;
+
+  // props needs to exclude projects, so can't be declared on a separate line as const
+  let { projects } = props; // eslint-disable-line prefer-const
+
+  let shouldShowButton = false;
+  if (!expanded) {
+    shouldShowButton = hiddenProjects > 0;
+    projects = projects.slice(0, maxProjects);
+  }
+
+  return (
+    <ExpanderContainer
+      expanded={!shouldShowButton}
+      controlArea={
+        <div>
+          <button>Paginate</button>
+          <button className="button-tertiary" onClick={handleClick} type="button">
+            Show all<Badge>{hiddenProjects}</Badge>
+          </button>
+        </div>
+      }
+    >
+      <ProjectsUL projects={projects} {...props} />
+    </ExpanderContainer>
+  );
+}
+
+/*
 class ExpandyProjects extends React.Component {
   constructor(props) {
     super(props);
@@ -71,6 +123,7 @@ class ExpandyProjects extends React.Component {
     );
   }
 }
+*/
 
 ExpandyProjects.propTypes = {
   projects: PropTypes.array.isRequired,
