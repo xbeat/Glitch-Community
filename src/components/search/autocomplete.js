c2fa96ad8345520d167b49eae1e6d253929f300b
Glitch (community) <none>
2019-03-29 16:55:48 +0000

    📻🚲 Checkpoint ./src/components/search/autocomplete.js:271885/4755

--- src/components/search/autocomplete.js
+++ src/components/search/autocomplete.js
@@ -1,37 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TeamLink } from '..
+import { TeamLink, UserLink } from '../../presenters/includes/link';
+import { TeamAvatar, UserAvatar } from '../../presenters/includes/avatar';
+import ProjectAvatar from '../../presenters/includes/project-avatar';
+import Markdown from '../text/markdown';
 
-const styles = {}
+const styles = {};
 
 const TeamResult = ({ value: team }) => (
   <TeamLink team={team} className={styles.resultContainer}>
     <div className={styles.avatarContainer}>
-      <img className={styles.rectAvatar} src={getAvatarUrl(team)} alt="" />
+      <TeamAvatar hideTooltip team={team} />
     </div>
     <div className={styles.infoContainer}>
       <div className={styles.infoPrimary}>{team.name}</div>
       <div className={styles.infoSecondary}>@{team.url}</div>
     </div>
-    <div className={styles.memberContainer}>
-      TODO: avatars
-    </div>
+    <div className={styles.memberContainer}>TODO: user avatars</div>
   </TeamLink>
-)
+);
 
+const UserResult = ({ value: user }) => (
+  <UserLink user={user} className={styles.resultContainer}>
+    <div className={styles.avatarContainer}>
+      <UserAvatar hideTooltip user={user} />
+    </div>
+    <div className={styles.infoContainer}>
+      <div className={styles.infoPrimary}>{user.name}</div>
+      <div className={styles.infoSecondary}>@{user.login}</div>
+    </div>
+  </UserLink>
+);
 
+const ProjectResult = ({ value: project }) => (
+  <ProjectLink project={project} className={styles.resultContainer}>
+    <div className={styles.avatarContainer}>
+      <ProjectAvatar {...project} />
+    </div>
+    <div className={styles.infoContainer}>
+      <div className={styles.infoPrimary}>{project.name}</div>
+      <div className={styles.infoSecondary}>
+        <Markdown truncate={20}>{project.description}</Markdown>
+      </div>
+    </div>
+  </ProjectLink>
+);
 
 const resultGroups = [
   { id: 'team', label: 'Team Results', Component: TeamResult },
-  { id: 'user', label: 'User Results',  },
-  { id: 'project', label: 'Project Results' },
+  { id: 'user', label: 'User Results', Component: UserResult },
+  { id: 'project', label: 'Project Results', Component: ProjectResult },
   { id: 'collection', label: 'Collection Results' },
-]
+];
 
 const AutocompleteResults = ({ query, results }) => {
-  const { status, totalHits } = results
+  const { status, totalHits } = results;
   if (status === 'loading') {
-    return <Loading />
+    return <Loading />;
   }
-
-}
+};
