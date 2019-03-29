ae7aa16a62686d91350043de98034c902535ea6c
Glitch (community) <none>
2019-03-29 16:44:49 +0000

    👗🏠 Checkpoint ./src/components/search/autocomplete.js:271885/3162

--- src/components/search/autocomplete.js
+++ src/components/search/autocomplete.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TeamLink } from '..
 
 const styles = {}
 
-const TeamResult = ({ value })
+const TeamResult = ({ value: team }) => (
+  <TeamLink team={team} className={styles.resultContainer}>
+    <div className={styles.avatarContainer}>
+      <img className={styles.rectAvatar} src={getAvatarUrl(team)} alt="" />
+    </div>
+    <div className={styles.infoContainer}>
+      <div className={styles.infoPrimary}>{team.name}</div>
+      <div className={styles.infoSecondary}>@{team.url}</div>
+    </div>
+    <div className={styles.memberContainer}>
+      TODO: avatars
+    </div>
+  </TeamLink>
+)
+
+
 
 const resultGroups = [
   { id: 'team', label: 'Team Results', Component: TeamResult },
