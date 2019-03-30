e600b6dbd1bb21902a44b9c611a024796117acff
Glitch (community) <none>
2019-03-30 11:02:48 +0000

    🏕🕍 Checkpoint ./src/presenters/pop-overs/create-team-pop.js:511016/34 ./src/components/fields/team-name-input.js:511016/26 ./src/components/fields/team-url-input.js:511016/16

--- src/components/fields/team-url-input.js
+++ src/components/fields/team-url-input.js
@@ -5,6 +5,7 @@ import OptimisticTextInput from './optimistic-text-input';
 
 const TeamUrlInput = ({ url, onChange }) => (
   <OptimisticTextInput
+    
     prefix="@"
     value={url}
     onChange={onChange}
