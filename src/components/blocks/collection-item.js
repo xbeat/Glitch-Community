1c1360c0f5ddc5ca848e5cff7f7f7b1684123f99
Glitch (community-staging) <none>
2019-04-01 21:10:22 +0000

    🐡😅 Checkpoint ./src/components/blocks/collection-item.js:271885/2134 ./src/presenters/collection-item.js:271885/788

--- src/components/blocks/collection-item.js
+++ src/components/blocks/collection-item.js
@@ -8,13 +8,37 @@ const collectionColorStyles = (collection) => ({
   border: collection.coverColor,
 })
 
+const FakeButton = ({ children }) => <div className="button">{children}</div>
+const PrivateIcon = () => <span className="project-badge private-project-badge" aria-label="private" />
+
 const SmallCollectionItem = ({ collection }) => (
   <div className={styles.smallContainer}>
+    <div className={styles.curator}>
+      {collection.user && (
+        <UserLink user={collection.user}>
+          <UserAvatar user={collection.user} />
+        </UserLink>
+      )}
+      {collection.team && (
+        <TeamLink team={collection.team}>
+          <TeamAvatar team={collection.team} />
+        </TeamLink>
+      )}
+    </div>
     <CollectionLink collection={collection}
-      className={styles.smallBody}
+      className={styles.smallLink}
       style={collectionColorStyles(collection)}>
-      <div>
-      
+      <div className={styles.smallNameDescriptionArea}>
+        <div className={styles.nameArea}>
+          <CollectionAvatar color={collection.coverColor} collectionId={collection.id} />
+          <FakeButton>
+            {collection.private && <PrivateIcon />}
+            <div className={styles.collectionName}>{collection.name}</div>
+          </FakeButton>
+        </div>
+        <div className={styles.description}>
+          {collection.description}
+        </div>
       </div>
       <div className={styles.smallProjectCount}>
         <Pluralize count={collection.projectCount} singular="project" /> →
