e80af43bb55dc9c3bcbc98a0b5ca91fae3e16b7f
meowwwls <meowwwls@users.noreply.github.com>
2019-04-01 14:05:33 -0500

    Fix conflict with header.js

--- src/presenters/header.js
+++ src/presenters/header.js
@@ -19,30 +19,6 @@ const ResumeCoding = () => (
   </TrackedExternalLink>
 );
 
-function SearchForm({ defaultValue }) {
-  const [value, onChange] = useState(defaultValue);
-  const [submitted, setSubmitted] = useState(false);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (!value) return;
-    setSubmitted(true);
-  };
-
-  return (
-    <form action="/search" method="get" role="search" onSubmit={onSubmit}>
-      <TextInput labelText="Search Glitch" className="header-search" name="q" onChange={onChange} opaque placeholder="bots, apps, users" type="search" value={value} />
-      {submitted && <Redirect to={`/search?q=${value}`} push />}
-    </form>
-  );
-}
-
-SearchForm.propTypes = {
-  defaultValue: PropTypes.string,
-};
-SearchForm.defaultProps = {
-  defaultValue: '',
-};
-
 const Header = ({ maybeUser, clearUser, searchQuery, showNewStuffOverlay }) => (
   <header role="banner">
     <div className="header-info">
