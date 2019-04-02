7592ea056e595983d559416d47a690eeb1ff1666
Glitch (community-staging) <none>
2019-04-02 16:45:38 +0000

    🎠👷 Checkpoint ./src/components/blocks/project-item.js:271885/2519

--- src/components/blocks/project-item.js
+++ src/components/blocks/project-item.js
@@ -2,28 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Markdown from 'Components/text/markdown';
-import { getAvatarUrl, getLink } from '../models/project';
-import { ProjectLink } from './includes/link';
-import ProjectOptionsPop from './pop-overs/project-options-pop';
-import UsersList from './users-list';
-import Note from './note';
-import WrappingLink from './includes/wrapping-link';
+import { ProjectLink } from '../../presenters/includes/link';
+import ProjectAvatar from '../../presenters/includes/project-avatar';
+import ProjectOptionsPop from '../../presenters/pop-overs/project-options-pop';
+import UsersList from '../../presenters/includes/users-list';
+import styles from './project-item.styl';
 
-con
+// TODO: componentize, share with small-collection-item
+const FakeButton = ({ children }) => <div className="button">{children}</div>;
+const PrivateIcon = () => <span className="project-badge private-project-badge" aria-label="private" />;
 
-
-const ProjectItem = ({ project, collection, hideProjectDescriptions, ...props }) => (
-  <>
-    <UsersList glitchTeam={project.showAsGlitchTeam} users={project.users} extraClass="single-line" teams={project.teams} />
+const ProjectItem = ({ project,  hideProjectDescriptions, ...props }) => (
+  <div className={styles.container}>
+    <div className={styles.userListContainer}>
+      <UsersList extraClass="single-line" glitchTeam={project.showAsGlitchTeam} users={project.users} teams={project.teams} />
+    </div>    
     <ProjectOptionsPop project={project} {...props} />
-    <WrappingLink href={getLink(project)} className="button-area">
-      <div
-        className={classnames('project', { 'private-project': project.private, 'hide-description': hideProjectDescriptions })}
-        data-track="project"
-        data-track-label={project.domain}
-      >
-        <div className="project-container">
-          <img className="avatar" src={getAvatarUrl(project.id)} alt="" />
+    <ProjectLink className={classnames(styles.projectBody, {
+        [styles.private]: project.private,
+        [styles.hideDescription]: hideProjctDescriptions,
+      })}>
+      <div className={styles.projectHeader}>
+        <div className={styles.avatarWrap}>
+          <ProjectAvatar id={project.id} />
+        </div>
+        <div className={styles.nameWrap}>
+        <FakeButton>
+          {project.private && <PrivateIcon />}
+          <div className={styles.projectDomain}>{project.domain}</div>
+        </FakeButton>
+        </div>
+      </div>
+
+      
+      <img className="avatar" src={getAvatarUrl(project.id)} alt="" />
           <ProjectLink project={project} className="button">
             <span className="project-badge private-project-badge" aria-label="private" />
             <div className="project-name">{project.domain}</div>
@@ -36,8 +48,8 @@ const ProjectItem = ({ project, collection, hideProjectDescriptions, ...props })
           <div className="overflow-mask" />
         </div>
       </div>
-    </WrappingLink>
-  </>
+    </ProjectLink>
+  </div>
 );
 
 ProjectItem.propTypes = {
