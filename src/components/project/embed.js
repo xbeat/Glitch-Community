3f56d9b2648deed5bd8a215bf0bf80724eb52001
Glitch (community-staging) <none>
2019-04-03 18:35:42 +0000

    🐑🐟 Checkpoint ./src/components/project/embed.js:433767/167

--- src/components/project/embed.js
+++ src/components/project/embed.js
@@ -11,18 +11,18 @@ const telescopeImageUrl = 'https://cdn.glitch.com/7138972f-76e1-43f4-8ede-84c3cd
 // Babel does not transpile URLSearchParams, so using this as a compatibility check for showing embeds now.
 // TODO(sheridan) make this more robust in future
 const browserSatisfiesRequirements = (() => {
-  try {
-    const test = new Function('return () => true'); // eslint-disable-line no-new-func
-    if (test()() !== true) {
-      throw new Error('Arrow functions are not supported, so the editor will not work');
-    }
-    return true;
-  } catch (error) {
-    console.log(
-      "Sorry, you don't have the necessary JavaScript features to run Glitch code editors. Try applying your latest system updates, or try again with a different web browser.",
-      error,
-    );
-  }
+  // try {
+  //   const test = new Function('return () => true'); // eslint-disable-line no-new-func
+  //   if (test()() !== true) {
+  //     throw new Error('Arrow functions are not supported, so the editor will not work');
+  //   }
+  //   return true;
+  // } catch (error) {
+  //   console.log(
+  //     "Sorry, you don't have the necessary JavaScript features to run Glitch code editors. Try applying your latest system updates, or try again with a different web browser.",
+  //     error,
+  //   );
+  // }
   return false;
 })();
 
@@ -45,7 +45,7 @@ const Embed = ({ domain }) => (
       // Error message if JS not supported
       // TODO(sheridan): Refactor this once we have a true error component
       <div>
-        <Image src={telescopeImageUrl} width="35%" />
+        <Image src={telescopeImageUrl} width="50%" height="50%" />
         <div>
           <h2>The web browser you're using is missing some important Javascript features</h2>
           <p>To use this app, please try applying your latest system updates, or try again with a different web browser.</p>
