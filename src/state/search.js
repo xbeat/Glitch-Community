75976c63aeff9bb1ec5a7b913c11aed63c6e3f09
Glitch (community) <none>
2019-04-04 21:34:25 +0000

    👌⛷ Checkpoint ./src/state/search.js:271885/1204

--- src/state/search.js
+++ src/state/search.js
@@ -110,6 +110,31 @@ export function useAlgoliaSearch(query) {
   };
 }
 
+const starterKits = [
+  {
+    id: 1,
+    keywords: ['react'],
+    imageURL: 'https://glitch.com/culture/content/images/2018/10/react-starter-kit-1.jpg',
+    name: 'Build a Web App with React',
+    url: 'https://glitch.com/culture/react-starter-kit/'
+  },
+  {
+    id: 2,
+    keywords: ['website'],
+    imageURL: 'https://glitch.com/culture/content/images/2018/10/website-starter-kit-1.jpg',
+    name: 'How to Make a Website',
+    url: 'https://glitch.com/culture/website-starter-kit/'
+  },
+  {
+    id: 3,
+    keywords: ['vr', 'webvr'],
+    imageURL: 'https://glitch.com/culture/content/images/2019/02/WebVR-Starter-Kit.-Part-1_-Intro-to-WebVR-1.png',
+    name: 'An Intro to WebVR',
+    url: 'https://glitch.com/culture/an-intro-to-webvr/',
+  },
+]
+
+
 const formatLegacyResult = (type, query) => (hit) => ({
   ...hit,
   type,
