16a276a8f7c3b6f0313d132e5e53e2270dc17fe9
Glitch (community-staging) <none>
2019-04-08 21:01:34 +0000

    🍱🍥 Checkpoint ./src/components/blocks/search-result-cover-bar.js:271885/3414 ./src/components/blocks/search-result-cover-bar.styl:271885/76 ./src/state/search.js:271885/12

--- src/components/blocks/search-result-cover-bar.js
+++ src/components/blocks/search-result-cover-bar.js
@@ -1,5 +1,7 @@
+/* globals CDN_URL */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Image from 'Components/images/image';
 import { getProfileStyle as getTeamProfileStyle } from '../../models/team';
 import { getProfileStyle as getUserProfileStyle } from '../../models/user';
 import styles from './search-result-cover-bar.styl';
@@ -9,7 +11,35 @@ const getProfileStyles = {
   user: getUserProfileStyle,
 };
 
-const SearchResultCoverBar = ({ type, item, size }) => <div className={styles.cover} style={getProfileStyles[type]({ ...item, size })} />;
+const cacheBuster = Math.floor(Math.random() * 1000);
+
+const TeamCoverBar = ({ size, id, cache = cacheBuster }) => (
+  <div className={styles.cover}>
+    <Image
+      src={`${CDN_URL}/team-cover/${id}/${size}?${cache}`}
+      defaultSrc="https://cdn.glitch.com/55f8497b-3334-43ca-851e-6c9780082244%2Fdefault-cover-wide.svg?1503518400625"
+      alt=""
+    />
+  </div>
+);
+
+const UserCoverBar = ({ size, id, cache = cacheBuster }) => (
+  <div className={styles.cover}>
+    <Image
+      src={`${CDN_URL}/team-cover/${id}/${size}?${cache}`}
+      defaultSrc="https://cdn.glitch.com/55f8497b-3334-43ca-851e-6c9780082244%2Fdefault-cover-wide.svg?1503518400625"
+      alt=""
+    />
+  </div>
+);
+
+const SearchResultCoverBar = ({ type, item, size }) => {
+  if (type === 'team') {
+    return <TeamCoverBar size={size} id={item.id} />;
+  }
+
+  return <div className={styles.cover} style={getProfileStyles[type]({ ...item, size })} />;
+};
 
 SearchResultCoverBar.propTypes = {
   type: PropTypes.oneOf(['user', 'team']).isRequired,
