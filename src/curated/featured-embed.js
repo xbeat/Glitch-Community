e4fb62d6eccd8f03f29b0a4060dcbe6506abfaa5
Glitch (community-staging) <none>
2019-04-10 12:11:22 +0000

    🎠📼 Checkpoint ./src/curated/featured-embed.js:11/36

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -6,7 +6,7 @@ export default {
   image: 'https://community.glitch.me/culture/content/images/2019/04/Glitch_Illo_TechArt-VR-Headline-v1--1-.png',
   mask: 'mask-4',
   title: 'Master WebVR This Week',
-  appDomain: 'aframe-torus-knots',
+  appDomain: 'aframe-vr-10-print',
   blogUrl: '/master-webvr-with-a-week-of-experiments/',
   body,
   color: '#f5f5f5',
