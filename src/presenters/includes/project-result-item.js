f51834882f2d1966cfab673a4f8a14ca447cfea1
Glitch (nova-chance) <none>
2019-04-10 14:50:22 +0000

    🐠🖼 Checkpoint ./src/presenters/users-list.js:97325/30 ./src/presenters/includes/project-result-item.js:97325/19

--- src/presenters/includes/project-result-item.js
+++ src/presenters/includes/project-result-item.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { ProjectLink } from './link';
-import { StaticUsersList } from '../users-list';
+import { StaticUsersList } from 'Components/user/users-list';
 import ProjectAvatar from './project-avatar';
 
 const ProjectResultItem = ({ onClick, isActive, isPrivate, ...project }) => {
