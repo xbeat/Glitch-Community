e0fa97c1a4d609517d382b8c353ea97e21785308
Glitch (community-staging) <none>
2019-04-15 10:24:29 +0000

    🚨💐 Checkpoint ./src/curated/featured.js:11/3 ./package.json:11/2

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -16,16 +16,16 @@ export default [
   {
     title: 'Pseudo-chat UI for Transcripts',
     img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FEmbedded-chat.png?1555319742567',
-    link: 'https://glitch.com/~embedded-chat'
+    link: 'https://glitch.com/~embedded-chat',
   },
   {
     title: 'AI Maze Solver',
     img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FMaze%20solver.png?1555319743474',
-    link: 'https://glitch.com/~maze-solver'  
+    link: 'https://glitch.com/~maze-solver',  
   },  
   {
     title: 'Knitting Color Combos',
     img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fknitting-color-combo.png?1555319743236',
-    link: 'https://glitch.com/~knitting-color-combo'
+    link: 'https://glitch.com/~knitting-color-combo',
   }
 ];
